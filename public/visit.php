f9899861ebc2335a10a4eb2aded76fe4fc9fdf63
cameronpoe <cameronmpoe@gmail.com>
2022-08-26 15:21:07 -0500

    hours of op edits

--- public/visit.php
+++ public/visit.php
@@ -16,6 +16,10 @@
       <?php include "./includes/dropdown.html" ?>
 
       <div class="information">
+        <h1>Hours of Operation</h1>
+        <p>Doc does not have set hours, but our cinema doors typically open 30 minutes before the first screening of the day and close 15 minutes after the last screening of the day. If you need access to the space outside of those hours or if you think you lost something in the cinema, please <u><a href="/about/#contact">send us an email or leave a voicemail</a></u>.</p>
+
+        <br><br>
 
         <h1>Tickets</h1>
         <p>Single tickets and passes can be bought at the theater 30 minutes before each screening. Quarterly membership passes grant holders free entry to all general admission films for the quarter.</p>
@@ -30,6 +34,29 @@
         </ul></i>
         <br>
         <p style="font-size: 1.5rem;"><i>For now, Doc is <u>cash-only</u> for tickets purchased at the theater. There is a Citibank ATM in Ida Noyes for your convenience.</i></p>
+        <br>
+        <p><b>Pass discounts:</b> Doc offers discounts on our quarter passes. Please let a ticket seller know if the following applies to you:</p>
+        <br>
+        <ul class="prices">
+          <li><p><b><u>Criteria</u></b></p></li>
+          <li><p><b><u>Discount</u></b></p></li>
+        </ul>
+        <i><ul class="prices">
+          <li><p>Pass from most recent quarter...</p></li>
+          <li><p>$2</p></li>
+        </ul>
+        <ul class="prices">
+          <li><p>Ticket stub from first 2 weeks of current quarter...</p></li>
+          <li><p>$7</p></li>
+        </ul>
+        <ul class="prices">
+          <li><p>Prior pass and recent ticket stub...</p></li>
+          <li><p>$9</p></li>
+        </ul></i>
+        <br>
+        <p><b>Group discounts:</b> We also sell single tickets at a <u>$5/ticket</u> discount for parties of 8 or more, but you must <u><a href="/about/#contact">send us an email or voicemail</a></u> ahead of time. </p>
+        <br>
+        <!-- <p style="font-size: 1.5rem;"><i>Note: discounts cannot be applied to online purchases.</i></p> -->
 
         <br><br>
 
