7367a4e273ac148e671053f616e83193b71ade63
cameronpoe <cameronmpoe@gmail.com>
2022-09-05 11:56:16 -0500

    created custom 404 page

---
+++ public/page-not-found.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Error</title>
+    <link rel="stylesheet" type="text/css" href="/style.css">
+  </head>
+
+  <body>
+    
+    <?php include "./includes/header.html" ?>
+  
+    <main>
+
+      <?php include "./includes/dropdown.html" ?>
+
+      <div class="information">
+        <h1>ERROR: 404 Page Not Found</h1>
+        <p>Unfortunately, the URL entered either does not correspond to a page on our site or the page has been removed. Please check the spelling of your URL and try again.</p>
+      </div>
+        
+    </main>
+    
+    <?php include "./includes/footer.html" ?>
+
+  </body>
+</html>
