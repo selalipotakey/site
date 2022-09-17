73793c709716b3079e3088d1023d2400311608ed
cameronpoe <cameronmpoe@gmail.com>
2022-09-17 18:07:41 -0500

    colons edit

--- public/calendar/index.php
+++ public/calendar/index.php
@@ -28,7 +28,7 @@
             <a href="/calendar/monday"><img src="/images/2022fall/series_stills/whats-up-doc-1972.jpeg" alt="Whats Up Doc still"></a>
             <div class="series-text">
               <a href="/calendar/monday">
-                <p class="day">Monday:</p>
+                <p class="day">Monday</p>
                 <p class="title">Wonderfully Loathsome: Screwball Romance Through the Ages</p>
               </a>
               <p class="programmer">Programmed by: Liam Flanigan</p>
@@ -39,7 +39,7 @@
             <a href="/calendar/tuesday"><img src="/images/2022fall/series_stills/long-days-journey-into-night-2018.jpg" alt="Long Days Journey Into Night still"></a>
             <div class="series-text">
               <a href="/calendar/tuesday">
-                <p class="day">Tuesday:</p>
+                <p class="day">Tuesday</p>
                 <p class="title">After the 5th: China and the 21st Century</p>
               </a>
               <p class="programmer">Programmed by: Addison Wood</p>
@@ -50,7 +50,7 @@
             <a href="/calendar/wednesday"><img src="/images/2022fall/series_stills/irma-vep-1996.jpg" alt="Irma Vep still"></a>
             <div class="series-text">
               <a href="/calendar/wednesday">
-                <p class="day">Wednesday:</p>
+                <p class="day">Wednesday</p>
                 <p class="title">Center Stage: The Films of Maggie Cheung</p>
               </a>
               <p class="programmer">Programmed by: Deany Cheng</p>
@@ -61,7 +61,7 @@
             <a href="/calendar/thursday1"><img src="/images/2022fall/series_stills/chimes-at-midnight-1966.jpg" alt="Chimes at Midnight still"></a>
             <div class="series-text">
               <a href="/calendar/thursday1">
-                <p class="day">Thursday I:</p>
+                <p class="day">Thursday I</p>
                 <p class="title">Shakespeare Remixed</p>
               </a>
               <p class="programmer">Programmed by: Michelle Chow</p>
@@ -72,7 +72,7 @@
             <a href="/calendar/thursday2"><img src="/images/2022fall/series_stills/belladonna-of-sadness-1973.jpg" alt="Chimes at Midnight still"></a>
             <div class="series-text">
               <a href="/calendar/thursday2">
-                <p class="day">Thursday II:</p>
+                <p class="day">Thursday II</p>
                 <p class="title">Myths, Legends, and Folk Tales: A Brief History of Animation</p>
               </a>
               <p class="programmer">Programmed by: Solana Adedokun</p>
@@ -83,7 +83,7 @@
             <a href="/calendar/friday"><img src="/images/2022fall/series_stills/dogtooth-2009.jpg" alt="Dogtooth still"></a>
             <div class="series-text">
               <a href="/calendar/friday">
-                <p class="day">Friday:</p>
+                <p class="day">Friday</p>
                 <p class="title">Programmers' Picks</p>
               </a>
               <p class="programmer">Programmed by: Addison Wood, Ian Resnick, and Hannah Halpern</p>
@@ -94,8 +94,8 @@
             <a href="/calendar/saturday"><img src="/images/2022fall/series_stills/top-gun-maverick-2022.png" alt="Top Gun Maverick still"></a>
             <div class="series-text">
               <a href="/calendar/saturday">
-                <p class="day">Saturday:</p>
-                <p class="title">Top Doc: Maverdock: New Releases</p>
+                <p class="day">Saturday</p>
+                <p class="title">Top Doc: Maverdock - New Releases</p>
               </a>
             </div>
           </div>
@@ -104,7 +104,7 @@
             <a href="/calendar/sunday"><img src="/images/2022fall/series_stills/the-nun-1966.jpg" alt="The Nun still"></a>
             <div class="series-text">
               <a href="/calendar/sunday">
-                <p class="day">Sunday:</p>
+                <p class="day">Sunday</p>
                 <p class="title">Jacques Rivette: New Wave Master</p>
               </a>
               <p class="programmer">Programmer: Kathleen Geier</p>
