d615a77a936a7ff06dbb11cac4771b84cfa42fb4
cameronpoe <cameronmpoe@gmail.com>
2022-09-17 17:34:01 -0500

    sunday page

--- public/calendar/sunday.php
+++ public/calendar/sunday.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sunday</title>
+    <title>Monday Series</title>
     <link rel="stylesheet" type="text/css" href="/style.css">
   </head>
 
@@ -15,13 +15,89 @@
 
       <?php include "../includes/dropdown.html" ?>
 
-      <div class="information" style="text-align: center">
-        <h1>Come back soon to see the full screening list!</h1>
+      <div class="screenings-list">
+
+        <div class="text-section">
+          <h1>SUNDAY: Jacques Rivette, New Wave Master</h1>
+          <h3>Programmed by Kathleen Geier</h3>
+
+          <p>Jacques Rivette was one of the greatest directors of his time, an acknowledged inspiration for such contemporary cinematic visionaries as David Lynch and Claire Denis. If his work remains less well known than that of his peers, that is largely because he was, as Martin Scorsese noted, “the most experimental of the French New Wave directors.” His films are distinguished by their tendency to explode cinematic conventions and upend traditional narratives. He strongly emphasized the collective nature of filmmaking and his own methods were heavily collaborative. Many of his films were improvised or co-scripted by the actors.</p>
+
+          <p>Frequently, his most important collaborators were women. That’s not surprising, because of all the French New Wave directors, it is Rivette who, along with Agnès Varda, is the most deeply interested in women. His films are notable for their strong female characters and their exploration of feminist themes. During our own era of ferocious antifeminist backlash, a film like his scathing masterpiece <i>The Nun</i> seems timelier than ever.</p>
+
+          <p>There are other reasons why Rivette seems necessary right now. In an era when political paranoia is at a fever pitch, his haunting films, with their conspiracy-obsessed protagonists, feel especially resonant. At a time when Hollywood products are more formulaic than ever, his radical experimentation is all the more thrilling. Finally, his films are greatly benefitted by being seen on the big screen, in the immersive environment of a theater. His films are demanding but also richly rewarding. Rivette is the rare filmmaker who can make you see not just cinema, but the world, in an entirely new way.</p>
+        </div>
+
+        <div class="screening">
+            <h1>5:00PM October 2nd</h1>
+            <img src="/images/2022fall/the-nun-1966.jpg" alt="THE NUN still">
+            <h2>The Nun (1966)</h2>
+            <h3>Jacques Rivette &middot; 140m &middot; DCP</h3>
+            <p>Rivette’s second feature is an incendiary feminist masterpiece banned in France and condemned by the Catholic Church. It tells the story of a young woman (a radiant Anna Karina) forced to enter a convent. She fights for her freedom, but finds tyranny and brutality at every turn. The intensity and formal rigor of Rivette’s technique and the striking contemporary score make <i>The Nun</i> one of the most exhilarating and essential films of the French New Wave.</p>
+        </div>
+
+        <div class="screening">
+            <h1>5:00PM October 9th</h1>
+            <img src="/images/2022fall/celine-and-julie-go-boating-1974.jpg" alt="CELINE AND JULIE GO BOATING still">
+            <h2>Céline and Julie Go Boating (1974)</h2>
+            <h3>Jacques Rivette &middot; 193min &middot; Digital</h3>
+            <p>Rivette’s best-known film is an exuberant, freewheeling fantasy about the power of female camaraderie, the delights of narrative, and the fluid boundaries between life and art. After magician Céline (Juliet Berto) and librarian Julie (Dominique Labourier) meet, they move in together, swap identities, and time travel to a mansion with a gothic murder mystery. Co-written by the film's actresses, the film is one of Rivette's most enjoyable and enduring works.</p>
+        </div>
+
+        <div class="screening">
+            <h1>5:00PM October 16th</h1>
+            <img src="/images/2022fall/duelle-1976.jpg" alt="DUELLE still">
+            <h2>Duelle (1976)</h2>
+            <h3>Jacques Rivette &middot; 121m &middot; DCP</h3>
+            <p>In the shadowy demimonde of contemporary Paris, the Sun and Moon Goddesses do battle over a magic diamond that will allow the winner to remain on Earth. At the time of its release, this ravishing, daringly experimental, noir-stained fantasy divided critics. But it has since come to be regarded as one of Rivette's most striking and inventive films. It was part of a projected tetralogy of which only one other episode (1976’s <i>Noroît</i>) was ever completed.</p>
+        </div>
+
+        <div class="screening">
+            <h1>5:00PM October 30th</h1>
+            <img src="/images/2022fall/out-1-spectre-1972.jpg" alt="OUT 1: SPECTRE still">
+            <h2>Out 1: Spectre (1972)</h2>
+            <h3>Jacques Rivette &middot; 264min &middot; Digital</h3>
+            <p>This condensed version of Rivette’s 13-hour magnum opus, <i>Out 1: Noli Me Tangere</i>, has been hailed as a masterpiece in its own right. Entirely improvised, it tells the story of two outsiders investigating a conspiracy involving avant-garde theater troupes and an Illuminati-like secret society. In the words of critic Jonathan Romney, the film is “about things, people, and communal causes falling apart, arguably a lament for France’s failed utopia of 1968.”</p>
+        </div>
+
+        <div class="screening">
+            <h1>5:00PM November 6th</h1>
+            <img src="/images/2022fall/the-gang-of-four-1988.jpeg" alt="THE GANG OF FOUR still">
+            <h2>The Gang of Four (1989)</h2>
+            <h3>Jacques Rivette &middot; 160min &middot; DCP</h3>
+            <p>Four young women are sharing a home and studying at a drama school run by a notoriously demanding grande dame. As they rehearse a play, one of the group mysteriously disappears, an enigmatic stranger arrives on the scene, and the lines between theater and “real life” become increasingly blurred. Gorgeously lensed by the great Caroline Champetier, <i>Gang of Four</i> is an engrossing exploration of Rivette’s classic themes and one of his most thoughtful works.</p>
+        </div>
+
+        <div class="screening">
+            <h1>5:00PM November 13th</h1>
+            <img src="/images/2022fall/la-belle-noiseuse-1991.jpg" alt="LA BELLE NOISEUSE still">
+            <h2>La Belle Noiseuse (1991)</h2>
+            <h3>Jacques Rivette &middot; 238min &middot; DCP</h3>
+            <p>Rivette’s biggest box office hit and the most acclaimed film of his later career was this mesmerizing drama of artistic obsession. It stars an aging painter, his reluctant muse, and their struggle to finish a long-abandoned portrait. Jonathan Rosenbaum wrote, “rarely has Rivette’s use of duration to look at process been as spellbinding as it is here . . . Rivette’s superb sense of rhythm and mise en scene never falters, and the plot has plenty of twists.”</p>
+        </div>
+
+        <div class="screening">
+            <h1>5:00PM November 20th</h1>
+            <img src="/images/2022fall/joan-the-maid-the-battles-1994.jpg" alt="JOAN THE MAID: THE BATTLES still">
+            <h2>Joan the Maid: The Battles (1994)</h2>
+            <h3>Jacques Rivette &middot; 160m &middot; DCP</h3>
+            <p>Countless films have been about Joan of Arc, but as critic Jonathan Rosenbaum noted, Rivette’s rigorously materialist version is “perhaps the only movie that offers a plausible portrait of what the 15th-century teenager who led the French into battle was actually like." In part one of this epic, young Joan (an extraordinary Sandrine Bonnaire) leaves her childhood home and persuades Charles VII to allow her to lead his troops in the war against the English.</p>
+        </div>
+
+        <div class="screening">
+            <h1>5:00PM December 4th</h1>
+            <img src="/images/2022fall/joan-the-maid-the-prisons-1994.jpg" alt="JOAN THE MAID: THE PRISONS still">
+            <h2>Joan the Maid: The Prisons (1994)</h2>
+            <h3>Jacques Rivette &middot; 177min &middot; DCP</h3>
+            <p>Part two of Rivette’s epic chronicles Joan’s imprisonment and interrogation by the British, but notably omits her trial, which features prominently in other cinematic accounts of her life. As in part one, Rivette’s focus on quotidian details and the spaces between the great events of Joan's life humanizes her in ways that are profoundly moving. Rivette’s long takes and the beauty and austerity of his images illuminate the saint’s inner and outer lives..</p>
+        </div>
+
       </div>
-        
+      
     </main>
     
     <?php include "../includes/footer.html" ?>
+    <script src="/js/resize-menu.js"></script>
 
   </body>
 </html>
