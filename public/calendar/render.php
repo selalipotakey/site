755ed898a222ae7de4c7a713227abd756c7e3155
cameronpoe <cameronmpoe@gmail.com>
2022-10-06 17:50:26 -0500

    fixed the add to calendar issue

---
+++ public/calendar/render.php
@@ -0,0 +1,6 @@
+<html>
+    <head>
+        <!--THIS FILE IS JUST TO REROUTE THE LOCAL ICALENDAR 'ADD TO CALENDAR' LINK TO AN ACTUAL GOOGLE CALENDAR 'ADD TO CALENDAR' PAGE-->
+        <meta http-equiv="refresh" content="0; URL=https://calendar.google.com/calendar/u/0/r?cid=c_7a2j66n29m80g326io6blho2cc@group.calendar.google.com" />
+    </head>
+</html>
