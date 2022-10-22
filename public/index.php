9525c150f1bba5f801efa197eaa900c3ce3cc41f
cameronpoe <cameronmpoe@gmail.com>
2022-10-22 14:19:52 -0500

    added weekend screenings.

--- public/index.php
+++ public/index.php
@@ -96,63 +96,91 @@
       <div class="screenings-list">
 
         <div class="text-section">
-          <h1>Playing this week:</h1>
+          <h1>Playing this weekend:</h1>
           <h3><i>$7 tickets, theater opens 30 minutes before the screening.</i></h3>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Monday, October 17th</h1>
-          <img src="/images/2022fall/nintochka-1938.jpeg" alt="Ninotchka still">
-          <h2>Ninotchka (1939)</h2>
-          <h3>Ernst Lubitsch &middot; 110m &middot; 35mm</h3>
-          <p>Greta Garbo shines as the titular Soviet envoy who is summoned to interbellum Paris to finalize the sale of jewelry confiscated in the Russian Revolution. While there, she must resist the charms of both capitalism and Melvyn Douglas, playing an aristocrat who is trying to retrieve the jewelry himself. Directed by Ernst Lubitsch from a script co-written by Billy Wilder, <i>Ninotchka</i> is a warm-hearted love story with a sharp satirical edge.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E4DB3648-D3E3-4664-80D3-46B0DFEBB2AB" target="_blank">here</a></u></b>.</p>
+          <h1>7:00PM October 21st</h1>
+          <img src="/images/2022fall/akira-1988.jpg" alt="AKIRA still">
+          <h2>Akira (1988)</h2>
+          <h3>Katsuhiro Otomo &middot; 124min &middot; DCP</h3>
+          <p>In a post-WWIII Tokyo, a secret military project causes biker gang member Tetsuo to develop telekinetic powers—the same powers used by Akira, a mysterious figure from the past, to annihilate Tokyo 31 years prior. His childhood friend Kaneda infiltrates the military complex to find him, but the Tetsuo he knew is already gone. Haunting, frenetic, and masterfully animated, <i>Akira</i> pioneered cyberpunk and resonated deeply in Japanese pop culture and beyond.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=54D0FD07-B203-479E-9BE3-6A7139EBDCF6" target="_blank">here</a></u></b>.
         </div>
 
         <div class="screening">
-          <h1>7:00PM Tuesday, October 18th</h1>
-          <img src="/images/2022fall/dead-pigs-2018.jpg" alt="Dead Pigs still">
-          <h2>Dead Pigs (2018)</h2>
-          <h3>Cathy Yan &middot; 130min &middot; DCP</h3>
-          <p>After last week’s screening, we need a little levity. Based on an actual incident involving 16,000 dead pigs, Cathy Yan’s (<i>Succession</i>, <i>Birds of Prey</i>) directorial debut is a kaleidoscopic tale of Shanghai in an era of breakneck development and increasing Western influence. Featuring a mix of Mandarin, English, and Shanghainese, <i>Dead Pigs</i> is a darkly hilarious social satire of humanity’s inability to find peace in an ever-changing world.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=76ECF22D-5462-4BDA-B2B4-BD18DD32B283" target="_blank">here</a></u>.</b></p>
+          <h1>9:30PM October 21st</h1>
+          <img src="/images/2022fall/the-queen-1968.jpg" alt="THE QUEEN still">
+          <h2>Lives:Visible (2018) // The Queen (1968)</h2>
+          <h3>Michelle Citron // Frank Simon &middot; 35min // 68min &middot; Digital // DCP</h3>
+          <p>Welcome to the 1967 Miss All-America Camp Beauty Contest! Featuring drag performances by the likes of Crystal LaBeija and a cameo from Andy Warhol, <i>The Queen</i> is one of the earliest looks at what would later become New York City ball culture. Sampled by Frank Ocean, reenacted on <i>Drag Race</i>, this documentary is only beginning to re-emerge as an essential piece of American queer culture. Screening will be preceded by Michelle Citron's <i>Lives:Visible</i>, documenting butch/fem life in mid-century Chicago. <b>NOTE: Free admission to all attendees who come in drag.</b></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=1F333E92-4962-4464-A4AD-EEFCFEF65FEB" target="_blank">here</a></u></b>.
         </div>
 
         <div class="screening">
-          <h1>7:00PM Wednesday, October 19th</h1>
-          <img src="/images/2022fall/the-eagle-shooting-heroes-1993.jpg" alt="The Eagle Shooting Heroes still">
-          <h2>The Eagle Shooting Heroes (1993)</h2>
-          <h3>Jeffrey Lau &middot; 103m &middot; Digital</h3>
-          <p>Rumored to have been born, like <i>Chungking Express</i>, from the long editing process of Wong Kar-wai’s <i>Ashes of Time</i>, Jeffrey Lau’s <i>The Eagle Shooting Heroes</i> features a veritable all-star cast of Hong Kong’s finest—Maggie Cheung, Brigitte Lin, and not one but both Tony Leungs, among others—clowning it up exquisitely. Featuring more slapstick, musical numbers, and wire-fu than you can shake a stick at, it is the gold standard for star-studded cash-grabs.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=4F3A86B1-A429-40D8-9285-5BFE93C3BEC1" target="_blank">here</a></u>.</b></p>
+          <h1>1:00PM October 22nd</h1>
+          <img src="/images/2022fall/twin-peaks-fire-walk-with-me-1992.png" alt="FIRE WALK WITH ME still">
+          <h2>Twin Peaks: Fire Walk with Me (1992)</h2>
+          <h3>David Lynch &middot; 135min &middot; DCP</h3>
+          <p>The prequel to Lynch's acclaimed TV series <i>Twin Peaks</i>, <i>Fire Walk With Me</i> chronicles the fateful last seven days of Laura Palmer's life. What could have led to the death of the beautiful homecoming queen and nightshift waitress in the strange town of Twin Peaks? Agent Dale Cooper (Kyle MacLachlan) uncovers the horrid, chilling details. Torn apart by critics at release, this twisted tragedy is now considered by some to be Lynch's underrated masterpiece.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=EFEB89AE-2E9A-4D2D-8FAF-17BD252BCBAB" target="_blank">here</a></u></b>.
         </div>
 
         <div class="screening">
-          <h1>7:00PM Thursday, October 20th</h1>
-          <img src="/images/2022fall/forbidden-planet-1956.jpg" alt="Forbidden Planet still">
-          <h2>Forbidden Planet (1956)</h2>
-          <h3>Fred M. Wilcox &middot; 98m &middot; DCP</h3>
-          <p>A hugely influential space opera that draws inspiration from "The Tempest," <i>Forbidden Planet</i> is a classic that lays claim to many sci-fi firsts. With its vivid Cinemascope color and all-electronic score, the film follows Commander Adams (Leslie Nielsen) and his starship as they travel to a distant planet to discover what happened to the members of a previous expedition, but find only one scientist, his daughter, and a robot—who warn them not to approach.</p>
-          <p style="color: red"><b>Followed by a discussion with Professor Ellen Mackay from the Dept. of English Language and Literature!</b></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=F63808B5-A78F-4D45-B240-6367E30E33C6" target="_blank">here</a></u>.</b></p>
+          <h1>4:00PM October 22nd</h1>
+          <img src="/images/2022fall/marnie-1964.png" alt="MARNIE still">
+          <h2>Marnie (1964)</h2>
+          <h3>Alfred Hitchcock &middot; 130min &middot; DCP</h3>
+          <p>While answers may differ to which is the best Hitchcock movie, it’s difficult to argue against <i>Marnie</i> being the <i>most</i> Hitchcock movie. Tippi Hedren stars as the titular kleptomaniac, whose ruse is found out by her outwardly dashing but controlling boss (Sean Connery). Filled with striking visuals, chilling performances, and sexual perversion hidden under a high-class veneer, <i>Marnie</i> is the Master of Suspense’s bag of tricks at its deepest.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=C33F3AF2-F412-42D5-B617-3ED643DE504B" target="_blank">here</a></u></b>.
         </div>
 
         <div class="screening">
-          <h1><u>10:00PM</u> Thursday, October 20th</h1>
-          <img src="/images/2022fall/the-thief-and-the-cobbler-1992.png" alt="The Thief and the Cobbler: A Moment in Time still">
-          <h2>The Thief and the Cobbler: A Moment in Time (1992)</h2>
-          <h3>Richard Williams &middot; 89m &middot; DCP</h3>
-          <p>Directed by Richard Williams, <i>The Thief and the Cobbler</i> is infamous for its nearly thirty year production cycle that left it incomplete. While grossly over budget and meeting critical and commercial failure upon release, the film would later become revered by critics and fans. Doc is proud to present the <i>A Moment in Time</i> Academy restoration, the closest to Williams' intended 1992 version, saved before the studios radically altered it for release.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=B89A8450-EB75-4A9A-9BD9-6332A66F7319" target="_blank">here</a></u>.</b></p>
-        </div>
-
-        <div class="screening">
-          <h1>This Weekend:</h1>
+          <h1>7:30PM October 22nd</h1>
           <img src="/images/2022fall/funeral-parade-of-roses-1969.jpeg" alt="FUNERAL PARADE OF ROSES still">
-          <h2>Doc90: Celebrating 90 Years of Doc</h2>
-          <h3>Friday 10/21 through Sunday 10/23</h3>
-          <p>Join Doc Films as we celebrate 90 years of being Chicago's snobbiest cinephiles! From October 21st to the 23rd, stop by the Max P Cinema for the late-night zombie masterpiece <i>Night of the Living Dead</i>, hear director Michelle Citron discuss her works <i>Daughter Rite</i> and <i>What You Take For Granted</i>, or find out how current University President Paul Alivisatos feels about his days in Doc Films. We hope to see you there!</p>
-          <p>Find out more information <b><u><a href="/90th">here</a></u></b>.</p>
+          <h2>Funeral Parade of Roses (1969)</h2>
+          <h3>Toshio Matsumoto &middot; 105min &middot; DCP</h3>
+          <p>Arthouse, documentary, and '60s Avant-garde come together in Tokyo's queer underworld in this loose adaptation of "Oedipus Rex." <i>Funeral Parade of Roses</i> follows Eddie, a transgender woman (played by Japanese gay entertainer Peter—no last name) through a scrambled timeline and a mounting rivalry at one of Tokyo's gay bars. Both ahead of its time and a product of it, the film is a heady, wild, beautiful, and sometimes grisly gay cinematic experience.</p>
+          <p><i style="color: red">With introduction by former Doc Films President and current University of Chicago President Paul Alivisatos!</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=A2B2E8A9-CD6B-42DB-8A01-1BF8088E7C79" target="_blank">here</a></u></b>.
+        </div>
+
+        <div class="screening">
+          <h1>10:00PM October 22nd</h1>
+          <img src="/images/2022fall/night-of-the-living-dead-1968.jpeg" alt="NIGHT OF THE LIVING DEAD still">
+          <h2>Night of the Living Dead (1968)</h2>
+          <h3>George Romero &middot; 97min &middot; DCP</h3>
+          <p>THEY WON'T STAY DEAD! Much like Doc Films, George Romero's gruesome ghouls have reanimated to plague the living. In this revolutionary film that brought us the modern zombie flick, a group of strangers are marooned in an isolated farmhouse where they must fend for their lives against the undead. Derided by critics at its release, the film has since become a classic, alongside Duane Jones' role as a hero—a rare casting choice for a Black actor at the time.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=05B8EF0A-E74C-4E69-936F-2C6D300A9AFD" target="_blank">here</a></u></b>.
+        </div>
+
+        <div class="screening">
+          <h1>1:00PM October 23rd</h1>
+          <img src="/images/2022fall/to-sleep-with-anger-1990.jpeg" alt="TO SLEEP WITH ANGER still">
+          <h2>To Sleep With Anger (1990)</h2>
+          <h3>Charles Burnett &middot; 102min &middot; DCP</h3>
+          <p>Charming drifter Harry (Danny Glover) drops in to visit his old friend Gideon (Paul Butler) at his home in South-Central LA. His genteel manner, however, belies a corrupting influence which brings Gideon's family tensions to a rolling boil. Set to a gospel- and blues-infused soundtrack, <i>To Sleep With Anger</i> is an intimate, evocative family drama, painting its themes of religion, family, and duty with both sensitive realism and a deft comedic hand.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=55AD2F1B-3ED6-45E0-B225-2AAD1C597820" target="_blank">here</a></u></b>.
+        </div>
+
+        <div class="screening">
+          <h1>4:00PM October 23rd</h1>
+          <img src="/images/2022fall/daughter-rite-1978.jpeg" alt="DAUGHTER RITE still">
+          <h2>Daughter Rite (1978)  // What You Take For Granted (1983)</h2>
+          <h3>Michelle Citron &middot; 53min // 73min &middot; 16mm</h3>
+          <p>In her pseudo-documentary <i>Daughter Rite</i>, famed Chicago filmmaker Michelle Citron sets the precedent for the documentary genre that some stories become more truthful through narrative retelling. <i>What You Take For Granted</i> combines the real accounts of 40 working women into a fictive narrative about the friendship between two women in traditionally male jobs. A bona fide meta-film, Citron then splices in fictionalized interviews of other working women. </p>
+          <p><i style="color: red">With introduction and Q&A by director Michelle Citron!</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=2D9FC525-F170-4497-8907-F976A976F66D" target="_blank">here</a></u></b>.
+        </div>
+
+        <div class="screening">
+          <h1>8:00PM October 23rd</h1>
+          <img src="/images/2022fall/au-hasard-balthazar-1966.jpeg" alt="AU HASARD BALTHAZAR still">
+          <h2>Au Hasard Balthazar (1966)</h2>
+          <h3>Robert Bresson &middot; 95min &middot; 35mm</h3>
+          <p>Impossible as it is to name one film as Robert Bresson’s masterpiece, a compelling case can be made that the title should go to this monument of empathetic filmmaking. Following the titular donkey as he is passed from owner to owner, Bresson turns an unflinching eye to cruelties both big and small. Of all the wonders in this beautifully unadorned film, perhaps the most impressive is the “performance” of Balthazar—never has such a donkey seemed so human.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=0BB68652-4251-475F-B0C9-59BFBAD99FA3" target="_blank">here</a></u></b>.
         </div>
 
       </div>
