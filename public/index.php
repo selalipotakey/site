054b5c2e8ab1305986d0e35df9af892aeed94529
cameronpoe <cameronmpoe@gmail.com>
2022-11-29 12:55:53 -0600

    updated for week 9

--- public/index.php
+++ public/index.php
@@ -102,78 +102,75 @@
         </div>
 
         <div class="screening">
-          <h1>7:00PM Monday, November 14th</h1>
-          <img src="/images/2022fall/the-philadelphia-story-1940.jpeg" alt="The Philadelphia Story still">
-          <h2>The Philadelphia Story (1940)</h2>
-          <h3>George Cukor &middot; 112m &middot; Digital*</h3>
-          <p>After a string of unsuccessful films labeled her “box office poison,” Katherine Hepburn developed <i>The Philadelphia Story</i> as a comeback vehicle and was vindicated with a critical and commercial hit. Hepburn plays Tracy Lord, a Philadelphia socialite whose upcoming second marriage is drawing national attention. Jimmy Stewart and Cary Grant co-star as a tabloid journalist and Tracy’s first husband, each of whose arrival complicates her wedding plans.</p>
-          <p><i>*Note: previously billed as 35mm, but now digital due to print condition.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=D78E4E2C-671B-42BE-BD85-3E6FF654A3F8" target="_blank">here</a></u></b>.</p>
-        </div>
-
-
-        <div class="screening">
-          <h1>7:00PM Tuesday, November 15th</h1>
-          <img src="/images/2022fall/bitter-money-2016.jpg" alt="Bitter Money still">
-          <h2>Bitter Money (2016)</h2>
-          <h3>Wang Bing &middot; 163m &middot; DCP</h3>
-          <p>Pivoting from the legacy of forced labor in Maoist China, Wang Bing’s gaze now shifts toward present-day Huzhou, to the hundreds of thousands of migrants working 12 hours a day in its textile industry. Unflinching in its portrayal of poverty and the inescapable routines of industrialization, <i>Bitter Money</i> refuses to play into pornographic depictions of suffering. Instead, it is an emphathetic lens into the modern conditions of working-class China.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=676E37E3-A533-4A0B-A9E1-8EF888198FAA" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Monday, November 28th</h1>
+          <img src="/images/2022fall/his-girl-friday-1940.jpg" alt="His Girl Friday still">
+          <h2>His Girl Friday (1940)</h2>
+          <h3>Howard Hawks &middot; 92min &middot; 35mm</h3>
+          <p>Possibly <i>the</i> definitive screwball comedy, <i>His Girl Friday</i> stars Cary Grant and Rosalind Russell as newspaper editor Walter Burns and his star reporter Hildy Johnson…who also happen to be ex-spouses. When Hildy decides to remarry and quit the paper, Walter convinces her to cover one last story, even as he tries to win her back. With pitch-perfect performances from everyone involved, this movie is more than a laugh-a-minute—it’s a laugh-a-line.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=AC87112A-0AA5-4644-8A0F-E43BCC316BCA" target="_blank">here</a></u></b>.</p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Wednesday, November 16th</h1>
-          <img src="/images/2022fall/hero-2002.jpg" alt="Hero still">
-          <h2>Hero (2002)</h2>
-          <h3>Yimou Zhang &middot; 99m &middot; Digital*</h3>
-          <p>In one of her last roles before all but retiring from the screen, Maggie Cheung joins a stacked cast of Hong Kong’s most luminous stars—Jet Li, Zhang Ziyi, and frequent co-star Tony Leung—in Zhang Yimou’s balletic, billowing wuxia about the unification of China by Qin Shi Huang. Drenched in rich color and bursting with exquisitely choreographed action sequences, <i>Hero</i> may be the most beautiful piece of nationalist propaganda ever shot.</p>
-          <p><i>*Note: previously billed as DCP, but now digital.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=6E59C0B4-5EA0-4057-824F-5C4A3DFCD304" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Tuesday, November 29th</h1>
+          <img src="/images/2022fall/up-the-yangtze-2007.jpg" alt="Up the Yangtze still">
+          <h2>Up the Yangtze (2007)</h2>
+          <h3>Yung Chang &middot; 93m &middot; 35mm</h3>
+          <p>A quietly angry depiction of rural China’s shift towards a tourism-based consumer economy, <i>Up the Yangtze</i> serves as a stark reminder of North American complicity in the struggle of everyday Chinese citizens. Centered around a riverboat cruise ship catered towards English-speaking tourists, Yung Chang’s 2007 feature debut maintains a melancholic optimism for the lives of two ordinary people coming to terms with their place in a new China.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=A507A5B2-8E11-4785-B107-574C56975784" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Thursday, November 17th</h1>
-          <img src="/images/2022fall/strange-illusion-1945.jpg" alt="Strange Illusion still">
-          <h2>Strange Illusion (1945)</h2>
-          <h3>Edgar G. Ulmer &middot; 87m &middot; 35mm</h3>
-          <p>A noir retelling of "Hamlet" in Southern California, <i>Strange Illusion</i> is assuredly odd -- even for the seasoned cinephile. Expert cinematography brings the extended dream sequences to life, blanketing audiences in the eerie confusion of a black-and-white fever dream. Starring Jimmy Lydon, Sally Eilers, and Warren William (the so-called "King of Pre-Code"), <i>Strange Illusion</i> is yet another distinct classic in auteur director Edgar G. Ulmer's world. <u>Preserved by the Library of Congress</u>.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=2DCB2C3D-F6D2-4216-8971-D372317F25C6" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Wednesday, November 30th</h1>
+          <img src="/images/2022fall/executioners-1993.png" alt="Executioners still">
+          <h2>Executioners (1993)</h2>
+          <h3>Siu-Tung Ching, Johnnie To &middot; 97m &middot; DCP</h3>
+          <p>As much a dark reprise as it is a sequel to <i>The Heroic Trio, Executioners</i> finds our protagonists now navigating a post-apocalyptic city suffering from nuclear fallout and a scarce, dictator-controlled water supply. Zagging towards steampunk where the first film zigged towards Marvel, Johnnie To’s follow-up is a heavier affair that nonetheless features copious amounts of fisticuffs and Maggie Cheung-thrown grenades--how could you not be entertained?</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=626874F8-FF58-41A7-80A6-B7391EDD0ED2" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>9:30PM Thursday, November 17th</h1>
-          <img src="/images/2022fall/night-is-short-walk-on-girl-2017.jpg" alt="The Night Is Short, Walk on Girl still">
-          <h2>The Night Is Short, Walk on Girl (2017)</h2>
-          <h3>Masaaki Yuasa &middot; 93m &middot; DCP</h3>
-          <p>With an utterly surrealist style of Japanese animation painting an unforgettable drunken night, <i>The Night is Short, Walk on Girl</i> follows two unnamed students at Kyoto University. While the girl is enjoying a night full of parties, drinking contests, and supernatural beings, the guy is determined to find a successful way to ask her out. But will he be able to as he fights back strange characters, exorbitant drinks, and the common cold?</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=C386E6A3-43D6-4F03-8133-667FD21AFB57" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Thursday, December 1st</h1>
+          <img src="/images/2022fall/shes-the-man-2006.jpeg" alt="She's The Man still">
+          <h2>She's The Man (2006)</h2>
+          <h3>Andy Fickman &middot; 105m &middot; DCP</h3>
+          <p>Frustrated by the sexism of her coaches and peers, soccer player Viola (Amanda Bynes) finds an opportunity to prove them wrong by taking her twin brother's place at a new school. In disguise, Viola joins the boys’ team, develops a crush on the captain, Duke (Channing Tatum in his breakout role), and attracts the affections of Duke’s crush. <i>She’s the Man</i> transposes the delightfully messy love triangle of "Twelfth Night" onto a pitch-perfect teen rom-com.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=6D0B6EB3-6C2C-4AC3-BFAA-90D47077A25D" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Friday, November 18th</h1>
-          <img src="/images/2022fall/enter-the-void-2009.jpeg" alt="Enter the Void still">
-          <h2>Enter the Void (2009)</h2>
-          <h3>Gaspar Noé &middot; 143m &middot; 35mm</h3>
-          <p><i>Enter the Void</i> is Gaspar Noé's psychedelic afterlife movie from hell—a movie you should keep away from the children, the parents, and the dog. Shot entirely from the point of view of Oscar (Nathaniel Brown), an American drug dealer killed by Tokyo police in a drug bust, the film follows Oscar's soul as it journeys through past, present, and future seeking resurrection. An extreme, exhilarating movie, it's guaranteed to leave beads of sweat on your brow.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=EF88A4C0-1C23-4EE0-B5DA-35AC2537A5A0" target="_blank">here</a></u>.</b></p>
+          <h1>9:30PM Thursday, December 1st</h1>
+          <img src="/images/2022fall/wolfwalkers-2020.jpg" alt="Wolfwalkers still">
+          <h2>Wolfwalkers (2020)</h2>
+          <h3>Tomm Moore & Ross Stewart &middot; 103m &middot; DCP</h3>
+          <p>Set in Ireland in 1650, <i>Wolfwalkers</i> follows young Robyn Goodfellowe, who has moved to the town of Kilkenny with her dad at the request of the Lord Protector to kill the last remaining wolf pack. Outside the city walls, Robyn discovers the remaining wolves and meets Mebh the Wolfwalker, a girl who has the ability to turn into a wolf at night. While forging new friendships, Robyn has to decide whether to remain a hunter, or to help Mebh’s wolfpack.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=51DD4E18-0860-41E8-A292-EF8EE8E9766D" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00 Saturday, November 19th</h1>
-          <img src="/images/2022fall/rrr-2022.jpg" alt="RRR still">
-          <h2>RRR (2022)</h2>
-          <h3>S. S. Rajamouli &middot; 181m &middot; DCP</h3>
-          <p><i>RRR</i> is an exhilarating, action-packed spectacular mythologizing two real-life freedom fighters who helped lead India’s fight for independence from the British Raj: village guardian Komaram Bheem and Alluri Sitarama Raju, an ambitious Imperial officer. Set in the 1920s before their fight for India’s independence began, <i>RRR</i> imagines a fictional meeting between them, set into motion when a young Gond girl is stolen from her village by British soldiers. </p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=5BD37CB6-9E8F-4997-84E2-52B3835F480E" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Friday, December 2nd</h1>
+          <img src="/images/2022fall/tetsuo-the-iron-man-1989.jpeg" alt="Tetsuo: The Iron Man still">
+          <h2>Tetsuo: The Iron Man (1989)</h2>
+          <h3>Shinya Tsukamoto &middot; 67m &middot; DCP</h3>
+          <p>A homoerotic merger of Lynch's <i>Eraserhead</i> and the kaiju genre, director Shinya Tsukamoto's first feature is the epitome of cyberpunk body horror. <i>Tetsuo: The Iron Man</i> follows the "Salaryman" as he is thrust into a carnal, industrial nightmare, slowly turning into a metallic monster after being cursed by the "Metal Fetishist" he accidentally hits with his car. Grimy, grotesque, and grossly phallic, this is the film masochist's cult classic must-see.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=4A4BA023-A9C9-48DD-87A5-445D9799A643" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>5:00PM Sunday, November 20th</h1>
-          <img src="/images/2022fall/joan-the-maid-the-battles-1994.jpg" alt="JOAN THE MAID: THE BATTLES still">
-          <h2>Joan the Maid: The Battles (1994)</h2>
-          <h3>Jacques Rivette &middot; 160m &middot; DCP</h3>
-          <p>Countless films have been about Joan of Arc, but as critic Jonathan Rosenbaum noted, Rivette’s rigorously materialist version is “perhaps the only movie that offers a plausible portrait of what the 15th-century teenager who led the French into battle was actually like." In part one of this epic, young Joan (an extraordinary Sandrine Bonnaire) leaves her childhood home and persuades Charles VII to allow her to lead his troops in the war against the English.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=5AC926DC-9713-4662-B367-CF12BF89A3D3" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Saturday, December 3rd</h1>
+          <img src="/images/2022fall/autoerotica-2021.jpg" alt="AUTOEROTICA still">
+          <h2>Autoerótica (2021) — US Premiere</h2>
+          <h3>Andrea Hoyos &middot; 92m &middot; Digital</h3>
+          <p>Join us at Doc for the <b>US premiere</b> of <i>Autoerótica</i>! <i>Autoerótica</i> tells the story of Bruna, a teenage girl exploring her sexuality in modern-day Peru, and weaves the cringiest, true-to-life details of adolescence with quietly angry, biting social commentary. A tale of female friendship and the struggle for reproductive justice, Andrea Hoyos' directorial debut is a fiery work that marks the beginning of a new voice in contemporary Latin American cinema.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=058A6043-62F2-4893-A0F5-AA4C5039BC0A" target="_blank">here</a></u>.</b></p>
+        </div>
+
+        <div class="screening">
+          <h1>5:00PM Sunday, December 4th</h1>
+          <img src="/images/2022fall/joan-the-maid-the-prisons-1994.jpg" alt="JOAN THE MAID: THE PRISONS still">
+          <h2>Joan the Maid: The Prisons (1994)</h2>
+          <h3>Jacques Rivette &middot; 177min &middot; DCP</h3>
+          <p>Part two of Rivette’s epic chronicles Joan’s imprisonment and interrogation by the British, but notably omits her trial, which features prominently in other cinematic accounts of her life. As in part one, Rivette’s focus on quotidian details and the spaces between the great events of Joan's life humanizes her in ways that are profoundly moving. Rivette’s long takes and the beauty and austerity of his images illuminate the saint’s inner and outer lives..</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=37F01F9F-16C3-4DD3-8E9E-B8DA1A042611" target="_blank">here</a></u>.</b></p>
         </div>
 
       </div>
