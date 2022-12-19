479372b5a1c71220467c0345688389c8ad034db9
cameronpoe <cameronmpoe@gmail.com>
2022-12-19 17:27:34 -0600

    edits

--- database/dbcreds.php
+++ database/dbcreds.php
@@ -1,13 +1,6 @@
 <?php
-    
     require('../../vendor/autoload.php');
 
     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__) . '/../../ext_includes/');
     $dotenv->safeload();
-
-    // $db = $_ENV['DB'];
-    // $server = $_ENV['DB_SERVER'];
-    // $user = $_ENV['DB_USER'];
-    // $pass = $_ENV['DB_PASS'];
-
 ?>
