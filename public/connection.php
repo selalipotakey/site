15ebd45bae3fd42cae1ccc72aeb37f63fe289329
cameronpoe <cameronmpoe@gmail.com>
2022-12-19 17:18:30 -0600

    test

--- public/connection.php
+++ public/connection.php
@@ -4,6 +4,9 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '../database/dbcreds.php');
 
 echo $_SERVER['DOCUMENT_ROOT'];
 echo $_ENV['DB'];
+echo $_ENV['DB_SERVER'];
+echo $_ENV['DB_USER'];
+echo $_ENV['DB_PASS'];
 
 // $link = mysqli_connect($_ENV['DB'], $_ENV['DB_SERVER'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
