c0dd694c8675a49085e505529d61c398c05e8196
cameronpoe <cameronmpoe@gmail.com>
2022-12-19 17:03:16 -0600

    edits

--- database/dbcreds.php
+++ database/dbcreds.php
@@ -5,9 +5,9 @@
     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__) . '/../../ext_includes/');
     $dotenv->safeload();
 
-    $db = $_ENV['DB'];
-    $server = $_ENV['DB_SERVER'];
-    $user = $_ENV['DB_USER'];
-    $pass = $_ENV['DB_PASS'];
+    // $db = $_ENV['DB'];
+    // $server = $_ENV['DB_SERVER'];
+    // $user = $_ENV['DB_USER'];
+    // $pass = $_ENV['DB_PASS'];
 
 ?>
--- public/connection.php
+++ public/connection.php
@@ -2,7 +2,7 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '../database/dbcreds.php');
 
-$link = mysqli_connect($db, $server, $user, $pass);
+$link = mysqli_connect($_ENV['DB'], $_ENV['DB_SERVER'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 if (mysqli_connect_errno()) {
    die("Connect failed: %s\n" + mysqli_connect_error());
