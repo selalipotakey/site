93abf985605cf0d808bf863db780eb23739e6a32
cameronpoe <cameronmpoe@gmail.com>
2022-12-19 17:10:49 -0600

    test

--- public/connection.php
+++ public/connection.php
@@ -2,10 +2,12 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '../database/dbcreds.php');
 
-$link = mysqli_connect($_ENV['DB'], $_ENV['DB_SERVER'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+echo 'hello world';
 
-if (mysqli_connect_errno()) {
-   die("Connect failed: %s\n" + mysqli_connect_error());
-   exit();
-}
+// $link = mysqli_connect($_ENV['DB'], $_ENV['DB_SERVER'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+
+// if (mysqli_connect_errno()) {
+//    die("Connect failed: %s\n" + mysqli_connect_error());
+//    exit();
+// }
 ?>
