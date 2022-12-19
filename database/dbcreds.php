c5054085ebaa7d9527887542877ec1ca3a0a30e0
cameronpoe <cameronmpoe@gmail.com>
2022-12-19 17:46:31 -0600

    edit

--- database/dbcreds.php
+++ database/dbcreds.php
@@ -1,5 +1,5 @@
 <?php
-    require('../../vendor/autoload.php');
+    require dirname(__FILE__) . '/../../vendor/autoload.php';
 
     $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__) . '/../../ext_includes/');
     $dotenv->safeload();
