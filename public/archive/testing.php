6884cf8e962e899b0bcda96c1fc4e0eaea9cd587
cameronpoe <cameronmpoe@gmail.com>
2022-12-28 21:42:54 -0600

    updated testing page to allow for search  director

--- public/archive/testing.php
+++ public/archive/testing.php
@@ -16,17 +16,42 @@
           box-sizing: border-box;
           height: 3.5rem;
           width: 70%;
-          border: 1px solid #000000;
+          border: .25rem solid #000000;
+          border-right: .125rem solid #000000;
           border-radius: 0px;
           background-color: #ffffff;
         }
         .database-query input[type=text]:focus {
           outline: none;
-          background-color: #f8f8f8;
+          background-color: #f0f0f0;
         }
-        .dtabase-query input[type=submit] {
-          margin-right: 0;
+        .database-query select {
+          height: 3.5rem;
+          width: 20%;
+          border: .25rem solid #000000;
+          border-left: .125rem solid #000000;
+          border-right: .125rem solid #000000;
+          border-radius: 0rem;
         }
+        .database-query select:focus {
+          outline: none;
+          background-color: #f0f0f0;
+        }
+        .database-query input[type=submit] {
+          height: 3.5rem;
+          width: 10%;
+          background-color: #ffffff;
+          border: .25rem solid #000000;
+          border-left: .125rem solid #000000;
+        }
+        .database-query input[type=submit]:hover, input[type=submit]:focus {
+          outline: none;
+          background-color: #f0f0f0;
+        } input[type=submit]:hover {
+          cursor: pointer;
+        }
+
+
 
         ul.query-results {
             list-style: none;
@@ -37,7 +62,6 @@
           width: 100%;
           border-collapse: collapse;
           margin-bottom: 2rem;
-          /* border: .3rem solid #000000; */
         }
         table.query-results-films {
           border: .25rem solid #000000;
@@ -49,17 +73,20 @@
         .query-results-films thead th:nth-child(1) {
           width: 30%;
         }
-
         thead th:nth-child(2) {
-          width: 20%;
+          width: 7%;
         }
-
         thead th:nth-child(3) {
-          width: 15%;
+          width: 17%;
         }
-
         thead th:nth-child(4) {
-          width: 35%;
+          width: 17%;
+        }
+        thead th:nth-child(5) {
+          width: 8%;
+        }
+        thead th:nth-child(6) {
+          width: 20%;
         }
 
         th, td {
@@ -94,40 +121,64 @@
         <form class="database-query" method="GET">
           <input type="text" name="simple-query"/>
           <select name="simple-field" id="simple-search-select">
-            <option value="films.title">Film Title</option>
-            <!-- <option value="directors.name">Director</option> -->
+            <option value="films.title" <?php if ($_GET['simple-field']=="films.title") {echo "selected='selected'"; } ?>>Film Title</option>
+            <option value="directors.name" <?php if ($_GET['simple-field']=="directors.name") {echo "selected='selected'"; } ?>>Director</option>
           </select>
           <input type="submit" value="Search"/>
         </form>
 
         <?php
+            function load_simple_query($mysqli_object, $sql_query, $query_value) {
+              $stmt = $mysqli_object->prepare($sql_query);
+              $stmt->bind_param('s', $query_value);
+              $msc = microtime(true);
+              $stmt->execute();
+              $msc = round(microtime(true)-$msc, 3);
+              $result = $stmt->get_result();
+              $stmt->close();
+              $result_num_rows = $result->num_rows;
+              echo "<p>Fetched {$result_num_rows} result(s) in {$msc} seconds.</p>";
+              return array($result, $result_num_rows);
+            }
+            function format_director($row_director) {
+              $director_array = explode(', ', $row_director);
+              echo $director_array[0];
+              $i = 1;
+              while ($i < count($director_array) - 1) {
+                echo ", " . $director_array[$i];
+                $i++;
+              }
+              if (count($director_array) > 1) {
+                echo " and " . end($director_array);
+              }
+              return NULL;
+            }
+
+            function format_showdate($row_showdate) {
+              $showdate_array = explode(', ', $row_showdate);
+              echo date("m/d/y", strtotime($showdate_array[0]));
+              $i = 1;
+              while ($i < count($showdate_array) - 1) {
+                echo ", " . date("m/d/y", strtotime($showdate_array[$i]));
+                $i++;
+              }
+              if (count($showdate_array) > 1) {
+                echo " and " . date("m/d/y", strtotime(end($showdate_array)));
+              }
+              return NULL;
+            }
+
             require_once $_SERVER['DOCUMENT_ROOT'] . '/connection.php';
             if ($_GET) {
                 $simple_field = $_GET['simple-field'];
-                $simple_table = explode('.', $simple_field)[0];
                 $simple_query = "%{$_GET['simple-query']}%";
 
                 
                 if ($simple_field == "films.title") {
-                    $film_listing_query = 'SELECT `films`.`title` AS film_title, 
-                      GROUP_CONCAT(DISTINCT `directors`.`name` SEPARATOR ", ") AS director, 
-                      GROUP_CONCAT(DISTINCT `times`.`showdate` ORDER BY `times`.`showdate` ASC SEPARATOR ", ") AS showdate, 
-                      `series`.`name` AS series_name, 
-                      `screenings`.`id` AS screening_id 
-                    FROM `films` 
-                    INNER JOIN `films_directors` ON `films`.`id` = `films_directors`.`films_id` 
-                    INNER JOIN `directors` ON `films_directors`.`directors_id` = `directors`.`id` 
-                    INNER JOIN `instances` ON `films`.`id` = `instances`.`films_id` 
-                    INNER JOIN `screenings` ON `instances`.`screenings_id` = `screenings`.`id` 
-                    INNER JOIN `series` ON `screenings`.`series_id` = `series`.`id` 
-                    INNER JOIN `times` ON `screenings`.`id` = `times`.`screenings_id` 
-                    WHERE `films`.`title` COLLATE utf8mb4_general_ci LIKE ? 
-                    GROUP BY `screenings`.`id` 
-                    ORDER BY `times`.`showdate` DESC;';
-
                     $film_listing_query = 'SELECT * FROM
                       (SELECT `films`.`title` as film_title,
                         `films`.`id` as film_id,
+                        `films`.`releaseyear` as release_year,
                         GROUP_CONCAT(DISTINCT `directors`.`name` SEPARATOR ", ") AS director
                       FROM `films`
                       INNER JOIN `films_directors` ON `films`.`id` = `films_directors`.`films_id` 
@@ -137,6 +188,7 @@
 
                     INNER JOIN 
                       (SELECT `instances`.`films_id` as film_id,
+                        `instances`.`format` as format,
                         `instances`.`screenings_id` as screening_id
                       FROM `instances`) t2
                     ON t1.film_id = t2.film_id
@@ -156,23 +208,17 @@
                     
                     ;';
 
+                    list($result_films, $result_films_num_rows) = load_simple_query($mysqli, $film_listing_query, $simple_query);
 
-                    $stmt_films = $mysqli->prepare($film_listing_query);
-                    $stmt_films->bind_param('s', $simple_query);
-                    $msc = microtime(true);
-                    $stmt_films->execute();
-                    $msc = round(microtime(true)-$msc, 3);
-                    $result_films = $stmt_films->get_result();
-                    $stmt_films->close();
-                    $result_films_num_rows = mysqli_num_rows($result_films);
-                    echo "<p>Fetched {$result_films_num_rows} result(s) in {$msc} seconds.</p>";
                     if ($result_films_num_rows > 0) {
                         echo '<table class="query-results-films">
                         <thead>
                           <tr>
                             <th scope="col">Film Title</th>
-                            <th scope="col">Director(s)</th>
-                            <th scope="col">Screening Date(s)</th>
+                            <th scope="col">Release Year</th>
+                            <th scope="col">Director</th>
+                            <th scope="col">Screening Date</th>
+                            <th scope="col">Format</th>
                             <th scope="col">Series</th>
                           </tr>
                         </thead>
@@ -180,35 +226,92 @@
                         while ($row = mysqli_fetch_assoc($result_films)) {
                             echo '<tr>
                               <td>' . $row['film_title'] . '</td>
+                              <td>' . $row['release_year'] . '</td>
                               <td>';
                               
-                              $director_array = explode(', ', $row['director']);
-                              echo $director_array[0];
-                              $i = 1;
-                              while ($i < count($director_array) - 1) {
-                                echo ", " . $director_array[$i];
-                                $i++;
-                              }
-                              if (count($director_array) > 1) {
-                                echo " and " . end($director_array);
-                              }
+                              format_director($row['director']);
 
                               echo '</td>
                               <td>';
 
-                              // formats times to make them look pretty
-                              $showdate_array = explode(', ', $row['showdate']);
-                              echo date("m/d/y", strtotime($showdate_array[0]));
-                              $i = 1;
-                              while ($i < count($showdate_array) - 1) {
-                                echo ", " . date("m/d/y", strtotime($showdate_array[$i]));
-                                $i++;
-                              }
-                              if (count($showdate_array) > 1) {
-                                echo " and " . date("m/d/y", strtotime(end($showdate_array)));
-                              }
+                              format_showdate($row['showdate']);
 
                               echo '</td>
+                              <td>' . $row['format'] . '</td>
+                              <td>';
+                              
+                              echo $row['series_name'];
+                              
+                              echo '</td>
+                            </tr>';
+                        }
+                        echo '</tbody>
+                        </table>';
+                    }
+                } elseif ($simple_field == "directors.name") {
+                  $director_listing_query = 'SELECT * FROM
+                      (SELECT `films`.`title` as film_title,
+                        `films`.`id` as film_id,
+                        `films`.`releaseyear` as release_year,
+                        GROUP_CONCAT(DISTINCT `directors`.`name` SEPARATOR ", ") AS director
+                      FROM `films`
+                      INNER JOIN `films_directors` ON `films`.`id` = `films_directors`.`films_id` 
+                      INNER JOIN `directors` ON `films_directors`.`directors_id` = `directors`.`id`
+                      WHERE `directors`.`name` COLLATE utf8mb4_general_ci LIKE ? 
+                      GROUP BY `films`.`id`) t1  
+
+                    INNER JOIN 
+                      (SELECT `instances`.`films_id` as film_id,
+                        `instances`.`format` as format,
+                        `instances`.`screenings_id` as screening_id
+                      FROM `instances`) t2
+                    ON t1.film_id = t2.film_id
+
+                    INNER JOIN
+                      (SELECT
+                        `series`.`name` AS series_name, 
+                        `screenings`.`id` AS screening_id,
+                        GROUP_CONCAT(DISTINCT `times`.`showdate` ORDER BY `times`.`showdate` ASC SEPARATOR ", ") AS showdate
+                      FROM `screenings`
+                      INNER JOIN `series` ON `screenings`.`series_id` = `series`.`id` 
+                      INNER JOIN `times` ON `screenings`.`id` = `times`.`screenings_id`
+                      GROUP BY `screenings`.`id`) t3
+                    ON t2.screening_id = t3.screening_id 
+                    
+                    ORDER BY showdate DESC
+                    
+                    ;';
+
+                    list($result_directors, $result_directors_num_rows) = load_simple_query($mysqli, $director_listing_query, $simple_query);
+
+                    if ($result_directors_num_rows > 0) {
+                        echo '<table class="query-results-films">
+                        <thead>
+                          <tr>
+                            <th scope="col">Film Title</th>
+                            <th scope="col">Release Year</th>
+                            <th scope="col">Director</th>
+                            <th scope="col">Screening Date</th>
+                            <th scope="col">Format</th>
+                            <th scope="col">Series</th>
+                          </tr>
+                        </thead>
+                        <tbody>';
+                        while ($row = mysqli_fetch_assoc($result_directors)) {
+                            echo '<tr>
+                              <td>' . $row['film_title'] . '</td>
+                              <td>' . $row['release_year'] . '</td>
+                              <td>';
+                              
+                              format_director($row['director']);
+
+                              echo '</td>
+                              <td>';
+
+                              format_showdate($row['showdate']);
+
+                              echo '</td>
+                              <td>' . $row['format'] . '</td>
                               <td>';
                               
                               echo $row['series_name'];
@@ -221,26 +324,6 @@
                     }
                 }
 
-                // } elseif ($simple_field == "directors.name") {
-                //     $base_query = $base_query . " INNER JOIN films_directors ON directors.id = films_directors.directors_id INNER JOIN films on films_directors.films_id = films.id";
-                // }
-
-                // $stmt_films = $mysqli->prepare($base_query.";");
-                // $stmt_films->bind_param('s', $simple_query);
-                // $msc = microtime(true);
-                // $stmt_films->execute();
-                // $result_films = $stmt_films->get_result();
-                // $stmt_films->close();
-                // $msc = microtime(true)-$msc;
-                // echo $msc . ' s'; // in seconds
-                // echo ($msc * 1000) . ' ms'; // in millseconds
-                // $result_films_num_rows = mysqli_num_rows($result_films);
-                // if ($result_films_num_rows > 0) {
-                //     while ($row = mysqli_fetch_assoc($result_films)) {
-                //         // echo $row['name'] . '<br>';
-                //         echo implode(" ", $row) . '<br>';
-                //     }
-                // }
             }
         ?>
 
