ac1502fede9acd603e3501eb1103fb9b3ec59288
cameronpoe <cameronmpoe@gmail.com>
2022-12-30 12:19:55 -0600

    fixed images loading slow (removed them)

--- public/archive/2022-series/index.php
+++ public/archive/2022-series/index.php
@@ -23,7 +23,6 @@
         <div class="series-list">
 
           <div class="series-tile">
-            <a href="/archive/2022-series/winter/monday"><img src="/images/2022winter/series_stills/harlan-county-usa-1976.png" alt="HARLAN COUNTY USA still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/winter/monday">
                 <p class="day">Monday</p>
@@ -34,7 +33,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/winter/tuesday"><img src="/images/2022winter/series_stills/the-connection-1962.jpeg" alt="THE CONNECTION still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/winter/tuesday">
                 <p class="day">Tuesday</p>
@@ -45,7 +43,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/winter/wednesday"><img src="/images/2022winter/series_stills/dracula-pages-from-a-virgins-diary-2002.jpg" alt="DRACULA PAGES FROM A VIRGINS DIARY still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/winter/wednesday">
                 <p class="day">Wednesday</p>
@@ -56,7 +53,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/winter/thursday-1"><img src="/images/2022winter/series_stills/raising-arizona-1987.jpg" alt="RAISING ARIZONA still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/winter/thursday-1">
                 <p class="day">Thursday I</p>
@@ -67,7 +63,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/winter/thursday-2"><img src="/images/2022winter/series_stills/the-bird-people-in-china-1998.jpg" alt="THE BIRD PEOPLE IN CHINA still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/winter/thursday-2">
                 <p class="day">Thursday II</p>
@@ -78,7 +73,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/winter/friday"><img src="/images/2022winter/series_stills/a-flower-in-hell-1958.jpeg" alt="A FLOWER IN HELL still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/winter/friday">
                 <p class="day">Friday</p>
@@ -89,7 +83,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/winter/saturday"><img src="/images/2022winter/series_stills/the-french-dispatch-2021.jpg" alt="THE FRENCH DISPATCH still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/winter/saturday">
                 <p class="day">Saturday</p>
@@ -99,7 +92,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/winter/sunday"><img src="/images/2022winter/series_stills/india-song-1975.jpg" alt="INDIA SONG still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/winter/sunday">
                 <p class="day">Sunday</p>
@@ -111,12 +103,13 @@
 
         </div>
 
+        <br><br><br><br><br><br>
+
         <h1><u>Spring 2022</u></h1>
 
         <div class="series-list">
 
           <div class="series-tile">
-            <a href="/archive/2022-series/spring/special-events"><img src="/images/2022spring/series_stills/house-1977.jpg" alt="HOUSE still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/special-events">
                 <p class="title">Special Events</p>
@@ -125,7 +118,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/spring/monday"><img src="/images/2022spring/series_stills/monangambee-1968.jpeg" alt="MONANGAMBEE still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/monday">
                 <p class="day">Monday</p>
@@ -136,7 +128,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/spring/tuesday"><img src="/images/2022spring/series_stills/one-false-move-1992.jpeg" alt="ONE FALSE MOVE still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/tuesday">
                 <p class="day">Tuesday</p>
@@ -147,7 +138,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/spring/wednesday"><img src="/images/2022spring/series_stills/heaven-can-wait-1978.jpeg" alt="HEAVEN CAN WAIT still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/wednesday">
                 <p class="day">Wednesday</p>
@@ -158,7 +148,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/spring/thursday-1"><img src="/images/2022spring/series_stills/wag-the-dog-1997.jpg" alt="WAG THE DOG still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/thursday-1">
                 <p class="day">Thursday I</p>
@@ -169,7 +158,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/spring/thursday-2"><img src="/images/2022spring/series_stills/mod-fuck-explosion-1994.png" alt="MOD FUCK EXPLOSION still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/thursday-2">
                 <p class="day">Thursday II</p>
@@ -180,7 +168,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/spring/friday"><img src="/images/2022spring/series_stills/fallen-angels-1995.jpeg" alt="FALLEN ANGELS still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/friday">
                 <p class="day">Friday</p>
@@ -191,7 +178,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/spring/saturday"><img src="/images/2022spring/series_stills/everything-everywhere-all-at-once-2022.jpg" alt="EVERYTHING EVERYWHERE ALL AT ONCE still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/saturday">
                 <p class="day">Saturday</p>
@@ -201,7 +187,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/spring/sunday"><img src="/images/2022spring/series_stills/big-night-1996.jpg" alt="BIG NIGHT still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/spring/sunday">
                 <p class="day">Sunday</p>
@@ -220,7 +205,6 @@
         <div class="series-list">
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/summer/special-events"><img src="/images/2022summer/series_stills/spinning-home-movies-time-travelin.png" alt="SPINNING HOME MOVIES still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/summer/special-events">
                 <p class="title">Special Events</p>
@@ -229,7 +213,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/summer/thursday"><img src="/images/2022summer/shadow-of-a-doubt-1943.jpg" alt="SHADOW OF A DOUBT still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/summer/thursday">
                 <p class="day">Thursday</p>
@@ -240,7 +223,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/summer/friday-1"><img src="/images/2022summer/seconds-1966.jpg" alt="SECONDS still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/summer/friday-1">
                 <p class="day">Friday I</p>
@@ -259,7 +241,6 @@
         <div class="series-list">
 
           <div class="series-tile">
-            <a href="/archive/2022-series/fall/90th"><img src="/images/2022fall/funeral-parade-of-roses-1969.jpeg" alt="FUNERAL PARADE OF ROSES still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/90th">
                 <p class="title">Doc90: 90 Years of Doc</p>
@@ -268,7 +249,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/fall/special-events"><img src="/images/2022fall/series_stills/long-days-journey-into-night-2018.jpg" alt="LONG DAYS JOURNEY INTO NIGHT still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/special-events">
                 <p class="title">Special Events</p>
@@ -277,7 +257,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/fall/monday"><img src="/images/2022fall/series_stills/whats-up-doc-1972.jpeg" alt="WHAT'S UP DOC still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/monday">
                 <p class="day">Monday</p>
@@ -288,7 +267,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/fall/tuesday"><img src="/images/2022fall/wolf-warrior-2-2017.jpg" alt="WOLF WARRIOR 2 still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/tuesday">
                 <p class="day">Tuesday</p>
@@ -299,7 +277,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/fall/wednesday"><img src="/images/2022fall/series_stills/irma-vep-1996.jpg" alt="IRMA VEP still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/wednesday">
                 <p class="day">Wednesday</p>
@@ -310,7 +287,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/fall/thursday-1"><img src="/images/2022fall/series_stills/chimes-at-midnight-1966.jpg" alt="CHIMES AT MIDNIGHT still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/thursday-1">
                 <p class="day">Thursday I</p>
@@ -321,7 +297,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/fall/thursday-2"><img src="/images/2022fall/series_stills/belladonna-of-sadness-1973.jpg" alt="BELLADONNA OF SADNESS still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/thursday-2">
                 <p class="day">Thursday II</p>
@@ -332,7 +307,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/fall/friday"><img src="/images/2022fall/series_stills/dogtooth-2009.jpg" alt="DOGTOOTH still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/friday">
                 <p class="day">Friday</p>
@@ -343,7 +317,6 @@
           </div>
 
           <div class="series-tile">
-            <a href="/archive/2022-series/fall/saturday"><img src="/images/2022fall/series_stills/top-gun-maverick-2022.png" alt="TOP GUN MAVERICK still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/saturday">
                 <p class="day">Saturday</p>
@@ -353,7 +326,6 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/archive/2022-series/fall/sunday"><img src="/images/2022fall/series_stills/the-nun-1966.jpg" alt="THE NUN still"></a>
             <div class="series-text">
               <a href="/archive/2022-series/fall/sunday">
                 <p class="day">Sunday</p>
