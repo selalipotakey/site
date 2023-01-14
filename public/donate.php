7f94b33cd1902e9e6fbf31b263de285d0bbe6f88
cameronpoe <cameronmpoe@gmail.com>
2023-01-14 16:47:59 -0600

    added digital cinema upgrades

--- public/donate.php
+++ public/donate.php
@@ -16,6 +16,24 @@
       <?php $version='1.2'; include "./includes/dropdown.html" ?>
 
       <div class="information">
+        <h1>Digital Cinema Upgrades</h1>
+
+        <p>Many things have changed since the Max Palevsky Cinema opened its doors in 1986. Over those 36 years, we’ve seen the rise of digital projection and exponential improvements in cinema technology. Our last major cinema upgrade was the installation of our current digital projector 10 years ago, and critical equipment like our DCP server is reaching the end of its life. To ensure that we can continue our screenings at a low cost, we hope to finance capital improvements that will have tangible benefits both for our patrons and for those that work with us.</p>
+
+        <br>
+ 
+        <p>We are hoping to raise $60,000 in donations to help accomplish the following: </p>
+
+        <br>
+        <ol>
+          <li>Replace our DCP server with a newer model to extend the life of equipment used for new releases and digital films unavailable on celluloid.</li><br>
+          <li>Upgrade our central audio processor to improve sound quality and surround sound in the cinema.</li><br>
+          <li>Incorporate a control system and panel for our digital projector in the projection booth to streamline shows for our projectionists.</li><br>
+        </ol>
+
+        <p>Doc’s mission is to preserve our theater as a place of discovery and delight, and to bring our audiences, from the casual moviegoer to the hardcore cinephile, the best cinematic experience possible. With the help of your generous contributions, Doc will be able to bring the unparalleled experience of cinema to Chicago audiences for many years to come.</p>
+
+
         <h1>Donating</h1>
         <p>Doc Films is a <a href="/about/"><u>student-led, volunteer-run</u></a> movie theater, film society, and non-profit at the University of Chicago. Our celluloid-first commitment to showing highly varied, highly repertory cinema often leaves us with fewer profits than giant theater chains.</p>
         <br>
