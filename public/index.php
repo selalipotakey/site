89e1c026be376b33b5d9723ce1119d5ecce8415b
cameronpoe <cameronmpoe@gmail.com>
2023-01-16 13:58:25 -0600

    week 3

--- public/index.php
+++ public/index.php
@@ -100,77 +100,92 @@
           <h3><i>$7 tickets, theater opens 30 minutes before the screening.</i></h3>
         </div>
 
+        <h3>Note: there is no screening on Monday 1/16.</h3>
+
+        <br>
+
         <div class="screening">
-          <h1>7:00PM Monday, January 9th</h1>
-          <img src="/images/2023winter/ladies-of-leisure-1930.jpg" alt="Ladies of Leisure (1930) still">
-          <h2>Ladies of Leisure (1930)</h2>
-          <h3>Frank Capra &middot; 99m &middot; 35mm</h3>
-          <p>It’s difficult to believe that Barbara Stanwyck was only 23 when she starred in <i>Ladies of Leisure</i>, as she brings a worldly assuredness to Kay, a gold-digging party girl who enters a relationship of convenience with an artistic scion (Lowell Sherman) that soon turns into something more. A risqué pre-Code outing with heart to spare, the mature romance was the first of five creative collaborations between Stanwyck and the great Frank Capra.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=81B51BA1-1C98-42D2-8DC0-E1694FB86B21" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Tuesday, January 17th</h1>
+          <img src="/images/2023winter/chan-is-missing-1982.jpg" alt="Chan Is Missing (1982) still">
+          <h2>Chan Is Missing (1982)</h2>
+          <h3>Wayne Wang &middot; 76m &middot; DCP</h3>
+          <p>"<i>Chan is Missing</i> announced that Asian Americans could be artists," remembers scholar Peter Feng. An audacious mix of noir and documentary, it follows a manhunt for Chan Hung, who becomes a stand-in for Chinese American identity itself, as no one can agree who he is, or was. Chan is never found: to be Asian American is not to inhabit a single preexisting identity, but to negotiate a new, unstable one, forging solidarities where they didn't exist before.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=6774F813-F024-4B70-8D38-9DCADB4D5FEC" target="_blank">here</a></u>.</b></p>
         </div>
         
-        <div class="screening">
-          <h1>7:00PM Tuesday, January 10th</h1>
-          <img src="/images/2023winter/history-and-memory-1991.png" alt="History and Memory: For Akiko and Takashige (1991) // Fresh Kill (1994) still">
-          <h2>History and Memory: For Akiko and Takashige (1991) // Fresh Kill (1994)</h2>
-          <h3>Rea Tajiri // Shu Lea Cheang &middot; 32m // 80m &middot; DCP</h3>
-          <p>These videos adopt poststructuralist doubts about objective truth, but end up with divergent results. <i>History and Memory</i> layers materials from both found and reconstructed sources to examine Japanese internment and its aftermath on a family. <i>Fresh Kill</i> is a bewilderingly fun "eco cyber noia": there's a South Asian lesbian who lives in garbage, a Chinese hacker and chef who serves radioactive sushi, and a Black "satellite dishwasher" raving about birds.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=5744C407-F8C7-4617-AE3F-9123AAACF50C" target="_blank">here</a></u>.</b></p>
+        <div class="screening" id="close" style="scroll-margin-top: var(--header-height);">
+          <h1>6:00PM Wednesday, January 18th</h1>
+          <img src="/images/2023winter/close-2022.jpg" alt="CLOSE still">
+          <h2>Close (2022)</h2>
+          <h3>Lukas Dhont &middot; 105m &middot; DCP</h3>
+          <p>His first film after his feature debut and Cannes Caméra d'Or-winning <i>Girl</i>, Lukas Dhont's <i>Close</i> stars Eden Dambrine and Gustav de Waele as two young teen boys, Léo and Rémi. Partially a retelling of Dhont's own childhood experiences, Léo and Rémi must navigate their relationship under new and unwanted scrutiny after returning to their normal lives following an idyllic summer together. Dhont took home the Grand Prix at Cannes for this film.</p>
+          <p><i>Free pre-release screening! Followed by discussion and Q+A with director Lukas Dhont moderated by CMST Professor Aurore Spiers.</i></p>
+          <p><b>200 tickets can be reserved online <u><a href="https://forms.gle/q9DWFNfJ4bsrRZueA" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Wednesday, January 11th</h1>
-          <img src="/images/2023winter/boudu-saved-from-drowning-1932.jpeg" alt="Boudu Saved from Drowning (1932) still">
-          <h2>Boudu Saved from Drowning (1932)</h2>
-          <h3>Jean Renoir &middot; 85m &middot; DCP</h3>
-          <p>Michel Simon is gloriously anti-social, dirty, charming, and undomesticated as Boudu, the tramp whom the bookseller Lestingois rescues from drowning. When he brings Boudu home and tries to civilize him, the wild man instead lusts after Mrs. Lestingois and the maid, and otherwise brings chaos into this middle-class household. It's a rich satire that contrasts Boudu's instinctive anarchism with French bourgeois values—oh, and it's funny, too.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=85296884-C253-4290-A7FA-F8E7808E57AD" target="_blank">here</a></u>.</b></p>
+          <h1><u>9:30PM</u> Wednesday, January 18th</h1>
+          <img src="/images/2023winter/toni-1936.jpg" alt="Toni (1935) still">
+          <h2>Toni (1935)</h2>
+          <h3>Jean Renoir &middot; 82m &middot; DCP</h3>
+          <p>In the midst of the Great Depression, Renoir used a true crime story and non-professional actors to show the plight of migrant workers as they struggled in the south of France. Toni is a laborer from Italy loved by his landlady, but he becomes obsessed with Josepha, a new migrant worker from Spain. Josepha then falls in love with a Frenchman, and the conflict of wills leads to inevitable tragedy. Uniquely for 1935, <i>Toni</i>  was entirely filmed on location.</p>
+          <p><i style="color: red;">Note: originally billed at 7:00PM</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=189F8CCC-AA82-4E91-99D5-CCB612AE7E04" target="_blank">here</a></u>.</b></p>
+        </div>
+
+        <div class="screening" id="close" style="scroll-margin-top: var(--header-height);">
+          <h1>4:30PM Thursday, January 19th</h1>
+          <img src="/images/2023winter/a-touch-of-spice-2003.jpg" alt="A TOUCH OF SPICE still">
+          <h2>A Touch of Spice (2003)</h2>
+          <h3>Tassos Boulmetis &middot; 108m &middot; DCP</h3>
+          <p>A Greek family living in Istanbul is broken apart when they are deported from Turkey; the family's young son, Fanis, struggles to adapt to life in Greece and takes refuge in cooking, as it is his only connection to his grandfather and his homeland. The film's original title, <i>Politiki Kouzina</i>, can either be translated as "Cuisine of the City" (referring to Constantinople) or as "Political Cuisine," reflecting the dual themes underpinning this film.</p>
+          <p><i>Sponsored by the UChicago Hellenic Students Association (HSA).</i></p>
+          <!-- <p><b>Tickets can be bought <u><a href="https://forms.gle/q9DWFNfJ4bsrRZueA" target="_blank">here</a></u>.</b></p> -->
         </div>
 
         <div class="screening">
-          <h1>7:00PM Thursday, January 12th</h1>
-          <img src="/images/2023winter/hiroshima-1953.jpg" alt="Hiroshima (1953) still">
-          <h2>Hiroshima (1953)</h2>
-          <h3>Hideo Sekigawa &middot; 104m &middot; DCP</h3>
-          <p>Set during the immediate years after August 6th, 1945, <i>Hiroshima</i> portrays a classroom in their attempt to return to normalcy after the atom bomb destroyed their city, ravaged not just by radiation poisoning but also reckless gambling and discrimination against scarred victims. Featuring hundreds of Hiroshima survivors within its cast, the film portrays a dramatic yet accurate recreation of one of the 20th century’s most infamous tragedies.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=6205D2D2-0DFB-4337-BE46-C8A4375967FD" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Thursday, January 19th</h1>
+          <img src="/images/2023winter/winter-light-1963.jpg" alt="Winter Light (1963) still">
+          <h2>Winter Light (1963)</h2>
+          <h3>Ingmar Bergman &middot; 81m &middot; DCP</h3>
+          <p>The second film of Ingmar Berman’s “spiritual” trilogy, <i>Winter Light</i> focuses on Tomas Ericsson, a pastor stricken with a personal crisis in faith, as he delivers a service for a small congregation including a fisherman debilitatingly frightened by nuclear war to Ericsson’s atheist ex-mistress. Profound in its depiction of existentialism and faith on an individual scale, the film harshly ponders God’s absence, silence, and love for His creations.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=672A26B1-10E7-4086-B27F-4E61769A86BB" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>9:30PM Thursday, January 12th</h1>
-          <img src="/images/2023winter/bamboozled-2000.jpeg" alt="Bamboozled (2000) still">
-          <h2>Bamboozled (2000)</h2>
-          <h3>Spike Lee &middot; 135m &middot; 35mm</h3>
-          <p>In Spike Lee’s fierce satire of mass media, Pierre Delacroix (Damon Wayans), an Ivy-educated network television writer, creates a modern-day minstrel show complete with African American actors donning blackface. Though commercially unsuccessful upon its release, <i>Bamboozled</i> has become a cult favorite within the director’s oeuvre, with critic Ashley Clark calling it “the central film in Lee’s canon—the house on fire to which all roads lead.”</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=301350D8-27DF-4A45-AC37-59FDB98B8014" target="_blank">here</a></u>.</b></p>
+          <h1>9:30PM Thursday, January 19th</h1>
+          <img src="/images/2023winter/the-forest-for-the-trees-2003.jpeg" alt="The Forest for the Trees (2003) still">
+          <h2>The Forest for the Trees (2003)</h2>
+          <h3>Maren Ade &middot; 81m &middot; 35mm</h3>
+          <p>Long before writer/director Maren Ade rocketed to international acclaim with the comedy <i>Toni Erdmann</i>, she showed a talent for finding humor and psychological depth in the drudgery of everyday life. Her directorial debut <i>The Forest for the Trees</i> focuses on the struggles of an awkward young teacher (Eva Löbau) starting over in a new town. The distinctly lo-fi video aesthetic highlights the growing desperation within Löbrau’s subtle performance.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=7A4460FA-5DBA-4A62-8007-02846683186C" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Friday, January 13th</h1>
-          <img src="/images/2023winter/magnolia-1999.jpeg" alt="Magnolia (1999) still">
-          <h2>Magnolia (1999)</h2>
-          <h3>Paul Thomas Anderson &middot; 183m &middot; Digital</h3>
-          <p>PTA’s <i>Magnolia</i> is a varied tapestry of interwoven stories, each following a different resident of Los Angeles’s San Fernando Valley, and slowly coming together as the story unfolds. A marathon of unearthed secrets, family drama, and unrequited desire—with a final twist that feels almost like divine intervention—this is a film that shines through both the stellar performances of its ensemble cast and the tense relationships that Anderson constructs.</p>
-          <p><i>Note: previously billed as 35mm, but changed to digital.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=9FB26EB5-EE19-4458-A256-9E5E5388D8AD" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Friday, January 20th</h1>
+          <img src="/images/2023winter/love-liza-2002.jpeg" alt="Love Liza (2002) still">
+          <h2>Love Liza (2002)</h2>
+          <h3>Todd Louiso &middot; 90m &middot; 35mm</h3>
+          <p><i>Love Liza</i> centers website designer Wilson Joel (Philip Seymour Hoffman), who turns to gasoline fumes after the unexplained suicide of his wife Liza. Attempting to cope with his deteriorating psyche, he delves deeper and deeper into a spiral of self-destruction in one of this series' most subtle features. Todd Louiso puts Hoffman’s acting chops at the forefront of the film, producing a stunning study of a harrowing miasma of loss and pain.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=2C8B50E9-FFC0-42D6-A9CA-777E35AADBAD" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Saturday, January 14th</h1>
-          <img src="/images/2023winter/decision-to-leave-2022.jpeg" alt="Decision to Leave (2022) still">
-          <h2>Decision to Leave (2022)</h2>
-          <h3>Park Chan-wook &middot; 138m &middot; DCP</h3>
-          <p>Park Chan-wook won Best Director at Cannes for this haunting noir about a policeman (Park Hae-il) who falls for a murder suspect (Tang Wei). Park strips away the baroque style he became known for in films like <i>Oldboy</i> and <i>The Handmaiden</i>, stating he wanted to subvert modern tropes of the detective as either excessively violent or brilliant. The love story here was inspired by the Korean song “Mist,” which is woven throughout the film’s gorgeous score.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E6ED907A-D008-406C-A545-9708FAADF0B0" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Saturday, January 21st</h1>
+          <img src="/images/2023winter/woman-king-2022.jpeg" alt="The Woman King (2022) still">
+          <h2>The Woman King (2022)</h2>
+          <h3>Gina Prince-Bythewood &middot; 135m &middot; DCP</h3>
+          <p>Inspired by—but in no way limited to—true historical events, Gina Prince-Bythewood's most recent release <i>The Woman King</i> stars Viola Davis as the titular leader of the Agojie, a group of warrior women who protected the African kingdom of Dahomey (modern-day Benin) in the 1800s. The film is an action-packed and energetic historical epic with some electrifying performances (what else would one expect from Viola Davis?), and a lot of heart.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=54437724-64FF-420C-AEFA-AF6957BC409C" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Sunday, January 15th</h1>
-          <img src="/images/2023winter/the-seventh-seal-1957.jpeg" alt="The Seventh Seal (1957) still">
-          <h2>The Seventh Seal (1957)</h2>
-          <h3>Ingmar Bergman &middot; 96m &middot; DCP</h3>
-          <p>Having just returned to plague-ridden Sweden after fighting in the Crusades, medieval knight Antonius Block is met suddenly by Death, whom he challenges to a chess match over his very soul. Bergman is uncompromising, placing the silence of God at the very heart of his film; but he weaves glints of human kindness into this macabre tapestry, as Block and his squire Jöns try to accomplish one redemptive act before joining for eternity in the Dance of Death.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=BBDE830C-55CC-4EDE-885D-A7D3311FCD47" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Sunday, January 22nd</h1>
+          <img src="/images/2023winter/the-phantom-carriage-1921.jpeg" alt="The Phantom Carriage (1921) still">
+          <h2>The Phantom Carriage (1921)</h2>
+          <h3>Victor Sjöström &middot; 106m &middot; Digital</h3>
+          <p>Cited by Ingmar Bergman as a major influence on his own work, <i>The Phantom Carriage</i> is an indisputable classic of ghoulish Swedish cinema. Released itself on New Year's Day, 1921, the film begins on New Year's Eve, as violent alcoholic David Holm and his drinking buddies share the story of the Phantom Carriage. Legend has it that the last great sinner to die each year will have to drive the eponymous chariot, collecting the souls of the dead for a year...</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=C0FB8012-143B-4D22-8BA2-A14858DCEB72" target="_blank">here</a></u>.</b></p>
         </div>
 
       </div>
