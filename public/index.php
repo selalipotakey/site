88983ef3170b62035b231902560856f3c735e67e
cameronpoe <cameronmpoe@gmail.com>
2023-02-13 17:31:09 -0600

    week 7

--- public/index.php
+++ public/index.php
@@ -101,86 +101,78 @@
         </div>
 
         <div class="screening">
-          <h1>7:00PM Monday, February 6th</h1>
-          <img src="/images/2023winter/the-strange-love-of-martha-ivers-1946.jpg" alt="The Strange Love of Martha Ivers (1946) still">
-          <h2>The Strange Love of Martha Ivers (1946)</h2>
-          <h3>Lewis Milestone &middot; 116m &middot; 35mm</h3>
-          <p>When an unhappily married couple—played by Barbara Stanwyck and Kirk Douglas (in his first film!)—reunites with their childhood friend (Van Heflin) with whom they share a terrible secret, drama ensues, in <i>The Strange Love of Martha Ivers</i>. One of a string of darker antiheroine roles Stanwyck took on after the success of <i>Double Indemnity</i>, the film shows her comfort with morally ambiguous characters, which was unique among megastars at the time.</p>
-          <p><i>Preserved by the Library of Congress.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=F2C00006-76F4-47EA-AC34-62FC180ABC49" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Monday, February 13th</h1>
+          <img src="/images/2023winter/sorry-wrong-number-1948.jpg" alt="Sorry, Wrong Number (1948) still">
+          <h2>Sorry, Wrong Number (1948)</h2>
+          <h3>Anatole Litvak &middot; 89m &middot; 35mm</h3>
+          <p>One of the bigger injustices of Oscars history is that Barbara Stanwyck never won a competitive Academy Award, with <i>Sorry, Wrong Number</i> netting her fourth and final Best Actress nomination. Playing a bedridden woman who is pulled into conspiracy after she overhears a murder plot over the telephone, Stanwyck (acting opposite fellow legend Burt Lancaster) mustered up every inch of Hollywood hysteria she could in this film noir curiosity.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=007683BF-B58F-4159-8BB0-9F798594A1CA" target="_blank">here</a></u>.</b></p>
         </div>
         
-        <div id="sa-i-gu-bontoc-eulogy" class="screening" style="scroll-margin-top: var(--header-height);">
-          <h1>7:00PM Tuesday, February 7th</h1>
-          <img src="/images/2023winter/sa-i-gu-1993.png" alt="Sa-I-Gu (1993) // Bontoc Eulogy (1995) still">
-          <h2>Sa-I-Gu (1993) // Bontoc Eulogy (1995)</h2>
-          <h3>Dai Sil Kim-Gibson and Christine Choy // Marlon Fuentes &middot; 41m // 56m &middot; Digital</h3>
-          <p>Tonight, the personal and the political collide in a revolt against the dominant narrative; countermemories from victims of trauma are presented which refuse victimization. <i>Sa-I-Gu</i> portrays the Korean-American women who fought against the brutality of the police after the LA Uprising; <i>Bontoc Eulogy</i> mixes fact, fiction, and performance as the filmmaker interrogates the archival record of his grandfather's exploitation as a human exhibit.</p>
-          <p><i>Free screening! </i>Sa-I-Gu<i> courtesy of CAAM. Sponsored by CSRPC.</i></p>
+        <div class="screening">
+          <h1>7:00PM Tuesday, February 14th</h1>
+          <img src="/images/2023winter/forbidden-city-usa-1989.jpg" alt="Forbidden City, U.S.A. (1989) // I Remember Dancing (2019) still">
+          <h2>Free Chol Soo Lee (2022) // Forbidden City, U.S.A. (1989) // I Remember Dancing (2019)</h2>
+          <h3>Julie Ha and Eugene Yi // Arthur Dong // Nguiyen Tan Hoang &middot; 83m // 56m // 5m &middot; DCP // DCP // Digital</h3>
+          <p>This screening begins with <i>Free Chol Soo Lee</i>, a film about the false imprisonment of Chol Soo Lee, which ignited a pan-Asian movement to free him. Rebelling against the model minority myth, <i>Forbidden City, U.S.A.</i> shows generations of Asian Americans clubbing instead of studying, and features delicious archival discoveries like performances by the "Chinese Frank Sinatra." <i>I Remember Dancing</i> adds to the sense of liberation, with images of gaysians erotically recounting their unrepressed sex, emphasizing the value in opposing stereotypes without privileging just masculinity or heterosexuality.</p>
+          <p><i>Note: FREE CHOL SOO LEE has been added to this screening due to a previous cancellation.</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=83F70A8F-F487-43E5-9DDA-FDF664FF487B" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Wednesday, February 8th</h1>
-          <img src="/images/2023winter/la-bete-humaine-1938.jpg" alt="La Bête Humaine (1938) still">
-          <h2>La Bête Humaine (1938)</h2>
-          <h3>Jean Renoir &middot; 100m &middot; 35mm</h3>
-          <p>Tormented by alcoholism and uncontrollable rages, engineer Lantier (the eponymous "human beast") takes refuge in his skill and love for his train, which runs daily between Le Havre and Paris. When his assistant stationmaster commits murder, the man's wife turns to Lantier for comfort and even begs Lantier to kill her husband. Jean Gabin and Simone Simon star in a story based on Emile Zola's classic novel, a darkly tinged ancestor of film noir.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=0956B8DF-0112-423D-A2CC-E248BC426519" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Wednesday, February 15th</h1>
+          <img src="/images/2023winter/the-river-1951.jpg" alt="The River (1951) still">
+          <h2>The River (1951)</h2>
+          <h3>Jean Renoir &middot; 99m &middot; Digital</h3>
+          <p>In Renoir's first color film, five English sisters grow up beside the Ganges in the British colony of India. The eldest sister, Harriet, absorbs Hindu and Christian beliefs as she comes of age, and falls in love with a handsome English soldier. When her love is unrequited and an even worse tragedy strikes, she turns to the river to end her terrible despair. <i>The River</i> helped launch the career of Satyajit Ray, who was the assistant director of the film.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=069F7C90-012D-4193-BF8C-41C5C0F0AEDD" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>9:30PM Wednesday, February 8th</h1>
-          <img src="/images/2023winter/boardinghouse-1982.jpeg" alt="Boardinghouse (1982) still">
-          <h2>Boardinghouse (1982)</h2>
-          <h3>John Wintergate &middot; 88m &middot; 35mm</h3>
-          <p>There’s normal 80s horror shlock, and then there’s <i>Boardinghouse</i>. Even its die-hard fans, like Cameron Worden of the Chicago Film Society, call it a “particularly crude labor of love.” Recognized as the first horror film shot on video but shown on film, Johnima and Kallassu Wintergate’s slasher film supplements the usual sex and gore with a healthy dose of telekinesis, a dash of new age sprituality, and a whole lot of the Wintergates’ band Lightstorm.</p>
-          <p><i>Note: </i>Boardinghouse<i> was originally slated to play on 2/2, but was rescheduled to 2/8.</i></p>
-          <!-- <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=43411E31-B863-4527-AFDB-E30AE0D3371A" target="_blank">here</a></u>.</b></p> -->
+          <h1>7:00PM Thursday, February 16th</h1>
+          <img src="/images/2023winter/fail-safe-1964.jpg" alt="Fail Safe (1964) still">
+          <h2>Fail Safe (1964)</h2>
+          <h3>Sidney Lumet &middot; 112m &middot; DCP</h3>
+          <p>When a critical system malfunction directs a group of nuclear-armed U.S. bombers to strike Moscow, the President scrambles to deflect them and prevent mutually assured destruction (sound familiar?). But whereas its twin film <i>Dr. Strangelove</i> played the nuclear threat as a dark political satire, <i>Fail Safe</i> gravely reminds us that even technical mistakes and political miscommunication cannot be treated lightly when Armageddon is but a button press away.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=B58BBFF8-9AEE-4B40-9E5B-80A925004D16" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Thursday, February 9th</h1>
-          <img src="/images/2023winter/godzilla-1954.jpg" alt="Godzilla (1954) still">
-          <h2>Godzilla (1954)</h2>
-          <h3>Ishirō Honda &middot; 96m &middot; DCP</h3>
-          <p>As fishing ships burst into flames across the Pacific, the Japanese government is horrified to find the culprit to be a prehistoric leviathan, awakened by hydrogen bomb tests in the Bikini Atoll. Both a timeless portrayal of national paranoia and a reimagination of the tragedies of Hiroshima and Nagasaki, <i>Godzilla</i> stands the test of time as a condemnation of mankind’s ever-mounting destructive nature and the immortal debut of the King of the Monsters.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=6B6A3A0E-5594-4F6C-9A09-AABE520D7E9B" target="_blank">here</a></u>.</b></p>
+          <h1>9:30PM Thursday, February 16th</h1>
+          <img src="/images/2023winter/28-days-later-2002.jpg" alt="28 Days Later (2002) still">
+          <h2>28 Days Later (2002)</h2>
+          <h3>Danny Boyle &middot; 113m &middot; 35mm</h3>
+          <p>When bike messenger Jim (Cillian Murphy) awakens from a coma, he finds that London has been ravaged by a mysterious “rage” virus. Following the massive box office success of Leonardo DiCaprio vehicle <i>The Beach</i>, Danny Boyle directed this smaller-budget horror film set in a post-apocalyptic England. <i>28 Days Later…</i> kickstarted the 2000s zombie revival with its gritty video cinematography and a crackling script from future A24 auteur Alex Garland.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=B45912D0-E2FF-4F05-8B8C-BC6F9FBECA3E" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>9:30PM Thursday, February 9th</h1>
-          <img src="/images/2023winter/dancer-in-the-dark-2000.jpeg" alt="Dancer in the Dark (2000) still">
-          <h2>Dancer in the Dark (2000)</h2>
-          <h3>Lars von Trier &middot; 140m &middot; 35mm</h3>
-          <p>Selma (Björk), a young Czechoslovakian mother, immigrates with her son to a factory town in rural Washington state during the 1960s in this Dogme 95-style musical. Winner of the Palme d’Or and best actress prizes at Cannes Film Festival in 2000, <i>Dancer in the Dark</i> features original music written and performed by Icelandic popstar Björk and observational yet distinctive digital camerawork from acclaimed <i>Paris, Texas</i> cinematographer Robby Müller.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=B0896081-04EB-472F-888D-34337DCE6776" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Friday, February 17th</h1>
+          <img src="/images/2023winter/the-master-2012.jpg" alt="The Master (2012) still">
+          <h2>The Master (2012)</h2>
+          <h3>Paul Thomas Anderson &middot; 137m &middot; 35mm</h3>
+          <p><i>The Master</i> follows traumatized Navy veteran Freddie Quell (Joaquin Phoenix) after he returns from serving in WWII. One fateful night, he stows away on a passing yacht being used by Lancaster Dodd (Philip Seymour Hoffman), the charismatic leader of “The Cause”. Dodd is immediately enamored with Quell, setting off a tempestuous relationship between the two. Phoenix and Hoffman's searing performances in this film are career-defining and unforgettable.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=3A873547-47ED-4950-AD6F-EDBD809C9559" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Friday, February 10th</h1>
-          <img src="/images/2023winter/the-savages-2007.png" alt="The Savages (2007) still">
-          <h2>The Savages (2007)</h2>
-          <h3>Tamara Jenkins &middot; 113m &middot; 35mm</h3>
-          <p>While critically acclaimed, <i>The Savages</i> is still one of this series’ most criminally-underwatched films. In it, playwright Wendy Savage (Laura Linney) and her brother Jon (Philip Seymour Hoffman) care for their estranged, senile father, all the while reckoning with the dysfunctional nature of their familial and personal lives. It is a harrowing depiction of a dreaded but largely universal experience—that of watching a parent age and ultimately die.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=DB20ABFB-5A3A-4105-B95F-E72D24E71BAD" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Saturday, February 18th</h1>
+          <img src="/images/2023winter/the-banshees-of-inisherin-2022.jpeg" alt="The Banshees of Inisherin (2022) still">
+          <h2>The Banshees of Inisherin (2022)</h2>
+          <h3>Martin McDonagh &middot; 114m &middot; DCP</h3>
+          <p>The relationship between lifelong companions Pádraic (Colin Farrell) and Colm (Brendan Gleeson) is suddenly thrown into turmoil when Colm abruptly ends the friendship. Refusing to give up easily, Pádraic determinedly sets out to repair the situation with help from his sister Siobhán and the island delinquent, Dominic. An ultimately gruesome tragicomedy, <i>The Banshees of Inisherin</i> is what Rotten Tomatoes dubs "a finely crafted feel-bad treat".</p>
+          <p><i>A Night Owls event with Professors Agnes Callard and Arnold Brooks. Students with a UCID get in for free!</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=61F8D5FC-CCFA-4A82-8365-DF974B3D9D2B" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Saturday, February 11th</h1>
-          <img src="/images/2023winter/black-panther-wakanda-forever-2022.jpeg" alt="Black Panther: Wakanda Forever (2022) still">
-          <h2>Black Panther: Wakanda Forever (2022)</h2>
-          <h3>Ryan Coogler &middot; 161m &middot; DCP</h3>
-          <p>The sequel to the 2018 Marvel blockbuster, <i>Black Panther: Wakanda Forever</i> sees a return to the Afrofuturist home of King T'Challa—the Black Panther—and his tribe. In the wake of T'Challa's death, his sister Shuri is given an ultimatum: Wakanda either relinquishes its vibranium detection technology, or puts itself in at risk of attack by aquatic superhumans. The nation must band together to defend itself, all while mourning their deceased king.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=F1EBD957-B9B9-4BF6-A91E-C8574812C332" target="_blank">here</a></u>.</b></p>
-        </div>
-
-        <div class="screening">
-          <h1>7:00PM Sunday, February 12th</h1>
-          <img src="/images/2023winter/dead-man-1995.png" alt="Dead Man (1995) still">
-          <h2>Dead Man (1995)</h2>
-          <h3>Jim Jarmusch &middot; 121m &middot; DCP</h3>
-          <p>On the run after killing a man, accountant William Blake (Johnny Depp) encounters a Native American man called "Nobody" (Gary Farmer), who thinks him the reincarnation of the English poet of the same name. <i>Dead Man</i> is Jim Jarmusch's masterful foray into the Western genre, presented at its most existential. The film features a largely improvised guitar score by Neil Young and gorgeous monochrome cinematography by the unparalleled Robby Müller.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=5A27BE7F-A331-40B9-82A6-3B126026BE38" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Sunday, February 19th</h1>
+          <img src="/images/2023winter/the-ballad-of-narayama-1958.jpeg" alt="The Ballad of Narayama (1958) still">
+          <h2>The Ballad of Narayama (1958)</h2>
+          <h3>Keisuke Kinoshita &middot; 98m &middot; 35mm</h3>
+          <p><i>The Ballad of Narayama</i> uses the art of kabuki theatre to explore the mythic practice of <i>obasute</i>—the abandonment of the elderly in a desolate place, where they are subsequently left to die. Kinuyo Tanaka plays Orin, a widow approaching 70, who accepts her fate graciously, unlike most of the village elderly. It is instead her son, Tatushei, who is reluctant to carry his aging mother up Narayama mountain.</p>
+          <p><i>35mm print courtesy of the Japan Foundation.</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=AA3DA64D-37D4-49C7-81EA-1ABBC94A9D88" target="_blank">here</a></u>.</b></p>
         </div>
 
       </div>
