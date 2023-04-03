60189e058dccb7bd86d9f393dc4eafd9091f80a2
selalipotakey <spotakey@uchicago.edu>
2023-04-03 09:33:54 +0200

    first actual

--- public/index.php
+++ public/index.php
@@ -100,71 +100,71 @@
         </div>
 
         <div class="screening">
-          <h1>7:00PM Tuesday, March 28th</h1>
-          <img src="/images/2023spring/everything-everywhere-all-at-once-2022.jpg" alt="Everything Everywhere All at Once (2022) still">
-          <h2>Everything Everywhere All at Once (2022)</h2>
-          <h3>Daniel Kwan and Daniel Scheinert &middot; 139m &middot; DCP</h3>
-          <p>Back on the big screen! <i>Everything Everywhere All at Once</i> is a whimsical, sci-fi, comedy-drama following Evelyn Wang (Michelle Yeoh) as her marriage to Waymond (Ke Huy Quan) and relationship with her daughter Joy (Stephanie Hsu) begin to implode. <i>EEAAO</i> swept the awards season, being nominated for 11 Academy Awards and winning Yeoh a Golden Globe for best actress.</p>
-          <p><i>Co-presented with the Society of Women in Physics. All proceeds to go to Girls 4 Science, a nonprofit organization dedicated to exposing girls in Chicago, ages 10–18 years old, to STEM.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=5CEBE0DB-3026-4481-838A-297FCF538022" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Monday, April 3rd</h1>
+          <img src="/images/2023spring/pride-and-prejudice-2005.png" alt="Pride and Prejudice (2005) still">
+          <h2>Pride and Prejudice (2005)</h2>
+          <h3>Joe Wright &middot; 127m &middot; DCP</h3>
+          <p>It is a truth universally acknowledged that a Doc Films theatergoer in possession of a quarter pass must be in want of a dreamy period romance. An adaptation of a beloved classic, <i>Pride and Prejudice</i> follows headstrong Elizabeth Bennet as she becomes acquainted with Mr. Darcy, a wealthy, dour bachelor. Amidst encounters with impossible sisters, bumbling clergymen, and rakish strangers, Elizabeth comes to find that first impressions can be misleading.</p>
+          <p><i>Event sponsored by the English Department, in honor of Emily Cheng’s BA Thesis on re-reading and the feminine gaze in both Austen's and Wright's </i>Pride and Prejudice<i>.</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=2031A7FE-8464-4688-B9DB-EC23F083DBD0" target="_blank">here</a></u>.</b></p>
         </div>
         
-        <div id="jeanne-dielman" class="screening" style="scroll-margin-top: var(--header-height);">
-          <h1>7:00PM Wednesday, March 29th; 4:00PM Sunday, April 2nd</h1>
-          <img src="/images/2023spring/jeanne-dielman-23-quai-du-commerce-1080-bruxelles-1975.jpeg" alt="Jeanne Dielman, 23, quai du Commerce, 1080 Bruxelles (1975) still">
-          <h2>Jeanne Dielman, 23, quai du Commerce, 1080 Bruxelles (1975)</h2>
-          <h3>Chantal Akerman &middot; 201m &middot; DCP</h3>
-          <p>The film that changed everything. Delphine Seyrig gave her greatest contribution to cinema in Chantal Akerman’s <i>Jeanne Dielman</i> as a widowed housewife who spends her days peeling potatoes for her son's meals and trying to make ends meet. The film’s brilliance lies in its attentiveness towards its subject, the textural duration and repetition of Jeanne’s daily tasks, and the eventual disruption that occurs. Quietly harrowing, exhilarating, and triumphant.</p>
-          <p><b>March 29th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=A2F0623E-AA14-4C83-A914-9C865DA8C6F4" target="_blank">here</a></u>.</b></p>
-          <p><b>April 2nd tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=EEF242A1-5005-43D8-B784-113A5E330205" target="_blank">here</a></u>.</b></p>
+        <div class="screening">
+          <h1>7:00PM Wednesday, April 5th</h1>
+          <img src="/images/2023spring/muriel-or-the-time-of-return-1963.jpg" alt="Muriel, or the Time of Return (1963) still">
+          <h2>Muriel, or the Time of Return (1963)</h2>
+          <h3>Alain Resnais &middot; 116m &middot; DCP</h3>
+          <p>Resnais exchanges Seyrig’s dreamy character in <i>Marienbad</i> for Hélenè, an agitated widow who lives in an antique shop with her stepson Bernard. He is deeply haunted by his time in the Algerian war, just as Hélenè is haunted by an ex-lover’s visit. Characters are trapped in their own histories as they confuse memories or obsess over them. Do they realize the futility of their perpetual forgetting and remembering? Perhaps, but they persist, or cannot escape.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=BEA9FEF5-E107-4CCD-9CE0-404277F574C4" target="_blank">here</a></u>.</b></p>
+        </div>
+
+        <div id="pans-labyrinth" class="anchor screening">
+          <h1>7:00PM Thursday, April 6th; 4:00PM Sunday, April 16th</h1>
+          <img src="/images/2023spring/pans-labyrinth-2006.jpg" alt="Pan's Labyrinth (2006) still">
+          <h2>Pan's Labyrinth (2006)</h2>
+          <h3>Guillermo del Toro &middot; 119m &middot; 35mm</h3>
+          <p><i>Pan’s Labyrinth</i> may very well be the film that truly put Guillermo del Toro on the map. While living In the countryside of Franco's Spain with her mother and dictatorial stepfather, 10-year-old Ofelia encounters an abandoned labyrinth, home to a mysterious faun who lays out a gauntlet of three trials Ofelia must complete to fulfill her only wish: to return to her real father. Destiny, fate, and mortality make strange bedfellows in this fantastical tale.</p>
+          <p><i>Screening subsidized for members of the organization of Latin American Students (OLAS) in conjunction with its cultural show. However, all are welcome!</i></p>
+          <p><b>April 6th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=398748C3-5E7C-4FBD-9A24-36D9AFFE0128" target="_blank">here</a></u>.</b></p>
+          <p><b>April 16th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=A13CF48F-3A03-46ED-9806-2C21529A84EB" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Thursday, March 30th</h1>
-          <img src="/images/2023spring/children-of-men-2006.png" alt="Children of Men (2006) still">
-          <h2>Children of Men (2006)</h2>
-          <h3>Alfonso Cuarón &middot; 109m &middot; 35mm</h3>
-          <p>In a dystopian future where society is on the brink of collapse and mankind’s existence is threatened by infertility, <i>Children of Men</i> follows a disillusioned, former-activist bureaucrat (Clive Owen) in Britain—now a xenophobic police state—as he works to transport a miraculously pregnant woman to sanctuary. Aided by the work of cinematographer Emmanuel Lubezki, Cuarón has created a timeless visual masterpiece filled with themes that still resonate today.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=845089CA-49D6-4BEB-A8F6-F3B267691F1B" target="_blank">here</a></u>.</b></p>
+          <h1>9:30PM Thursday, April 6th</h1>
+          <img src="/images/2023spring/crimes-of-the-future-2022.jpeg" alt="Crimes of the Future (2022) still">
+          <h2>Crimes of the Future (2022)</h2>
+          <h3>David Cronenberg &middot; 107m &middot; DCP</h3>
+          <p>The second film by Cronenberg to be titled <i>Crimes of the Future</i> makes this a kind of return to form. This is a cold, delirious, and orgasmic journey into the future of human evolution that seeks to ask who can and should control the physicality of human expression. "Body is Reality"—so said Cronenberg during the film's premiere at Cannes. And so control over one's flesh, and the extremes of that control, are at the very heart of the film.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=721DC3C0-BB86-46B1-9D7D-15FE731E0BED" target="_blank">here</a></u>.</b></p>
+        </div>
+
+        <div id="singin-in-the-rain" class="anchor screening">
+          <h1>7:00PM Friday, April 7th; 4:00PM Sunday, April 9th</h1>
+          <img src="/images/2023spring/singin-in-the-rain-1952.jpg" alt="Singin' in the Rain (1952) still">
+          <h2>Singin' in the Rain (1952)</h2>
+          <h3>Gene Kelly and Stanley Donen &middot; 115m &middot; 35mm</h3>
+          <p>If you didn’t know any better, you might think this film was 115 minutes of Gene Kelly doing nothing but, well, singing in the rain. After all, that is by and large the only image that people use nowadays to reference the damn thing. But <i>Singin’ in the Rain</i> has more to offer than that—a lot more. It’s a bombastic whirlwind: gaudy, colorful, loud, and ever so entertaining in all the right ways. All you <i>La La Land</i> fiends had better show up to this one...</p>
+          <p><b>April 7th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=4B3A0778-1C33-4E7D-A502-6817EF0C319F" target="_blank">here</a></u>.</b></p>
+          <p><b>April 9th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=06AB5611-72C8-47AE-B92A-4F2AE4910D44" target="_blank">here</a></u>.</b></p>
+        </div>
+
+        <div id="tar" class="anchor screening">
+          <h1>4:00PM Saturday, April 8th; 8:00PM Saturday, April 8th</h1>
+          <img src="/images/2023spring/tar-2022.jpeg" alt="Tár (2022) still">
+          <h2>Tár (2022)</h2>
+          <h3>Todd Field &middot; 158m &middot; DCP</h3>
+          <p>Lydia Tár has it all—fast cars, a hot wife, and acclaim as the first female chief conductor of the Berlin Philharmonic. She stands in front of her orchestra to give the downbeat to Mahler's Fifth, but a young cellist catches her eye. Shadows and sounds linger in her periphery... genius turned arrogance, past deeds, paranoia, and the rumblings of an orchestrated downfall. Can Tár face her orchestra and her ghosts? Can she obliterate herself for her music?</p>
+          <p><b>4:00PM tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=8642B635-A847-4CDF-921B-FE3051AABDBB" target="_blank">here</a></u>.</b></p>
+          <p><b>8:00PM tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=D874FB78-DE0B-454C-97CA-CCE5F0799408" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>9:30PM Thursday, March 30th</h1>
-          <img src="/images/2023spring/videodrome-1983.jpg" alt="Videodrome (1983) still">
-          <h2>Videodrome (1983)</h2>
-          <h3>David Cronenberg &middot; 87m &middot; DCP</h3>
-          <p>A film of near apocalyptic proportions squeezed down into the horrific city of Toronto, <i>Videodrome</i> follows the president of a nearly-pornographic television station as he becomes enraptured in a mysterious, foreign TV show full of blood, sex, and murder. Enthralled as he seeks out the show's source, reality begins to collapse around him as the worlds of sex, biology, and technology coalesce into one rapturous mess. LONG LIVE THE NEW FLESH.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=27381671-63EE-4AB7-BEB1-A42ADA4138BB" target="_blank">here</a></u>.</b></p>
-        </div>
-
-        <div class="screening">
-          <h1>7:00PM Friday, March 31st</h1>
-          <img src="/images/2023spring/sunrise-a-tale-of-two-humans-1927.jpg" alt="Sunrise: A Song of Two Humans (1927) still">
-          <h2>Sunrise: A Song of Two Humans (1927)</h2>
-          <h3>F. W. Murnau &middot; 94m &middot; 35mm</h3>
-          <p>The best German expressionist film ever made outside of Germany, <i>Sunrise: A Song of Two Humans</i> delivers on the promise of its title. A glorious, melodic wave of emotion, Murnau’s opus is a testament to what the medium of silent film could offer in its final days. <i>Sunrise</i> would go on to win the only once awarded Oscar for “Unique and Artistic Picture." It's a gorgeous film not to be missed, and in Addison's opinion, the best film in this series.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=3F366BB3-A710-40E8-8457-5ABC44329324" target="_blank">here</a></u>.</b></p>
-        </div>
-
-        <div class="screening">
-          <h1>4:00PM Saturday, April 1st; 8:00PM Saturday, April 1st</h1>
-          <img src="/images/2023spring/babylon-2022.png" alt="Babylon (2022) still">
-          <h2>Babylon (2022)</h2>
-          <h3>Damien Chazelle &middot; 189m &middot; DCP</h3>
-          <p>Damien Chazelle's <i>Babylon</i> follows an ensemble cast of Margot Robbie, Brad Pitt, Diego Calva and more through the glitz, glamor, and corruption of late '20s Hollywood, in the transition from silent to sound movies. Divisive among critics upon its release, the film is both a condemnation of and a love letter to the world of movies and the Hollywood institution itself. You may love it, you may hate it, but regardless, you'll have a good time at the movies.</p>
-          <p><b>4:00PM tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=26FDDE8C-A744-4DE7-85C5-B795FC1D3B71" target="_blank">here</a></u>.</b></p>
-          <p><b>8:00PM tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=95A8E7DF-61A7-45D7-A201-974A123141CD" target="_blank">here</a></u>.</b></p>
-        </div>
-
-        <div class="screening">
-          <h1>8:00PM Sunday, April 2nd</h1>
-          <img src="/images/2023spring/fear-and-desire-1953.jpeg" alt="Fear and Desire (1953) still">
-          <h2>Fear and Desire (1951) // Day of the Fight (1952)</h2>
-          <h3>Stanley Kubrick // Stanley Kubrick &middot; 72m // 16m &middot; 35mm</h3>
-          <p>Stanley Kubrick’s feature directorial debut, <i>Fear and Desire</i> was produced with a small crew of 15 people and an original shoestring budget of only $10,000, raised largely by Kubrick’s family. An anti-war film released under the context of the Korean War, Kubrick reportedly quit his job as a staff photographer for <i>Look</i> Magazine to make the film. Preceded by <i>Day of the Fight</i> a short documentary produced by Kubrick during his days with <i>Look</i> Magazine.</p>
-          <p><i>Preserved by the Library of Congress.</i></p>
-          <p><i>Note: DAY OF THE FIGHT had previously been billed as 16mm, but the format has changed to 35mm.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=765B7207-D7B4-4161-AEE1-DD1E0E5F0F59" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Sunday, April 9th</h1>
+          <img src="/images/2023spring/la-pointe-courte-1955.jpeg" alt="La Pointe Courte (1955) still">
+          <h2>La Pointe Courte (1955)</h2>
+          <h3>Agnès Varda &middot; 80m &middot; DCP</h3>
+          <p>A professional photographer before becoming a filmmaker, Varda cited a fluid relationship between her photography and filmmaking. Of <i>La Pointe Courte</i>, Varda recounted “I started making films with the sole experience of photography, that's to say, where to place the camera, at what distance, with which lens and what lights?” The film moseys through a port city in France, loosely following a couple contemplating their marriage, a fisherman, and a regatta.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=634D871D-E964-4EAD-9884-DC7EF0A93C52" target="_blank">here</a></u>.</b></p>
         </div>
 
       </div>
