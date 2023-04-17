a97a9ee227052f2d586e92c37200d6688e57eb4c
selalipotakey <spotakey@uchicago.edu>
2023-04-17 14:42:20 +0200

    yang changes donate page 2

--- public/donate.php
+++ public/donate.php
@@ -38,7 +38,7 @@
         <p>Doc Films is a <a href="/about/"><u>student-led, volunteer-run</u></a> movie theater, film society, and non-profit at the University of Chicago. Our celluloid-first commitment to showing highly varied, highly repertory cinema often leaves us with fewer profits than giant theater chains.</p>
         <br>
         <p>It is this reason why we are so crucially indebted to the many thousands of dollars our fans and alumni donate yearly. If you would like to donate to help the oldest student film society keep cinema alive, please read below on how to do so. We greatly appreciate any donation, no matter the amount, and as a non-profit, any donation is tax-deductable.</p>
-        <br><br>
+        <br>
         <h2>How to donate</h2>
         <br>
         <ol>
