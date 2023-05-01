330f8fce7cf1ab27af67d6d0a4b882f692561146
selalipotakey <spotakey@uchicago.edu>
2023-05-01 10:54:22 +0200

    new week + carousel

--- public/index.php
+++ public/index.php
@@ -30,33 +30,19 @@
           <div class="carousel__track-container">
             <ul class="carousel__track">
               <li class="carousel__slide current-slide">
-                <a href="/calendar/special-events#tantura">
-                  <img class="carousel__image" src="/images/2023spring/tantura-2022.jpg" alt="Tantura (2022) still">
+                <a href="/calendar/special-events#crip-camp">
+                  <img class="carousel__image" src="/images/2023spring/crip-camp-2020.jpeg" alt="Crip Camp (2020) still">
                 </a>
                   <div class="description">
-                    <a href="/calendar/special-events#tantura">
+                    <a href="/calendar/special-events#crip-camp">
                       <p class="event-title fittext">
-                      Tantura (Tues. 5/2 @ 6:30PM by the Chicago Palestine Film Festival)
+                      Crip Camp (Sat. 5/6 @ 4:00PM)
                       </p>
                     </a>
-                    <p class="capsule fittext">Part of the Chicago Palestine Film Festival, organized by UChicago Center for Middle Eastern Studies. Film is followed by a panel discussion 8:30-9:15pm with Dr. Sawsan Jaber, Prof. Darryl Li, and Kay Heikkenen, moderated by Prof. Orit Bashkin.
+                    <p class="capsule fittext">Directed by documentary filmmakers Nicole Newnham and James LeBrecht, <i>Crip Camp</i> tells the story of the campers-turned-activists of Camp Jened, a New York summer camp for disabled people. Newnham and LeBrecht begin the film by inviting audiences into a miniature utopia hidden in the Catskill Mountains, where campers were able to shamelessly be themselves. <i>Crip Camp</i> also stars real-life "Jenedians" Larry Allison, Judith Heumann, and Stephen Hofmann.
                     </p>
+                    <p class="capsule fittext"><i>Free screening! Co-sponsored by Students for Disability Justice.</i></p>
                   </div>
-              </li>
-
-              <li class="carousel__slide">
-                <a href="/calendar/friday#2001-a-space-odyssey">
-                  <img class="carousel__image" src="/images/2023spring/2001-a-space-odyssey-1968.png" alt="2001: A Space Odyssey (1968) still">
-                </a>
-                <div clas="description">
-                  <a href="/calendar/friday#2001-a-space-odyssey">
-                    <p class="event-title fittext">
-                      2001: A Space Odyssey (Fri. 4/28 @ 7:00PM)
-                    </p>
-                  </a>
-                  <p class="capsule fittext"><i>2001: A Space Odyssey</i> is a psychedelic space opera that basically wrote the textbook on everything. A spectacle like no other, Kubrick’s foray into science fiction has had an indelible impact on our culture. Whether it be the monolith, HAL 9000, Strauss, or the space fetus, it's the kind of movie most of us know before we’ve even seen it. But that isn’t to say it lacks surprises—<i>2001</i> packs one hell of a punch, and is a big-screen viewing essential.
-                  </p>
-                </div>
               </li>
 
               <li class="carousel__slide">
@@ -71,6 +57,20 @@
                     </a>
                     <p class="capsule fittext">Tenoch and Julio attempt to seduce an older Spanish woman by embarking on a road trip to a mythical and totally non-existent beach: the “Boca del Cielo.” Set against a backdrop of political upheaval and social change, Cuarón’s film explores sexuality, manhood, and youth in a way that has forever changed Mexican cinema.</p>
                   </div>
+              </li>
+
+              <li class="carousel__slide">
+                <a href="/calendar/friday#citizen-kane">
+                  <img class="carousel__image" src="/images/2023spring/citizen-kane-1941.jpg" alt="Citizen Kane (1941) still">
+                </a>
+                <div clas="description">
+                  <a href="/calendar/friday#citizen-kane">
+                    <p class="event-title fittext">Citizen Kane (Fri. 5/12 @ 7:00PM || Sun. 5/14 @ 4:00PM)
+                    </p>
+                  </a>
+                  <p class="capsule fittext">It's terrific!
+                  </p>
+                </div>
               </li>
 
             </ul>
@@ -99,61 +99,78 @@
           <h3><i>$7 tickets, theater opens 30 minutes before the screening.</i></h3>
         </div>
 
-        <div class="screening">
-          <h1>7:00PM Wednesday, April 26th</h1>
-          <img src="/images/2023spring/sois-belle-et-tais-toi-1981.jpg" alt="Sois belle et tais-toi! (1981) still">
-          <h2>Sois belle et tais-toi! (1981)</h2>
-          <h3>Delphine Seyrig &middot; 110m &middot; DCP</h3>
-          <p><i>Be Pretty and Shut Up!</i> Delphine Seyrig turns the camera around to become the filmmaker, breaking the expectations and illusions placed on herself and others as she documents a series of interviews with well-known actresses like Juliet Berto, Jane Fonda, Shirley MacLaine, Anne Wiazemsky, and more. Seyrig asks questions that radically allow the women to speak for themselves, illuminating what it means to be a woman in the patriarchal film industry.</p>
-          <p><i>Introduced by Matthew Hubbell, Cinema and Media Studies. Courtesy of the Centre audiovisuel Simone de Beauvoir.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=51969467-DA35-4CC0-9EA9-6D8A7F681CF9" target="_blank">here</a></u>.</b></p>
+        <div id="tantura" class="anchor screening">
+          <h1>6:30PM Tuesday, May 2nd</h1>
+          <img src="/images/2023spring/tantura-2022.jpg" alt="Tantura (2022) still">
+          <h2>Tantura (2022)</h2>
+          <h3>Alon Schwarz &middot; 94m &middot; Digital</h3>
+          <p>Part of the Chicago Palestine Film Festival, organized by UChicago Center for Middle Eastern Studies. Film is followed by a panel discussion 8:30-9:15pm with Dr. Sawsan Jaber, Prof. Darryl Li, and Kay Heikkenen, moderated by Prof. Orit Bashkin</p>
+          <p><b>For more information: <u><a href="https://www.google.com/url?q=https://www.palestinefilmfest.com/schedule/tantura-uc&sa=D&source=calendar&ust=1683228531663606&usg=AOvVaw2Fbh-78y3PPmcEhV_8GHMv" target="_blank">Palestine Film Festival-Tantura</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:30PM Thursday, April 27th</h1>
-          <img src="/images/2023spring/pacific-rim-2013.jpg" alt="Pacific Rim (2013) still">
-          <h2>Pacific Rim (2013)</h2>
-          <h3>Guillermo del Toro &middot; 131m &middot; 3D DCP</h3>
-          <p><i>Pacific Rim</i> is Guillermo del Toro’s sci-fi action epic where human-piloted mechs called Jaegers must fend off Kaiju, aliens from the deep sea. The story follows a washed-up pilot and an untested trainee who must work together to save the world. With impressive special effects and thrilling action sequences, Pacific Rim is an entertaining and visually stunning blockbuster that delivers on its core premise: giant robots fighting giant monsters.</p>
-          <p><i>This screening will happen at the Logan Center Room 201. Co-sponsored by the Film Studies Center.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=43123A41-C840-4D05-9E0D-A5A7C53AE38A" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Wednesday, May 3rd</h1>
+          <img src="/images/2023spring/maso-and-miso-go-boating-1976.jpg" alt="Maso and Miso Go Boating (1976) // Calamity Jane & Delphine Seyrig, A Story (2020) still">
+          <h2>Maso and Miso Go Boating (1976) // Calamity Jane & Delphine Seyrig, A Story (2020)</h2>
+          <h3>Carole Roussopoulos, Ioana Wieder, Delphine Seyrig, and Nadia Ringart // Babette Mangolte &middot; 55m // 87m &middot; DCP</h3>
+          <p>In 1975, a French TV show ran a program on the "year of the woman." Feminist video collective Les Insoumuses responded with <i>Maso and Miso</i>, a humorous and disruptive re-edit of the misogynistic show. Followed by a documentary about Seyrig's attempts to make a film about Calamity Jane's letters to her daughter. Babette Mangolte (cinematographer, <i>Jeanne Dielman</i>) interweaves footage and other materials from the incomplete journey in an homage to Seyrig.</p>
+          <p><i>Introduced by Aurore Spiers, CMS and CSGS. Restored by the ZKM de Karlsruhe in partnership with the Centre Pompidou. Courtesy of the Centre audiovisuel Simone de Beauvoir.</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=4AADC6E3-0346-4633-885C-67443FA0FC4F" target="_blank">here</a></u>.</b></p>
+        </div>
+
+        <div id="amores-perros" class="anchor screening">
+          <h1>6:00PM Thursday, May 4th; 4:00PM Sunday, May 7th</h1>
+          <img src="/images/2023spring/amores-perros-2000.jpg" alt="Amores Perros (2000) still">
+          <h2>Amores Perros (2000)</h2>
+          <h3>Alejandro González Iñárritu &middot; 153m &middot; 35mm</h3>
+          <p>The film that won Iñárritu his first moment of international stardom, <i>Amores Perros</i> is a gritty portrait of life in Mexico City. The movie tells the interconnected stories of three characters of different social statuses, whose lives are forever changed by a single car crash. The film’s nonlinear narrative structure, powerful performance, and sense of realism make it a gripping, emotionally resonant drama fully deserving of its critical acclaim.</p>
+          <p><b>May 4th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=81B35662-F7FD-4575-B99A-1E10221C0995" target="_blank">here</a></u>.</b></p>
+          <p><b>May 7th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=D75027E7-8439-4F15-9BBB-48DD426CD1C2" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>9:30PM Thursday, April 27th</h1>
-          <img src="/images/2023spring/rabid-1977.jpg" alt="Rabid (1977) still">
-          <h2>Rabid (1977)</h2>
-          <h3>David Cronenberg &middot; 91m &middot; DCP</h3>
-          <p><i>Rabid</i> (1977) is an infectiously tantalizing offering which follows a young woman named Rose (Marilyn Chambers) after she is injured in a motorcycle accident and is forced to undergo an odd new procedure. After awaking from surgery, she finds she now has a phallic shaped growth and an overwhelming need to drink human blood. What begins with one person quickly spreads—offering a terrifying and eerily familiar picture of a world under siege by disease.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=34BC890F-36CC-4CE5-A307-10868F1C88E9" target="_blank">here</a></u>.</b></p>
-        </div>
-
-        <div id="2001" class="anchor screening">
-          <h1>7:00PM Friday, April 28th; 4:00PM Sunday, April 30th</h1>
-          <img src="/images/2023spring/2001-a-space-odyssey-1968.png" alt="2001: A Space Odyssey (1968) still">
-          <h2>2001: A Space Odyssey (1968)</h2>
-          <h3>Stanley Kubrick &middot; 143m &middot; DCP</h3>
-          <p><i>2001: A Space Odyssey</i> is a psychedelic space opera that basically wrote the textbook on everything. A spectacle like no other, Kubrick’s foray into science fiction has had an indelible impact on our culture. Whether it be the monolith, HAL 9000, Strauss, or the space fetus, it's the kind of movie most of us know before we’ve even seen it. But that isn’t to say it lacks surprises—<i>2001</i> packs one hell of a punch, and is a big-screen viewing essential.</p>
-          <p><b>April 28th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=D732F8D7-1ABC-43EA-9D26-AEFD1F58A97E" target="_blank">here</a></u>.</b></p>
-          <p><b>April 30th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=402F0018-1635-4196-85F6-4DA034F0F941" target="_blank">here</a></u>.</b></p>
+          <h1>9:30PM Thursday, May 4th</h1>
+          <img src="/images/2023spring/naked-lunch-1991.jpg" alt="Naked Lunch (1991) still">
+          <h2>Naked Lunch (1991)</h2>
+          <h3>David Cronenberg &middot; 115m &middot; 35mm</h3>
+          <p>In part an obsessive adaptation of the eponymous 1959 classic and a metabiography of William S. Burroughs himself, <i>Naked Lunch</i> is obscene and beyond strange. It follows “Blank Face Bug Killer” Bill Lee (Peter Weller) and his wife Joan as they get high off their own insecticide supply. When Bill inadvertently kills Joan, it's off to the races: a winding story filled with salacious cockroach typewriters, crawling cities, and so, so many insects.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=85FD02F5-90C2-459C-B1B4-A4BBB4CCD273" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>8:00PM Saturday, April 29th</h1>
-          <img src="/images/2023spring/all-the-beauty-and-the-bloodshed-2022.jpeg" alt="All the Beauty and the Bloodshed (2022) still">
-          <h2>All the Beauty and the Bloodshed (2022)</h2>
-          <h3>Laura Poitras &middot; 122m &middot; DCP</h3>
-          <p>The second documentary ever to win Venice's prestigious Golden Lion, <i>All the Beauty and the Bloodshed</i>  examines photographer-activist Nan Goldin's valiant effort to hold Purdue Pharma responsible for the opioid epidemic. The film paints a compassionate, gritty portrait of the fight against the Sackler family—Purdue's owners—through the lens of an indefatigable, battle-hardened woman. This is a sobering work that deserves to be seen by all.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=1CBDBB55-979F-4311-B75D-67C922371E8E" target="_blank">here</a></u>.</b></p>
+          <h1>7:00PM Friday, May 5th</h1>
+          <img src="/images/2023spring/tokyo-story-1953.jpeg" alt="Tokyo Story (1953) still">
+          <h2>Tokyo Story (1953)</h2>
+          <h3>Yasujiro Ozu &middot; 136m &middot; DCP</h3>
+          <p>It’s week 7—we all need a good crying session. <i>Tokyo Story</i> 's movingly epic narrative takes place not across great landscapes, but in confined middle-class Tokyo homes, as an elderly couple visit their family in the bustling postwar metropolis. A hallmark of transcendental filmmaking, Ozu utilizes his signature tatami shots and veteran actors Setsuko Hara and Chishū Ryū to craft a generous, empathetic film about aging in an increasingly chaotic world.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=74E32CB6-7422-4425-ACA0-BD9196C23842" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Sunday, April 30th</h1>
-          <img src="/images/2023spring/me-and-my-brother-1969.jpeg" alt="Me and My Brother (1969) still">
-          <h2>Pull My Daisy (1959) // Me and My Brother (1969)</h2>
-          <h3>Robert Frank // Robert Frank &middot; 30m // 91m &middot; Digital // DCP</h3>
-          <p>A docu-fiction involving Allen Ginsberg, Joseph Chaikin, Peter Orlovsky, and his schizophrenic brother Julius, <i>Me and My Brother</i> serves as a significant document of the Beat movement in New York. A Swiss photographer, Robert Frank, provided an outsider’s eye to American life, traveling across the United States in the '50s and '60s and publishing the groundbreaking photobook <i>The Americans</i>. Preceeded by a short film also by Frank, <i>Pull My Daisy</i>.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=1628F234-7CAB-429F-B980-7F8ADE8085F2" target="_blank">here</a></u>.</b></p>
+          <h1>4:00PM Saturday, May 6th</h1>
+          <img src="/images/2023spring/crip-camp-2020.jpeg" alt="Crip Camp (2020) still">
+          <h2>Crip Camp (2020)</h2>
+          <h3>Nicole Newnham and James Lebrecht &middot; 108m &middot; DCP</h3>
+          <p>Directed by documentary filmmakers Nicole Newnham and James LeBrecht, <i>Crip Camp</i> tells the story of the campers-turned-activists of Camp Jened, a New York summer camp for disabled people. Newnham and LeBrecht begin the film by inviting audiences into a miniature utopia hidden in the Catskill Mountains, where campers were able to shamelessly be themselves. <i>Crip Camp</i> also stars real-life "Jenedians" Larry Allison, Judith Heumann, and Stephen Hofmann.</p>
+          <p><i>Free screening! Co-sponsored by Students for Disability Justice.</i></p>
+        </div>
+
+        <div class="screening">
+          <h1>8:00PM Saturday, May 6th</h1>
+          <img src="/images/2023spring/godland-2022.jpeg" alt="Godland (2022) still">
+          <h2>Godland (2022)</h2>
+          <h3>Hlynur Pálmason &middot; 143m &middot; DCP</h3>
+          <p>Like its Icelandic setting, <i>Godland</i> is beautiful, elemental, and more than 60% Lutheran. Writer-director Pálmason's third film follows Danish missionary Lucas as he journeys across the subarctic island with local guide Ragnar, lugging a hefty <i>fin de siècle</i>  camera across glacial meltwaters, volcanic mudflats, and others stunning landscapes of scarcity. Shot with natural light and in round-edged Academy ratio, <i>Godland</i> is simply sublime.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=3AC19F62-7101-4D3C-A4E3-AF67200BCD4F" target="_blank">here</a></u>.</b></p>
+        </div>
+
+        <div class="screening">
+          <h1>7:00PM Sunday, May 7th</h1>
+          <img src="/images/2023spring/the-learning-tree-1969.jpg" alt="The Learning Tree (1969) still">
+          <h2>The Learning Tree (1969)</h2>
+          <h3>Gordon Parks &middot; 107m &middot; DCP</h3>
+          <p>Based on his eponymous semi-autobiographical novel, <i>The Learning Tree</i> is Gordon Parks’ first major film effort and the first Black-directed movie for a major American studio. Alongside other classics like <i>Shaft</i>, Parks is recognized for his photojournalism career, in which he documented poverty, African American life, and civil rights issues. This film carries Parks’ photojournalist perspective, documenting his experience growing up in Kansas.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=51DCDE04-73D1-4623-8C0B-9283E04AF07D" target="_blank">here</a></u>.</b></p>
         </div>
 
         
