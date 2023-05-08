658e7d2bb9bdd5ff039404688a35c236fa3bae16
selalipotakey <spotakey@uchicago.edu>
2023-05-08 13:13:26 +0200

    new week

--- public/index.php
+++ public/index.php
@@ -29,22 +29,8 @@
 
           <div class="carousel__track-container">
             <ul class="carousel__track">
-              <li class="carousel__slide current-slide">
-                <a href="/calendar/special-events#crip-camp">
-                  <img class="carousel__image" src="/images/2023spring/crip-camp-2020.jpeg" alt="Crip Camp (2020) still">
-                </a>
-                  <div class="description">
-                    <a href="/calendar/special-events#crip-camp">
-                      <p class="event-title fittext">
-                      Crip Camp (Sat. 5/6 @ 4:00PM)
-                      </p>
-                    </a>
-                    <p class="capsule fittext">Directed by documentary filmmakers Nicole Newnham and James LeBrecht, <i>Crip Camp</i> tells the story of the campers-turned-activists of Camp Jened, a New York summer camp for disabled people. Newnham and LeBrecht begin the film by inviting audiences into a miniature utopia hidden in the Catskill Mountains, where campers were able to shamelessly be themselves. <i>Crip Camp</i> also stars real-life "Jenedians" Larry Allison, Judith Heumann, and Stephen Hofmann.
-                    <i>Free screening! Co-sponsored by Students for Disability Justice.</i></p>
-                  </div>
-              </li>
 
-              <li class="carousel__slide">
+              <li class="carousel__slide current-slide">
                 <a href="/calendar/thursday-1#y-tu-mama-tambien">
                   <img class="carousel__image" src="/images/2023spring/y-tu-mama-tambien-2001.jpeg" alt="quarter pass">
                 </a>
@@ -70,6 +56,21 @@
                   <p class="capsule fittext">Although <i>Vertigo</i> only lasted a decade as <i>Sight & Sound</i>'s #1, it isn’t any less impressive as a film. Directed by a promising young talent by the name of Alfred Hitchcock, <i>Vertigo</i> has it all! Our PI Scottie’s in a real mess: obsessed with at least one woman and recovering from a bad case of—surprise, surprise—vertigo, he dives headfirst into a hallucinatory and macabre journey of possession, filled to the brim with intrigue and drop-dead gorgeous style.
                   </p>
                 </div>
+              </li>
+
+              <li class="carousel__slide">
+                <a href="/calendar/thursday-1#shape-of-water">
+                  <img class="carousel__image" src="/images/2023spring/shape-of-water-2017.jpg" alt="The Shape of Water (2017) still">
+                </a>
+                  <div class="description">
+                    <a href="/calendar/thursday-1#shape-of-water">
+                      <p class="event-title fittext">
+                      The Shape of Water (Thurs. 5/18 @ 7:00PM)
+                      </p>
+                    </a>
+                    <p class="capsule fittext"><i>The Shape of Water</i>, which won the Oscar for Best Picture in 2018, is a quirky, fantastical romantic-fantasy that tells the story of an unexpected—and, yet, somehow natural—love between a mute janitor and an anthropomorphic aquatic creature/weapon held at a Cold War-era government facility. Guillermo del Toro’s poignant tale, which employs masterful set and costume designs, will capture your heart and imagination in a way you never thought possible.
+                    </p>
+                  </div>
               </li>
 
             </ul>
@@ -98,80 +99,61 @@
           <h3><i>$7 tickets, theater opens 30 minutes before the screening.</i></h3>
         </div>
 
-        <div id="tantura" class="anchor screening">
-          <h1>6:30PM Tuesday, May 2nd</h1>
-          <img src="/images/2023spring/tantura-2022.jpg" alt="Tantura (2022) still">
-          <h2>Tantura (2022)</h2>
-          <h3>Alon Schwarz &middot; 94m &middot; Digital</h3>
-          <p>Part of the Chicago Palestine Film Festival, organized by UChicago Center for Middle Eastern Studies. Film is followed by a panel discussion 8:30-9:15pm with Dr. Sawsan Jaber, Prof. Darryl Li, and Kay Heikkenen, moderated by Prof. Orit Bashkin</p>
-          <p><b>For more information: <u><a href="https://www.google.com/url?q=https://www.palestinefilmfest.com/schedule/tantura-uc&sa=D&source=calendar&ust=1683228531663606&usg=AOvVaw2Fbh-78y3PPmcEhV_8GHMv" target="_blank">Palestine Film Festival-Tantura</a></u>.</b></p>
+        <div class="screening">
+          <h1>7:00PM Wednesday, May 10th</h1>
+          <img src="/images/2023spring/letters-home-1984.png" alt="Letters Home (1986) still">
+          <h2>Letters Home (1986)</h2>
+          <h3>Chantal Akerman &middot; 104m &middot; DCP</h3>
+          <p>Known for the films about her relationship with her mother, Akerman’s third and last collaboration with Seyrig is fitting and poignant. Rarely seen, <i>Letters Home</i> is an adaptation of Rose Goldemberg's play, based on Sylvia Plath’s letters with her mother. Seyrig and her niece (Coralie Seyrig) recite the text directly to camera. Seyrig had asked Akerman to direct her in Racine's "Phèdre," but Akerman, not knowing Seyrig would die not long after, declined.</p>
+          <p><i>Courtesy of the Centre audiovisuel Simone de Beauvoir.</i></p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=84576CE6-2F72-489E-863D-93FA064D51BB" target="_blank">here</a></u>.</b></p>
+        </div>
+
+        <div id="y-tu-mama-tambien" class="anchor screening">
+          <h1>7:00PM Thursday, May 11th</h1>
+          <img src="/images/2023spring/y-tu-mama-tambien-2001.jpeg" alt="Y tu mamá también (2001) still">
+          <h2>Y tu mamá también (2001)</h2>
+          <h3>Alfonso Cuarón &middot; 106m &middot; 35mm</h3>
+          <p><i>Y tu mamá también</i>—an all-time classic—tells the tale of two horny Mexican teenagers, Tenoch (Diego Luna) and Julio (Gael García Bernal), as they attempt to seduce an older Spanish woman by embarking on a road trip to a mythical and totally non-existent beach: the “Boca del Cielo.” Set against a backdrop of political upheaval and social change, Cuarón’s film explores sexuality, manhood, and youth in a way that has forever changed Mexican cinema.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=ADDE09A5-A81E-469D-A40F-C67BC11E25DD" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Wednesday, May 3rd</h1>
-          <img src="/images/2023spring/maso-and-miso-go-boating-1976.jpg" alt="Maso and Miso Go Boating (1976) // Calamity Jane & Delphine Seyrig, A Story (2020) still">
-          <h2>Maso and Miso Go Boating (1976) // Calamity Jane & Delphine Seyrig, A Story (2020)</h2>
-          <h3>Carole Roussopoulos, Ioana Wieder, Delphine Seyrig, and Nadia Ringart // Babette Mangolte &middot; 55m // 87m &middot; DCP</h3>
-          <p>In 1975, a French TV show ran a program on the "year of the woman." Feminist video collective Les Insoumuses responded with <i>Maso and Miso</i>, a humorous and disruptive re-edit of the misogynistic show. Followed by a documentary about Seyrig's attempts to make a film about Calamity Jane's letters to her daughter. Babette Mangolte (cinematographer, <i>Jeanne Dielman</i>) interweaves footage and other materials from the incomplete journey in an homage to Seyrig.</p>
-          <p><i>Introduced by Aurore Spiers, CMS and CSGS. Restored by the ZKM de Karlsruhe in partnership with the Centre Pompidou. Courtesy of the Centre audiovisuel Simone de Beauvoir.</i></p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=4AADC6E3-0346-4633-885C-67443FA0FC4F" target="_blank">here</a></u>.</b></p>
+          <h1>9:30PM Thursday, May 11th</h1>
+          <img src="/images/2023spring/spider-2002.jpeg" alt="Spider (2002) still">
+          <h2>Spider (2002)</h2>
+          <h3>David Cronenberg &middot; 98m &middot; 35mm</h3>
+          <p><i>Spider</i> follows the schizophrenic "Spider" (Ralph Fiennes) as he navigates his subconscious understanding of his own traumatic childhood. A deeply personal film for both Cronenberg and Fiennes, <i>Spider</i> finds itself at the cusp of the director's primary foci: disturbing body horror and the exploration of internal drama. Its generic ambiguity makes it one of Cronenberg's most overlooked films, despite being an incredible portrayal of past and present pain.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=3CD338E8-9AF8-41CD-B1A5-E17BC4E80C06" target="_blank">here</a></u>.</b></p>
         </div>
 
-        <div id="amores-perros" class="anchor screening">
-          <h1>6:00PM Thursday, May 4th; 4:00PM Sunday, May 7th</h1>
-          <img src="/images/2023spring/amores-perros-2000.jpg" alt="Amores Perros (2000) still">
-          <h2>Amores Perros (2000)</h2>
-          <h3>Alejandro González Iñárritu &middot; 153m &middot; 35mm</h3>
-          <p>The film that won Iñárritu his first moment of international stardom, <i>Amores Perros</i> is a gritty portrait of life in Mexico City. The movie tells the interconnected stories of three characters of different social statuses, whose lives are forever changed by a single car crash. The film’s nonlinear narrative structure, powerful performance, and sense of realism make it a gripping, emotionally resonant drama fully deserving of its critical acclaim.</p>
-          <p><b>May 4th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=81B35662-F7FD-4575-B99A-1E10221C0995" target="_blank">here</a></u>.</b></p>
-          <p><b>May 7th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=D75027E7-8439-4F15-9BBB-48DD426CD1C2" target="_blank">here</a></u>.</b></p>
-        </div>
-
-        <div class="screening">
-          <h1>9:30PM Thursday, May 4th</h1>
-          <img src="/images/2023spring/naked-lunch-1991.jpg" alt="Naked Lunch (1991) still">
-          <h2>Naked Lunch (1991)</h2>
-          <h3>David Cronenberg &middot; 115m &middot; 35mm</h3>
-          <p>In part an obsessive adaptation of the eponymous 1959 classic and a metabiography of William S. Burroughs himself, <i>Naked Lunch</i> is obscene and beyond strange. It follows “Blank Face Bug Killer” Bill Lee (Peter Weller) and his wife Joan as they get high off their own insecticide supply. When Bill inadvertently kills Joan, it's off to the races: a winding story filled with salacious cockroach typewriters, crawling cities, and so, so many insects.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=85FD02F5-90C2-459C-B1B4-A4BBB4CCD273" target="_blank">here</a></u>.</b></p>
+        <div id="citizen-kane" class="anchor screening">
+          <h1>7:00PM Friday, May 12th; 4:00PM Sunday, May 14th</h1>
+          <img src="/images/2023spring/citizen-kane-1941.jpg" alt="Citizen Kane (1941) still">
+          <h2>Citizen Kane (1941)</h2>
+          <h3>Orson Welles &middot; 119m &middot; 35mm</h3>
+          <p>It’s terrific!</p>
+          <p><b>May 12th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=2D2A7B6B-4177-4AD5-B0EA-8D865132B4F0" target="_blank">here</a></u>.</b></p>
+          <p><b>May 14th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=C796F314-DFBC-4D81-A15F-5C2B60B8C3D9" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>7:00PM Friday, May 5th</h1>
-          <img src="/images/2023spring/tokyo-story-1953.jpeg" alt="Tokyo Story (1953) still">
-          <h2>Tokyo Story (1953)</h2>
-          <h3>Yasujiro Ozu &middot; 136m &middot; DCP</h3>
-          <p>It’s week 7—we all need a good crying session. <i>Tokyo Story</i> 's movingly epic narrative takes place not across great landscapes, but in confined middle-class Tokyo homes, as an elderly couple visit their family in the bustling postwar metropolis. A hallmark of transcendental filmmaking, Ozu utilizes his signature tatami shots and veteran actors Setsuko Hara and Chishū Ryū to craft a generous, empathetic film about aging in an increasingly chaotic world.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=74E32CB6-7422-4425-ACA0-BD9196C23842" target="_blank">here</a></u>.</b></p>
+          <h1>8:00PM Saturday, May 13th</h1>
+          <img src="/images/2023spring/pacifiction-2022.jpeg" alt="Pacifiction (2022) still">
+          <h2>Pacifiction (2022)</h2>
+          <h3>Albert Serra &middot; 162m &middot; DCP</h3>
+          <p>This post-colonial fever dream of French Polynesia was voted best film of 2022 by <i>Cahiers du Cinéma</i> and "suggests John le Carré by way of David Lynch," per A.O. Scott. It's a slow-burning character study of High Commissioner De Roller (Benoît Magimel, <i>The Piano Teacher</i>), who floats through all walks of island life with an easy charisma. But the lush Tahitian setting pulses with a sense of dread that only grows as political alliances shift and unravel.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=2E4A9ACA-EF9A-4183-99CD-5AC68A8DA72E" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div class="screening">
-          <h1>4:00PM Saturday, May 6th</h1>
-          <img src="/images/2023spring/crip-camp-2020.jpeg" alt="Crip Camp (2020) still">
-          <h2>Crip Camp (2020)</h2>
-          <h3>Nicole Newnham and James Lebrecht &middot; 108m &middot; DCP</h3>
-          <p>Directed by documentary filmmakers Nicole Newnham and James LeBrecht, <i>Crip Camp</i> tells the story of the campers-turned-activists of Camp Jened, a New York summer camp for disabled people. Newnham and LeBrecht begin the film by inviting audiences into a miniature utopia hidden in the Catskill Mountains, where campers were able to shamelessly be themselves. <i>Crip Camp</i> also stars real-life "Jenedians" Larry Allison, Judith Heumann, and Stephen Hofmann.</p>
-          <p><i>Free screening! Co-sponsored by Students for Disability Justice.</i></p>
+          <h1>7:00PM Sunday, May 14th</h1>
+          <img src="/images/2023spring/office-killer-1997.jpeg" alt="Office Killer (1997) still">
+          <h2>Office Killer (1997)</h2>
+          <h3>Cindy Sherman &middot; 95m &middot; Digital</h3>
+          <p>As a photographer, Cindy Sherman is regarded for her conceptual self-portrait photography, which probes questions of identity, celebrity, and representation. Sherman made her feature film debut with <i>Office Killer</i>, a camp horror film about a copy editor (Carol Kane) who tumbles into a murderous rampage directed at her co-workers. At once comedy, horror, melodrama, noir, and feminist art piece, this film is sure to surprise, provoke, and tickle the senses.</p>
+          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=84A4B286-6960-42A6-9862-170984759623" target="_blank">here</a></u>.</b></p>
         </div>
-
-        <div class="screening">
-          <h1>8:00PM Saturday, May 6th</h1>
-          <img src="/images/2023spring/godland-2022.jpeg" alt="Godland (2022) still">
-          <h2>Godland (2022)</h2>
-          <h3>Hlynur Pálmason &middot; 143m &middot; DCP</h3>
-          <p>Like its Icelandic setting, <i>Godland</i> is beautiful, elemental, and more than 60% Lutheran. Writer-director Pálmason's third film follows Danish missionary Lucas as he journeys across the subarctic island with local guide Ragnar, lugging a hefty <i>fin de siècle</i>  camera across glacial meltwaters, volcanic mudflats, and others stunning landscapes of scarcity. Shot with natural light and in round-edged Academy ratio, <i>Godland</i> is simply sublime.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=3AC19F62-7101-4D3C-A4E3-AF67200BCD4F" target="_blank">here</a></u>.</b></p>
-        </div>
-
-        <div class="screening">
-          <h1>7:00PM Sunday, May 7th</h1>
-          <img src="/images/2023spring/the-learning-tree-1969.jpg" alt="The Learning Tree (1969) still">
-          <h2>The Learning Tree (1969)</h2>
-          <h3>Gordon Parks &middot; 107m &middot; DCP</h3>
-          <p>Based on his eponymous semi-autobiographical novel, <i>The Learning Tree</i> is Gordon Parks’ first major film effort and the first Black-directed movie for a major American studio. Alongside other classics like <i>Shaft</i>, Parks is recognized for his photojournalism career, in which he documented poverty, African American life, and civil rights issues. This film carries Parks’ photojournalist perspective, documenting his experience growing up in Kansas.</p>
-          <p><b>Tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=51DCDE04-73D1-4623-8C0B-9283E04AF07D" target="_blank">here</a></u>.</b></p>
-        </div>
-
         
     </main>
     
