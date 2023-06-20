630fba377aed4e81794f551efb64649d55e7a97e
selalipotakey <spotakey@uchicago.edu>
2023-06-20 00:21:03 -0500

    page update 1

--- public/calendar/programmers-picks.php
+++ public/calendar/programmers-picks.php
@@ -27,10 +27,8 @@
           <img src="/images/2023summer/nashville-1975.jpg" alt="Nashville (1975) still">
           <h2>Nashville (1975)</h2>
           <h3>Robert Altman &middot; 159m &middot; DCP</h3>
-<!--
-          <p><b>June 30th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=4B3A0778-1C33-4E7D-A502-6817EF0C319F" target="_blank">here</a></u>.</b></p>
-          <p><b>June 31st tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=06AB5611-72C8-47AE-B92A-4F2AE4910D44" target="_blank">here</a></u>.</b></p>
--->
+          <p><b>June 30th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=FDE9F1F3-7CFA-4D23-9851-B05DED1A9F98" target="_blank">here</a></u>.</b></p>
+          <p><b>June 31st tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=572A111A-7201-4924-A133-DC42CBA70DAE" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="the-searchers" class="anchor screening">
@@ -38,6 +36,8 @@
           <img src="/images/2023summer/the-searchers-1956.jpg" alt="The Searchers (1956) still">
           <h2>The Searchers (1956)</h2>
           <h3>John Ford &middot; 119m &middot; 35mm</h3>
+          <p><b>July 7th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=D6A9D413-22DB-4E48-AC52-8FD9A71B6C67" target="_blank">here</a></u>.</b></p>
+          <p><b>July 8th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=5099868A-972E-4D2D-AC72-013A80917499" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="meet-me-in-st-louis" class="anchor screening">
@@ -45,6 +45,8 @@
           <img src="/images/2023summer/meet-me-in-st-louis-1944.jpg" alt="Meet Me in St. Louis (1944) still">
           <h2>Meet Me in St. Louis (1944)</h2>
           <h3>Vincent Minnelli &middot; 113m &middot; 35mm</h3>
+          <p><b>July 21st tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=FA70990D-3815-4375-8E96-8FB6C15A6AEC" target="_blank">here</a></u>.</b></p>
+          <p><b>July 22nd tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=CC2F20D7-E879-4976-906F-64AAB0209357" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="the-flaming-forest" class="anchor screening">
@@ -53,6 +55,8 @@
           <h2>The Flaming Forest (1926)</h2>
           <h3>Reginald Barker &middot; 70m &middot; 35mm</h3>
           <p><i>Preserved by the Library of Congress.</i></p>
+          <p><b>July 28th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=6BAAEE27-3BBC-42EC-A890-45BE3E939C55" target="_blank">here</a></u>.</b></p>
+          <p><b>July 29th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E5DB43D8-3E87-486A-996A-4C6C1A90FFA6" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="clash-of-the-wolves" class="anchor screening">
@@ -61,6 +65,8 @@
           <h2>Clash of the Wolves (1925)</h2>
           <h3>Noel M. Smith &middot; 74m &middot; 35mm</h3>
           <p><i>Preserved by the Library of Congress.</i></p>
+          <p><b>August 4th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=42939F3B-150A-434C-B1DB-68EF1AD246DF" target="_blank">here</a></u>.</b></p>
+          <p><b>August 5th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=4F4F6223-07E8-42DF-B1BD-3645C2B48C4C" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="within-the-law" class="anchor screening">
@@ -69,6 +75,8 @@
           <h2>Within the Law (1923)</h2>
           <h3>Frank Lloyd &middot; 65m &middot; 35mm</h3>
           <p><i>Preserved by the Library of Congress.</i></p>
+          <p><b>August 11th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=F54042DE-9031-4614-9610-9AA98AE4E1C5" target="_blank">here</a></u>.</b></p>
+          <p><b>August 12th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=909F7A26-0197-4A57-97A9-2AE53FDC16FC" target="_blank">here</a></u>.</b></p>
         </div>
         
       </div>
--- public/calendar/silver-screen.php
+++ public/calendar/silver-screen.php
@@ -28,10 +28,8 @@
           <h2>Bob & Carol & Ted & Alice (1969)</h2>
           <h3>Paul Mazursky &middot; 105m &middot; 35mm</h3>
           <p>It's ‘69, and the sexual mores of the mid-century have been smashed open. X-rated films were winning Best Picture, hippies camped out at a dairy farm upstate, gays were smashing windows, and a little dramedy about an orgy won over audiences across America. Featuring a killer score by Quincy Jones, multiple unshaved chests, and an Oscar-nominated screenplay, Paul Mazursky’s <i>Bob & Carol & Ted & Alice</i> was the little husband and wife-swapping film that could. Bob and Carol, liberated by a New Age retreat, plan a hookup with their more traditional pals, Ted and Alice, in this work of lasciviousness and surprising emotional honesty.</p>
-<!--
-          <p><b>June 23rd tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=4B3A0778-1C33-4E7D-A502-6817EF0C319F" target="_blank">here</a></u>.</b></p>
-          <p><b>June 24th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=06AB5611-72C8-47AE-B92A-4F2AE4910D44" target="_blank">here</a></u>.</b></p>
--->
+          <p><b>June 23rd tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=D2BE94AF-D253-4DC1-9659-5DA1DF6B56F4" target="_blank">here</a></u>.</b></p>
+          <p><b>June 24th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=806A6E51-820A-4242-A0D5-A1595AC3446B" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="the-long-goodbye" class="anchor screening">
@@ -40,6 +38,8 @@
           <h2>The Long Goodbye (1973)</h2>
           <h3>Robert Altman &middot; 112m &middot; DCP</h3>
           <p><i>The Long Goodbye</i> starts not with a bang, but with a vibe. Elliott Gould’s Philip Marlowe wanders through a 1973 L.A. laden with denizens who have little between their ears. In typical Robert Altman style, he sends Marlowe into a rabbit-hole of dead friends and strangers. Suspicion falls on Marlowe after an old friend’s wife is found dead, but this quickly becomes the least of his worries. Gangsters, alcoholic writers, and detox clinics all join Altman’s cavalcade symphony of Malibu vapidity.</p>
+          <p><b>June 23rd tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=02C5AA50-9084-4D90-AE41-99CCD1910AEB" target="_blank">here</a></u>.</b></p>
+          <p><b>June 24th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=1F73AA9C-BB39-42C8-8A1F-25A9E678C9EC" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="carwash" class="anchor screening">
@@ -48,7 +48,9 @@
           <h2>Car Wash (1976)</h2>
           <h3>Michael Schultz &middot; 97m &middot; 35mm</h3>
           <p>Taking place on a single day of a happening Los Angeles car wash, Michael Schultz’s film is the perfect summer comedy encapsulating the zeitgeist of a rapidly changing American landscape. With an all-star lineup that includes Richard Pryor, Bill Duke, George Carlin, the Pointer Sisters, and Franklyn Ajaye, <i>Car Wash</i> is a hilariously biting flick with an ineffable joie de vivre, and also a major milestone in ‘70s Black studio filmmaking.</p>
-         </div>
+          <p><b>June 30th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=8844C315-6CAC-412C-BFED-E28B63B8F2D9" target="_blank">here</a></u>.</b></p>
+          <p><b>July 1st tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=7D7F31FE-6E57-4F8D-91B6-59AB90010A9F" target="_blank">here</a></u>.</b></p>
+        </div>
 
         <div id="a-star-is-born" class="anchor screening">
           <h1>7:00PM Friday, July 7th; 4:00PM Saturday, July 8th</h1>
@@ -56,6 +58,8 @@
           <h2>A Star is Born (1976)</h2>
           <h3>Frank Pierson &middot; 139m &middot; DCP</h3>
           <p>Not the first nor the last time this story would be told, but the only version written by Joan Didion. Need we say more? Sure, why not. The 1976  <i>A Star Is Born</i> is the most recognizable adaptation of the classic story despite a generation accustomed to picturing Lady Gaga and Bradley Cooper in the starring roles. Barbara Streisand provides the tunes and Kris Kristofferson brings the raw '70s male sex appeal in the electric story of Esther and John: brought together by rock-and-roll, torn apart by alcohol and jealousy.</p>
+          <p><b>July 7th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=8487B4C1-76BC-4E7C-B926-227D5F3A07D3" target="_blank">here</a></u>.</b></p>
+          <p><b>July 8th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=2C2D693D-0E94-4AD3-ADA6-E00F5A229415" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="welcome-to-la" class="anchor screening">
@@ -64,6 +68,8 @@
           <h2>Welcome to L.A. (1976)</h2>
           <h3>Alan Rudolph &middot; 106m &middot; DCP</h3>
           <p>Want to get to know the city of one-night stands? Here’s your ticket. Keith Carradine, Sissy Spacek, and Harvey Keitel will take you on quite the tour of trysts, affairs, break-ups, and flings. Ostensibly a tale of the music world’s hollow core and a great excuse to look at absolutely miserable hot people, <i>Welcome to L.A.</i> is the cinematic embodiment of <i>summertime sadness</i> (I had to do it). A directorial debut for Alan Rudolph, the AD who cut his teeth on <i>The Long Goodbye</i> and <i>Nashville</i>, this one explodes with a wicked ensemble and a memorable score.</p>
+          <p><b>July 14th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=03E9A5AC-12E9-42DB-96AB-E1B77B35A7B8" target="_blank">here</a></u>.</b></p>
+          <p><b>July 15th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E7FA8C23-49B7-4B25-961C-7DE98BF6668A" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="beyond-the-valley-of-the-dolls" class="anchor screening">
@@ -72,6 +78,8 @@
           <h2>Beyond the Valley of the Dolls (1970)</h2>
           <h3>Russ Meyer &middot; 109m &middot; DCP</h3>
           <p>We tried writing a description of this one… and failed miserably. Here’s some of the original promo material instead: “The First Of The Shock Rock! Russ Meyer promised to make the wildest, craziest, funniest, the farthest out Musical-Horror-Sex-Comedy ever released. He has succeeded.” “This is not a sequel– there has NEVER been anything like it!” “The world is full of them, the super-octane girls who are old at twenty… if they get to be twenty.” “This time… they’ve really gone <i>Beyond the Valley of the Dolls</i>.”</p>
+          <p><b>July 21st tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=D82B753B-5652-4635-AB5C-4CA4159DE83D" target="_blank">here</a></u>.</b></p>
+          <p><b>July 22nd tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=C0B5BE27-9C48-475C-8516-82D54CDF6E77" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="play-it-as-it-lays" class="anchor screening">
@@ -80,6 +88,8 @@
           <h2>Play It As It Lays (1972)</h2>
           <h3>Frank Perry &middot; 99m &middot; 35mm</h3>
           <p>Here at Doc, we've got Didion fever! Rarely screened publicly, <i>Play It as It Lays</i> captures the sun-drenched burnout of the classic 1970 novel of the same name. Maria, a restless actress brilliantly played by Tuesday Weld, finds herself in a loveless marriage and near constant mental turmoil as she travels back and forth between L.A. and Vegas. Expressing a distinct Didion brand of nihilism, Frank Perry’s <i>Play It as It Lays</i> is perhaps the only big screen adaptation to do the late writer's work justice.</p>
+          <p><b>July 28th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=52C5E3BC-6C0A-4B88-8E20-BE8CE1DEAFA9" target="_blank">here</a></u>.</b></p>
+          <p><b>July 29th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=C6C4F24A-84AF-4D30-8896-28F4CDED38CC" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="point-blank" class="anchor screening">
@@ -88,6 +98,8 @@
           <h2>Point Blank (1976)</h2>
           <h3>John Boorman &middot; 92m &middot; 35mm</h3>
           <p>Betrayed by the man he thought he knew best, Walker (Lee Marvin) returns to L.A. seeking revenge only to be caught up in an endless hunt for his ex-partner, Reese (John Vernon). Equal parts American genre film and European arthouse, <i>Point Blank</i> contains the nihilism of a man who has lost himself in the face of constant double-crossing and is plagued by the remnants of the Second World War. With stellar photography by Philip Lathrop, John Boorman’s major breakthrough is today an enshrined classic that helped to usher in the new age of hard-boiled American crime cinema.</p>
+          <p><b>August 4th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=22666EE9-8AC2-4156-9B4E-48BE8626BC4C" target="_blank">here</a></u>.</b></p>
+          <p><b>August 5th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=EFE529CF-49A4-4B33-BF53-9A673ED0AE58" target="_blank">here</a></u>.</b></p>
         </div>
 
         <div id="once-upon-a-time-in-hollywood" class="anchor screening">
@@ -96,6 +108,8 @@
           <h2>Once Upon a Time in Hollywood (2019)</h2>
           <h3>Quentin Tarantino &middot; 160m &middot; 35mm</h3>
           <p>Okay, but seriously, let’s just have some fun with this last one. Rick Dalton (Leo DiCaprio) and his stunt double Cliff Booth (Brad Pitt) navigate the rapidly declining studio film industry as foreign and indie films seem more promising than ever before. Cliff and Rick rub elbows with all the So-Cal bigwigs and freaks: Sharon Tate, Polanski, Bruce Lee, Mama Cass, and a guy named Charlie Manson who seems to be planning something big. Tarantino’s chillest film to date, <i>Once Upon a Time in Hollywood</i> feels like a perfect summer day ... but remember, something creepy is always lurking in the night.</p>
+          <p><b>August 11th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=45D33CEE-B291-4B3D-9904-A1A85215112D" target="_blank">here</a></u>.</b></p>
+          <p><b>August 12th tickets can be bought <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=DA3F7BD4-1FF9-416C-8701-D1D71D839EEE" target="_blank">here</a></u>.</b></p>
         </div>
         
       </div>
