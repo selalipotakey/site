d86a3d9c25844606b4330d63474eea69a3f6f4a0
selalipotakey <spotakey@uchicago.edu>
2023-06-26 12:29:02 -0500

    pt 2

--- public/calendar/index.php
+++ public/calendar/index.php
@@ -51,7 +51,7 @@
           </div>
 
           <div class="series-tile reverse">
-            <a href="/calendar/special-events"><img src="/images/2023summer/pacifiction-2022.jpg" alt="Pacifiction (2022) still"></a>
+            <a href="/calendar/special-events"><img src="/images/2023summer/pacifiction-2022.jpeg" alt="Pacifiction (2022) still"></a>
             <div class="series-text">
               <a href="/calendar/special-events">
                 <p class="title">Summer 2023 Special Events</p>
