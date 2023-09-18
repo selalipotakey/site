18c2fd4c5070c8f50351b6ecd22ec9ed2affed5f
selalipotakey <spotakey@uchicago.edu>
2023-09-17 21:47:50 -0400

    monday fall

--- public/calendar_files_2023_Spring/monday.php
+++ public/calendar_files_2023_Spring/monday.php
@@ -19,77 +19,76 @@
 
         <div class="text-section">
           <h1>MONDAY - Proto-noir: The Roots of the Film Noir Movement</h1>
-          <h3>Programmed by: Kathleen</h3>
+          <h3>Programmed by: Kathleen Geier</h3>
 
             <p>The cycle of hard-hitting, visually dazzling crime films that we now know as film noir began some time in the 1940s. While there are debates about what the first noir film was, by the end of the 1940s, noir was a thriving artistic movement. Its antecedents were international in scope. Scholars locate the roots of film noir in the German expressionist films of the 1920s, Hollywood gangster films of the 1930s, and hardboiled American crime fiction. Other important influences were French poetic realism, Italian neorealism, and pre-code American crime pictures. TAB This series will look at the cinematic roots of film noir — films made before the 1940s that had strong noir elements, either in terms of style, content, or both. Noir borrowed its visual palette from German expressionism, with its high-contrast lighting, dramatic shadows, and off-angle, deep focus camera shots. The pulp fiction influence is felt most deeply in noir’s morally conflicted protagonists. Ranging from ordinary people driven to desperate acts to outright sociopathic antiheroes, they remain compelling in spite of the terrible things they do. TAB The protonoir films are every bit as thrilling and darkly beautiful as the noir classics that came later, and include masterpieces of expressionism (<i>M</i>) and poetic realism (<i>Le jour se lève</i>), as well as some of Hollywood’s greatest crime films of the 1930s (<i>Scarface</i>, <i>You Only Live Once</i>). Noir remains one of the most enduringly popular film genres, and, as this series shows, owes an enormous debt to the equally brilliant cinematic legacy that inspired it.</p>
             
-            <p><b>Kathleen Geier</b></p>
         </div>
         
         <div class="screening">
           <h2>M (1931)</h2>
-          <img src="/images/2023spring/m-1931.jpg" alt="M (1931) still">
+          <img src="/images/2023fall/m-1931.jpg" alt="M (1931) still">
           <h3>Fritz Lang &middot; 110m &middot; Digital</h3>
           <p>In a city plagued by a shocking series of child murders, the cops and the criminals alike organize to catch the killer. Scripted by Lang’s wife Thea von Harbou, this stunning work was Lang’s first sound film and his own personal favorite. <i>M</i> combines dazzling visual and sonic inventiveness with an incisive critique of an entire society. Peter Lorre’s extraordinary performance as the killer manages to be both terrifying and sympathetic.</p>
-          <h2>7:00PM Monday, October 2nd&middot</h2>
+          <h2>7:00PM Monday, October 2nd</h2>
         </div>
         
         <div class="screening">
           <h2>Scarface (1932)</h2>
-          <img src="/images/2023spring/scarface-1932.jpg" alt="Scarface (1932) still">
+          <img src="/images/2023fall/scarface-1932.jpg" alt="Scarface (1932) still">
           <h3>Howard Hawks &middot; 93m &middot; DCP</h3>
           <p>This wild ride of a gangster film portrays the rise and fall of a Chicago gangster (Paul Muni) modeled on Al Capone. Its gleeful blend of anarchic comedy and brutal violence outraged censors, who held up the release of the film for over a year. But Hawks’ singular vision inspired generations of filmmakers—including François Truffaut, who wrote, “[<i>Scarface</i>] isn’t literature. It may be dance or poetry. It is certainly cinema.”</p>
-          <h2>7:00PM Monday, October 9th&middot</h2>
+          <h2>7:00PM Monday, October 9th</h2>
         </div>
         
         <div class="screening">
           <h2>Le jour se lève (1939)</h2>
-          <img src="/images/2023spring/le-jour-se-lve-1939.jpg" alt="Le jour se lève (1939) still">
+          <img src="/images/2023fall/le-jour-se-leve-1939.jpg" alt="Le jour se lève (1939) still">
           <h3>Marcel Carné &middot; 93m &middot; DCP</h3>
           <p>A factory worker (Jean Gabin) kills a man, then holes himself up in his apartment. As he awaits the police, he thinks back on the events that turned him into a murderer. Scripted by the poet Jaques Prévert, this melancholy masterpiece of poetic realism is distinguished by its gorgeously atmospheric filmmaking and Gabin’s utterly iconic performance. Its aura of fatalistic despair parallels the mood of France on the brink of world war.</p>
-          <h2>7:00PM Monday, October 16th&middot</h2>
+          <h2>7:00PM Monday, October 16th</h2>
         </div>
         
         <div class="screening">
           <h2>I Am a Fugitive from a Chain Gang (1932)</h2>
-          <img src="/images/2023spring/i-am-a-fugitive-from-a-chain-gang-1932.jpg" alt="I Am a Fugitive from a Chain Gang (1932) still">
+          <img src="/images/2023fall/i-am-a-fugitive-from-a-chain-gang-1932.jpg" alt="I Am a Fugitive from a Chain Gang (1932) still">
           <h3>Mervyn LeRoy &middot; 92m &middot; 35mm</h3>
           
           <p><i>Preserved by the Library of Congress</i></p><p>Down-on-his-luck World War I vet James Allen is railroaded onto a brutal Southern chain gang for a crime he didn’t commit. He escapes to Chicago, but after his past catches up to him, he is forced back to a life on the run. Based on a true story, this gripping and harrowing film is probably the best of the Warner Brothers’ hardboiled Depression-era social conscience pictures. Its gut-punch of an ending is one of the greatest in Hollywood history.</p>
-          <h2>7:00PM Monday, October 23rd&middot</h2>
+          <h2>7:00PM Monday, October 23rd</h2>
         </div>
         
         <div class="screening">
           <h2>Heat Lightning (1934)</h2>
-          <img src="/images/2023spring/heat-lightning-1934.jpg" alt="Heat Lightning (1934) still">
+          <img src="/images/2023fall/heat-lightning-1934.jpg" alt="Heat Lightning (1934) still">
           <h3>Mervyn LeRoy &middot; 63m &middot; 35mm</h3>
           
           <p><i>Preserved by the Library of Congress.</i></p><p>In this protofeminist protonoir, Olga (Aline MacMahon) and her sister (Ann Dvorak) run a gas station in the middle of the Mojave desert. When a shady character from Olga’s past shows up, the sisters’ hard-won independence is threatened. Packed with drama, lust, and sweaty longing, and featuring arresting cinematography, a powerful performance by Method pioneer MacMahon, and a shocking ending, this is a pre-code gem you won’t want to miss.</p>
-          <h2>7:00PM Monday, October 30th&middot</h2>
+          <h2>7:00PM Monday, October 30th</h2>
         </div>
         
         <div class="screening">
           <h2>Sabotage (1936)</h2>
-          <img src="/images/2023spring/sabotage-1936.jpg" alt="Sabotage (1936) still">
+          <img src="/images/2023fall/sabotage-1936.jpg" alt="Sabotage (1936) still">
           <h3>Alfred Hitchcock &middot; 76m &middot; 35mm</h3>
           <p>This loose adaptation of Joseph Conrad’s <i>The Secret Agent</i> is one of Hitchcock’s darkest films. Sylvia Sidney stars as a woman who is horrified to discover that her husband is a terrorist agent. <i>Sabotage</i> was not a box office hit and Hitch himself later felt that its famous “boy with the bomb” sequence was a mistake. However, its stature has grown over the years, and critics such as Pauline Kael have hailed it as the masterpiece of his British period.</p>
-          <h2>7:00PM Monday, November 6th&middot</h2>
+          <h2>7:00PM Monday, November 6th</h2>
         </div>
         
         <div class="screening">
           <h2>You Only Live Once (1937)</h2>
-          <img src="/images/2023spring/you-only-live-once-1937.jpg" alt="You Only Live Once (1937) still">
+          <img src="/images/2023fall/you-only-live-once-1937.jpg" alt="You Only Live Once (1937) still">
           <h3>Fritz Lang &middot; 86m &middot; DCP</h3>
           <p>Reformed ex-convict Eddie Taylor (Henry Fonda) can’t catch a break; when he’s framed for murder, he goes on the lam with his lawyer’s sympathetic secretary (Sylvia Sidney). Fritz Lang’s haunting, socially conscious melodrama has been called his best American film. Critic Dave Kehr noted that Lang’s style here had a huge influence on film noir: “it’s always night, usually raining, and the camera hovers over the characters like the heavy hand of fate.”</p>
-          <h2>7:00PM Monday, November 13th&middot</h2>
+          <h2>7:00PM Monday, November 13th</h2>
         </div>
         
         <div class="screening">
           <h2>Piccadilly (1929)</h2>
-          <img src="/images/2023spring/piccadilly-1929.jpg" alt="Piccadilly (1929) still">
+          <img src="/images/2023fall/piccadilly-1929.jpg" alt="Piccadilly (1929) still">
           <h3>E.A. Dupont &middot; 109m &middot; 35mm</h3>
           <p>Though she began her career in Hollywood, the groundbreaking Chinese-American actress Anna May Wong found her greatest roles abroad. In <i>Piccadilly</i>, she gives a mesmerizing performance as a dancer whose involvement in an interracial romance becomes her undoing. Made in England and helmed by German expressionist master E.A. Dupont, Piccadilly boasts dazzling camera work, spectacular sets and costumes, and a pervasive atmosphere of erotic doom.</p>
-          <h2>7:00PM Monday, November 27th&middot</h2>
+          <h2>7:00PM Monday, November 27th</h2>
         </div>
         
       </div>
