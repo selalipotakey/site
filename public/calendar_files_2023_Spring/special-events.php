56db658e1c325b5cc77716104bc4e48dfc6451a7
selalipotakey <spotakey@uchicago.edu>
2023-09-17 22:04:29 -0400

    special events fall

--- public/calendar_files_2023_Spring/special-events.php
+++ public/calendar_files_2023_Spring/special-events.php
@@ -24,70 +24,68 @@
         
         <div class="screening">
           <h2>The Blues Brothers (1980)</h2>
-          <img src="/images/2023spring/the-blues-brothers-1980.jpg" alt="The Blues Brothers (1980) still">
+          <img src="/images/2023fall/the-blues-brothers-1980.jpg" alt="The Blues Brothers (1980) still">
           <h3>John Landis &middot; 133m &middot; DCP</h3>
           
           <p><i>A Night Owls Event, with Professors Agnes Callard and Daniel Morgan. Free with Night Owls QR Code Sign-in.</i></p><p>Newly released ex-con Jake Blues reunites with his brother Elwood, and the titular Blues Brothers embark on a mission to raise enough money to save their childhood orphanage. Jake and Elwood set out to bring their old band back together, but the process is not without obstacles: the brothers careen around Chicago in a chaotic frenzy of explosive car chases, trains that come a little too close for comfort, and a bevy of enemies including the police.</p>
-          <h2>6:30PM Monday, September 25th&middot</h2>
+          <h2>6:30PM Monday, September 25th</h2>
         </div>
         
         <div class="screening">
           <h2>Taxi Driver (1976)</h2>
-          <img src="/images/2023spring/taxi-driver-1976.jpg" alt="Taxi Driver (1976) still">
+          <img src="/images/2023fall/taxi-driver-1976.jpg" alt="Taxi Driver (1976) still">
           <h3>Martin Scorsese &middot; 114m &middot; 35mm</h3>
           <p>Disturbed insomniac Travis Bickle (Robert De Niro) roams the grimy underbelly of 70s New York City as a taxi driver, taking chagrined notice of what he sees as repulsive social decay. Between visits to porn theaters, noting down witticisms in his diary, and an obsessive infatuation with a campaign volunteer (Cybill Shepard), Bickle becomes fixated on purging social ills by means of rescuing adolescent sex worker Iris (Jodie Foster) from her sinister pimp.</p>
-          <h2>4:00PM Saturday, September 30th&middot; 3:00PM Sunday, October 1st&middot</h2>
+          <h2>4:00PM Saturday, September 30th&middot; 3:00PM Sunday, October 1st</h2>
         </div>
         
         <div class="screening">
           <h2>Swimming Pool (2003)</h2>
-          <img src="/images/2023spring/swimming-pool-2003.jpg" alt="Swimming Pool (2003) still">
+          <img src="/images/2023fall/swimming-pool-2003.jpg" alt="Swimming Pool (2003) still">
           <h3>François Ozon &middot; 102m &middot; 35mm</h3>
           <p>20 years ago, a couple had their first date at Doc Films. The movie was Ozon’s <i>Swimming Pool</i> … Now here we are, hopefully with a romantic prospect by our side. Because there's no better date-night activity than watching this sizzling, slow-burn erotic thriller starring Charlotte Rampling as a writer whose holiday takes a turn when an unexpected guest shows up. Here’s to Meredith and Alex — may we all find true love at the movies, especially at Doc!</p>
-          <h2>7:00PM Thursday, October 19th&middot</h2>
+          <h2>7:00PM Thursday, October 19th</h2>
         </div>
         
         <div class="screening">
           <h2>La La Land (2016)</h2>
-          <img src="/images/2023spring/la-la-land-2016.jpg" alt="La La Land (2016) still">
+          <img src="/images/2023fall/la-la-land-2016.jpg" alt="La La Land (2016) still">
           <h3>Damien Chazelle &middot; 128m &middot; DCP</h3>
           <p>And this one’s for the fools who dream. Over the years, <i>La La Land</i> has garnered quite the reputation, whether for its iconic dance sequences, accolades, or the infamous Best Picture snafu. This dazzling musical drama centers Mia (Emma Stone) and Sebastian (Ryan Gosling), two determined souls looking to achieve their dreams in the city of stars. A story of both enchantment and heartache, <i>La La Land</i> is the perfect love letter to the road to stardom.</p>
-          <h2>7:00PM Saturday, September 30th&middot</h2>
+          <h2>7:00PM Saturday, September 30th</h2>
         </div>
         
         <div class="screening">
           <h2>The Exorcist (1973)</h2>
-          <img src="/images/2023spring/the-exorcist-1973.jpg" alt="The Exorcist (1973) still">
+          <img src="/images/2023fall/the-exorcist-1973.jpg" alt="The Exorcist (1973) still">
           <h3>William Friedkin &middot; 122m &middot; DCP</h3>
           <p>Based on a true story, <i>The Exorcist</i> ventures "somewhere between science and superstition" in the tale of an innocent young girl possessed by a foul-mouthed demon. Her mother desperately seeks help for her levitating, babbling daughter, but finds no answers in medicine--leaving Regan's salvation in the hands of two Catholic priests who must embark on an exorcism. Come for the thrilling religious horror, stay for the iconic torrents of green slime.</p>
-          <h2>9:30PM Tuesday, October 31st&middot</h2>
+          <h2>9:30PM Tuesday, October 31st</h2>
         </div>
         
         <div class="screening">
           <h2>Bad Black (2016)</h2>
-          <img src="/images/2023spring/bad-black-2016.jpg" alt="Bad Black (2016) still">
+          <img src="/images/2023fall/bad-black-2016.jpg" alt="Bad Black (2016) still">
           <h3>Nabwana Isaac Godfrey Geoffrey &middot; 68m &middot; DCP</h3>
           <p>From the Ugandan slum studio Wakaliwood of cult <i>Who Killed Captain Alex?</i> fame comes this gonzo action-comedy. After Bad Black (Nalwanga Gloria), the biggest mafiosa in Uganda’s capital city, steals from an American doctor, he must team up with Kung fu kid Wesley Snipes (Rolean Kasule) to take back his family heirloom. Bear witness to the dizzying action and witty script born of a self-taught MacGyver director and a whopping $65 budget.</p>
-          <h2>7:00PM Saturday, November 18th&middot; 3:00PM Sunday, November 19th&middot</h2>
+          <h2>7:00PM Saturday, November 18th&middot; 3:00PM Sunday, November 19th</h2>
         </div>
         
         <div class="screening">
-          <h2>Mothra Vs. Godzilla (1964) // Ghidora, The Three-Headed Monster (1964)</h2>
-          <img src="/images/2023spring/mothra-vs-godzilla-1964.jpg" alt="Mothra Vs. Godzilla (1964) // Ghidora, The Three-Headed Monster (1964) still">
-          <h3>Ishiro Honda // Ishiro Honda &middot; 89m // 93m &middot; DCP</h3>
+          <h2>Mothra Vs. Godzilla // Ghidora, The Three-Headed Monster (1964)</h2>
+          <img src="/images/2023fall/mothra-vs-godzilla-1964.jpg" alt="Mothra Vs. Godzilla (1964) // Ghidora, The Three-Headed Monster (1964) still">
+          <h3>Ishiro Honda &middot; 89m // 93m &middot; DCP</h3>
           
-          <p><i>(Sponsored by X. or other notes TBD but almost definitely coming).
-
-Sponsored by The Brothers Valauskas. A companion feature to the exhibit Bibliosaurus! Exclusively at Special Collections in the Regenstein Library, opening January 2, 2024, through April 26.</i></p><p><i>Mothra</i> is a fantastical showcase of the Showa Era's flashy battes and goofy but impressive practical effects. It's also a critique of corporate Japan and the envrionmental toll of nuclear testing. Plus, <i>Ghidora</i> smashes in with an all-star Kaiju cast, as Rodan, Mothra, and Godzilla team up against the titular titan. With aliens, assassins, and political intrigue, <i>Ghidora</i> (and <i>Mothra!</i>) make for a one-of-a-kind experience to see on the big screen.</p>
-          <h2>2:00PM Saturday, December 2nd&middot</h2>
+          <p><i>Sponsored by The Brothers Valauskas. A companion feature to the exhibit Bibliosaurus! Exclusively at Special Collections in the Regenstein Library, opening January 2, 2024, through April 26.</i></p><p><i>Mothra</i> is a fantastical showcase of the Showa Era's flashy battes and goofy but impressive practical effects. It's also a critique of corporate Japan and the envrionmental toll of nuclear testing. Plus, <i>Ghidora</i> smashes in with an all-star Kaiju cast, as Rodan, Mothra, and Godzilla team up against the titular titan. With aliens, assassins, and political intrigue, <i>Ghidora</i> (and <i>Mothra!</i>) make for a one-of-a-kind experience to see on the big screen.</p>
+          <h2>2:00PM Saturday, December 2nd</h2>
         </div>
         
         <div class="screening">
           <h2>Full Metal Jacket (1987)</h2>
-          <img src="/images/2023spring/full-metal-jacket-1987.jpg" alt="Full Metal Jacket (1987) still">
+          <img src="/images/2023fall/full-metal-jacket-1987.jpg" alt="Full Metal Jacket (1987) still">
           <h3>Stanley Kubrick &middot; 116m &middot; 35mm</h3>
           <p>Based on a 1979 novel, Stanley Kubrick's <i>Full Metal Jacket</i> follows two privates as they struggle through rigorous military training, each earning a pejorative nickname from their hard-as-nails drill sergeant: 'Joker'  (Matthew Modine) and 'Pyle' (Vincent D'Onofrio). When Joker graduates to a journalist and is sent into the thick of the battlefield, he finds himself ensconced within the Battle of Hué, facing danger, loss, and moral dilemmas.</p>
-          <h2>7:00PM Saturday, December 2nd&middot; 3:00PM Sunday, December 3rd&middot</h2>
+          <h2>7:00PM Saturday, December 2nd&middot; 3:00PM Sunday, December 3rd</h2>
         </div>
         
       </div>
