7f68d0b22700e3596eb48f5bfb731340d54f1065
selalipotakey <spotakey@uchicago.edu>
2023-09-17 22:34:18 -0400

    wednesday fall

--- public/calendar_files_2023_Spring/wednesday.php
+++ public/calendar_files_2023_Spring/wednesday.php
@@ -19,83 +19,82 @@
 
         <div class="text-section">
           <h1>WEDNESDAY - The Films of Ang Lee</h1>
-          <h3>Programmed by: Wei</h3>
+          <h3>Programmed by: Wei Lu</h3>
 
             <p>Born in 1954, Ang Lee is the oldest son of Sheng Lee, a middle school principal who moved to Taiwan following the Chinese civil war. Despite strong opposition from his father, Lee went on to study drama and film production in the United States after graduating from a three-year college in Taiwan. This estranged relationship with his traditional authoritarian father has had a huge impact on Lee’s films—most prominently in his “Father Knows Best” trilogy, which includes his debut feature film, <i>Pushing Hands</i>. Even in later films like <i>Life of Pi</i>, we can see the same individual struggle to reconcile generational tension. TAB Lee considered himself “a drifter and an outsider” throughout his life, being a mainlander in Taiwan and an immigrant in the United States. His zigzagging personal trajectory enables him to be more aware of cultural nuance and venture into the realm of East and West. Who could have imagined that the film adaptations of Eileen Chang’s<i> Lust Caution</i> and Jane Austen’s <i>Sense and Sensibility</i> were from the same director? Even within the well-defined Western film genre, his bold infusion of gay romance in <i>Brokeback Mountain</i> subverted the traditional “American dream” core, receiving both critical acclaim and commercial success. Aside from the intricate depiction of emotional turmoil and cultural exploration, Lee’s films also embrace many technical innovations, from the CGI in <i>Hulk</i> to his latest experiments with high frame rate. Lee refuses to be stereotyped, and continues to be a permanent student of film. Come to Doc and enjoy the diverse filmography within this Ang Lee retrospective!</p>
             
-            <p><b>Wei Lu </b></p>
         </div>
         
         <div class="screening">
           <h2>Crouching Tiger, Hidden Dragon (2000)</h2>
-          <img src="/images/2023spring/crouching-tiger-hidden-dragon-2000.jpg" alt="Crouching Tiger, Hidden Dragon (2000) still">
+          <img src="/images/2023fall/crouching-tiger-hidden-dragon-2000.jpg" alt="Crouching Tiger, Hidden Dragon (2000) still">
           <h3>Ang Lee &middot; 120m &middot; 35mm</h3>
           <p>One of the most acclaimed films of the 21st-century, <i>Crouching Tiger, Hidden Dragon</i> follows a stolen mystical sword and two warriors’ (Chow Yun-fat and Michelle Yeoh) quest to retrieve it. Grand in its scope and mesmerizing in its execution—in part due to Yuen Woo-Ping’s intricate, masterfully-crafted fight choreography—Ang Lee’s film is an enthralling visual experience not to be missed.</p>
-          <h2>7:00PM Wednesday, September 27th&middot</h2>
+          <h2>7:00PM Wednesday, September 27th</h2>
         </div>
         
         <div class="screening">
           <h2>Brokeback Mountain (2005)</h2>
-          <img src="/images/2023spring/brokeback-mountain-2005.jpg" alt="Brokeback Mountain (2005) still">
+          <img src="/images/2023fall/brokeback-mountain-2005.jpg" alt="Brokeback Mountain (2005) still">
           <h3>Ang Lee &middot; 134m &middot; 35mm</h3>
           <p>It’s hard to overstate the significance of <i>Brokeback Mountain</i>. Devastatingly beautiful and incredibly profound, the film revolves around the secret, decades-long relationship between cowboys Ennis Del Mar (Heath Ledger) and Jack Twist (Jack Gyllenhaal). <i>Brokeback Mountain</i> is both a brilliant and important film that won Ang Lee the Academy Award for Best Director and proved that serious LGBTQ stories belong in mainstream media. Doc was actually the Chicago premiere for this film!</p>
-          <h2>7:00PM Wednesday, October 4th&middot; 12:00PM Sunday, October 8th&middot</h2>
+          <h2>7:00PM Wednesday, October 4th&middot; 12:00PM Sunday, October 8th</h2>
         </div>
         
         <div class="screening">
           <h2>Sense and Sensibility (1995)</h2>
-          <img src="/images/2023spring/sense-and-sensibility-1995.jpg" alt="Sense and Sensibility (1995) still">
+          <img src="/images/2023fall/sense-and-sensibility-1995.jpg" alt="Sense and Sensibility (1995) still">
           <h3>Ang Lee &middot; 136m &middot; 35mm</h3>
           <p>Sisters Elinor (Emma Thompson) and Marianne (Kate Winslet) grapple with financial hardship, societal expectations, and their different approaches to love. Thompson won the Academy Award for her screenplay, making her the only person to have won an Oscar for both screenwriting and acting. A tender and witty film,<i>Sense and Sensibility</i> demonstrates Ang Lee’s versatility as a director and is widely considered one of the best adaptations of an Austen work.</p>
-          <h2>7:00PM Wednesday, October 11th&middot</h2>
+          <h2>7:00PM Wednesday, October 11th</h2>
         </div>
         
         <div class="screening">
           <h2>Life of Pi (2012)</h2>
-          <img src="/images/2023spring/life-of-pi-2012.jpg" alt="Life of Pi (2012) still">
+          <img src="/images/2023fall/life-of-pi-2012.jpg" alt="Life of Pi (2012) still">
           <h3>Ang Lee &middot; 127m &middot; DCP</h3>
           <p>Ang Lee’s <i>Life of Pi</i>, based on Yann Martel’s novel of the same name, is perhaps his most gorgeous film. Deeply thought-provoking and emotionally rich, it depicts the adventure of a boy nicknamed Pi stranded in the middle of the ocean…with a Bengal tiger. The film's breathtaking visuals and philosophical narrative earned it critical acclaim and several Academy Awards, including Best Director. Come for the tiger, stay for a hopeful, beautiful experience.</p>
-          <h2>7:00PM Wednesday, October 18th&middot</h2>
+          <h2>7:00PM Wednesday, October 18th</h2>
         </div>
         
         <div class="screening">
           <h2>The Ice Storm (1997)</h2>
-          <img src="/images/2023spring/the-ice-storm-1997.jpg" alt="The Ice Storm (1997) still">
+          <img src="/images/2023fall/the-ice-storm-1997.jpg" alt="The Ice Storm (1997) still">
           <h3>Ang Lee &middot; 112m &middot; 35mm</h3>
           <p>With a plot containing enough family dysfunction to rival, and to some—<i>surpass</i>— <i>American Beauty</i>, <i>The Ice Storm</i> is a study on bleak, melancholic suburbia. Over the course of a Thanksgiving weekend (and under the threat of a severe ice storm), two families deal with adultery, substance abuse, and isolation in a time of personal and social unrest. <i>The Ice Storm</i> is not only among the best of Lee’s work, but is also one the finest films of its decade.</p>
-          <h2>7:00PM Wednesday, October 25th&middot; 4:00PM Saturday, October 28th&middot</h2>
+          <h2>7:00PM Wednesday, October 25th&middot; 4:00PM Saturday, October 28th</h2>
         </div>
         
         <div class="screening">
           <h2>Lust, Caution (2007)</h2>
-          <img src="/images/2023spring/lust-caution-2007.jpg" alt="Lust, Caution (2007) still">
+          <img src="/images/2023fall/lust-caution-2007.jpg" alt="Lust, Caution (2007) still">
           <h3>Ang Lee &middot; 158m &middot; 35mm</h3>
           <p>This erotic espionage thriller won Ang Lee his second Golden Lion at the Venice Film Festival. Tang Wei stars as Wong Chia Chi, a young university student who joins a resistance against the Japanese puppet government in Shanghai. She is tasked with seducing and killing Mr. Yee (Tony Leung), a high-ranking official, but as the assassination plot advances, Chia Chi’s loyalty to the resistance—and her feelings for Mr. Yee—become increasingly complicated.</p>
-          <h2>7:00PM Wednesday, November 1st&middot</h2>
+          <h2>7:00PM Wednesday, November 1st</h2>
         </div>
         
         <div class="screening">
           <h2>Pushing Hands (1991)</h2>
-          <img src="/images/2023spring/pushing-hands-1991.jpg" alt="Pushing Hands (1991) still">
+          <img src="/images/2023fall/pushing-hands-1991.jpg" alt="Pushing Hands (1991) still">
           <h3>Ang Lee &middot; 105m &middot; DCP</h3>
           <p>More than 30 years later, Ang Lee’s feature debut remains a testament to his skill as a director. The first in his "Father Knows Best" trilogy, <i>Pushing Hands</i> is centered on Mr. Chu—a Tai Chi master—who moves from Beijing to New York to stay with his son’s family. It's a warm and touching film, shaped by themes like clashing cultures and generational differences, which continue to appear in Lee’s later works.</p>
-          <h2>7:00PM Wednesday, November 8th&middot</h2>
+          <h2>7:00PM Wednesday, November 8th</h2>
         </div>
         
         <div class="screening">
           <h2>Ride with the Devil (1999)</h2>
-          <img src="/images/2023spring/ride-with-the-devil-1999.jpg" alt="Ride with the Devil (1999) still">
+          <img src="/images/2023fall/ride-with-the-devil-1999.jpg" alt="Ride with the Devil (1999) still">
           <h3>Ang Lee &middot; 138m &middot; Digital</h3>
           <p>Ang Lee’s (non-typical) take on the Western! Set during the American Civil War, it follows friends Jake Roedel (Tobey Maguire) and Jack Bull Chiles (Skeet Ulrich) who join the Irregulars, a guerilla warfare unit fighting for the Confederacy. Notable for its exploration of unconventional narratives, an intimate, character-driven story, and Frederick Elmes’ beautiful cinematography,<i>Ride with the Devil</i>is one of Lee’s most interesting films.</p>
-          <h2>7:00PM Wednesday, November 15th&middot</h2>
+          <h2>7:00PM Wednesday, November 15th</h2>
         </div>
         
         <div class="screening">
           <h2>Hulk (2003)</h2>
-          <img src="/images/2023spring/hulk-2003.jpg" alt="Hulk (2003) still">
+          <img src="/images/2023fall/hulk-2003.jpg" alt="Hulk (2003) still">
           <h3>Ang Lee &middot; 138m &middot; 35mm</h3>
           <p>Visually ambitious and incredibly dynamic, <i>Hulk</i> is unlike any other superhero film, bursting with bold creativity and technical innovation. It explores the origins of Hulk—the green-skinned monster that Dr. Bruce Banner transforms into after an experiment gone wrong—and is noteworthy for its portrayal of the character’s emotional aspects. Ang Lee’s <i>Hulk</i> is a must-see for those wanting both thrilling action and a nuanced depiction of Bruce Banner.</p>
-          <h2>7:00PM Wednesday, November 29th&middot</h2>
+          <h2>7:00PM Wednesday, November 29th</h2>
         </div>
         
       </div>
