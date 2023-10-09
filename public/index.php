6ebacf21d912283931a35c8791fb75a52cb58356
selalipotakey <spotakey@uchicago.edu>
2023-10-08 23:13:55 -0500

    ..

--- public/index.php
+++ public/index.php
@@ -104,7 +104,7 @@
           </a>
           <h3>Howard Hawks &middot; 93m &middot; DCP</h3>
           <p>This wild ride of a gangster film portrays the rise and fall of a Chicago gangster (Paul Muni) modeled on Al Capone. Its gleeful blend of anarchic comedy and brutal violence outraged censors, who held up the release of the film for over a year. But Hawks’ singular vision inspired generations of filmmakers—including François Truffaut, who wrote, “[<i>Scarface</i>] isn’t literature. It may be dance or poetry. It is certainly cinema.”</p>
-          <h2>Monday, October 9th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=73FE7067-9420-4219-B541-5C609A46C62A" target="_blank">7:00PM</a></u></h2>
+          <h3>Monday, October 9th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=73FE7067-9420-4219-B541-5C609A46C62A" target="_blank">7:00PM</a></u></h3>
         </div>
 
         <div id="wise-blood" class="anchor screening">
@@ -114,7 +114,7 @@
           </a>
           <h3>John Huston &middot; 106m &middot; Digital</h3>
           <p>John Huston’s <i>Wise Blood</i> captures the absurd, grotesque, yet revelatory tenor of Flannery O’Connor’s acclaimed novel. Wild-eyed Hazel Motes (Brad Dourif) becomes a self-proclaimed prophet of the Church Without Christ: “where the blind don’t see and the lame don’t walk and what’s dead stays that way." Motes battles a rival street preacher (Harry Dean Stanton) for souls - including his own.</p>
-          <h2>Tuesday, October 10th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=97B68625-C8F3-4051-8621-26DA78EBD65F" target="_blank">7:00PM</a></u></h2>
+          <h3>Tuesday, October 10th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=97B68625-C8F3-4051-8621-26DA78EBD65F" target="_blank">7:00PM</a></u></h3>
         </div>
         
         <div id="sense-and-sensibility" class="anchor screening">
@@ -124,7 +124,7 @@
           </a>
           <h3>Ang Lee &middot; 136m &middot; 35mm</h3>
           <p>Sisters Elinor (Emma Thompson) and Marianne (Kate Winslet) grapple with financial hardship, societal expectations, and their different approaches to love. Thompson won the Academy Award for her screenplay, making her the only person to have won an Oscar for both screenwriting and acting. A tender and witty film,<i>Sense and Sensibility</i> demonstrates Ang Lee’s versatility as a director and is widely considered one of the best adaptations of an Austen work.</p>
-          <h2>Wednesday, October 11th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=140BC31E-4733-4851-9C5E-0975A9034D63" target="_blank">7:00PM</a></u></h2>
+          <h3>Wednesday, October 11th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=140BC31E-4733-4851-9C5E-0975A9034D63" target="_blank">7:00PM</a></u></h3>
         </div>
 
         <div id="trainspotting" class="anchor screening">
@@ -134,8 +134,8 @@
           </a>
           <h3>Danny Boyle &middot; 93m &middot; DCP</h3>
           <p>Cool Britannia’s heroin addiction comedy, <i>Trainspotting</i> takes the highs and lows of junkie Mark Renton (Ewan McGregor) and throws on a mountain of nightmarish pop culture that would give the Scottish Highlands an inferiority complex. Bringing the thick Scottish brogue of Irvine Welsh’s cult novel to the silver screen, <i>Trainspotting</i> invites you to dive into ‘80s Edinburgh—all to the tune of Iggy Pop, Goldie, Blur and Underworld (to name a few).</p>
-          <h2>Thursday, October 12th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E5D37239-C269-4DD9-A03A-A5EEB46028E5" target="_blank">7:00PM</a></u> &middot; 
-              Sunday, October 15th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=979E377D-D5C0-4D0E-ABC2-C82E224AA848" target="_blank">4:30PM</a></u></h2>
+          <h3>Thursday, October 12th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E5D37239-C269-4DD9-A03A-A5EEB46028E5" target="_blank">7:00PM</a></u> &middot; 
+              Sunday, October 15th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=979E377D-D5C0-4D0E-ABC2-C82E224AA848" target="_blank">4:30PM</a></u></h3>
         </div>
 
         <div id="basket-case" class="anchor screening">
@@ -145,7 +145,7 @@
           </a>
           <h3>Frank Henenlotter &middot; 91m &middot; DCP</h3>
           <p>Shot on 16mm and a low budget, <i>Basket Case</i> has become a cult classic horror movie. Duane Bradley goes around New York City carrying a mutant creature who can talk to him telepathically in a basket. Together, they are on a mission to get revenge on the doctor that did them wrong in the past. How are they related, and what wrong was done? <i>Basket Case</i> is a classic good time with cheap but surprisingly good gore effects and an atmosphere of grime.</p>
-          <h2>Thursday, October 12th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=412931CD-2443-4542-BC05-600A3E3B9827" target="_blank">9:30PM</a></u></h2>
+          <h3>Thursday, October 12th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=412931CD-2443-4542-BC05-600A3E3B9827" target="_blank">9:30PM</a></u></h3>
         </div>
 
         <div id="a-woman-under-the-influence" class="anchor screening">
@@ -155,8 +155,8 @@
           </a>
           <h3>John Cassavetes &middot; 155m &middot; DCP</h3>
           <p>As lonely housewife Mabel becomes increasingly unhappy and unstable, her husband Nick, an L.A. construction worker, begins to think Mabel will be a danger to their children and himself, despite her love for them. Written by Cassavetes when Gena Rowlands desired a play about the difficulties faced by contemporary women, <i>A Woman Under the Influence</i> contains two of the most wrought and heart-breaking performances of a couple desperately in love but ostensibly incompatible.</p>
-          <h2>Friday, October 13th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E5277679-5B4C-4282-BE71-A668EF3D5C05" target="_blank">7:00PM</a></u> &middot; 
-              Saturday, October 14th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E01A60FA-ABB4-469D-A3E7-06AD1090278D" target="_blank">4:00PM</a></u></h2>
+          <h3>Friday, October 13th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E5277679-5B4C-4282-BE71-A668EF3D5C05" target="_blank">7:00PM</a></u> &middot; 
+              Saturday, October 14th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=E01A60FA-ABB4-469D-A3E7-06AD1090278D" target="_blank">4:00PM</a></u></h3>
         </div>
 
         <div id="spider-man" class="anchor screening">
@@ -166,7 +166,7 @@
           </a>
           <h3>Joaquim Dos Santos, Kemp Powers, and Justin K. Thompson &middot; 140m &middot; DCP</h3>
           <p>This bildungsroman follows two spider-people from different universes, Miles Morales and Gwen Stacey, who refuse to be boxed into a simple hero's arc. The lauded visual and auditory experience created by 1,000 animators, Daniel Pemberton, and Metro Boomin reinforce the film’s narrative-breaking style and masterful execution of the multiverse. The story is so big, it can't fit into one movie. You'll simply have to come back to Doc to catch the next one.</p>
-          <h2>Saturday, October 14th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=0AFB1C65-9169-4DDD-B11A-9A795C0CCC99" target="_blank">7:00PM</a></u></h2>
+          <h3>Saturday, October 14th <u><a href="https://tickets.uchicago.edu/Online/seatSelect.asp?createBO::WSmap=1&BOparam::WSmap::loadBestAvailable::performance_ids=0AFB1C65-9169-4DDD-B11A-9A795C0CCC99" target="_blank">7:00PM</a></u></h3>
         </div>
 
         <div id="de-cierta-manera" class="anchor screening">
@@ -176,7 +176,7 @@
           </a>
           <h3>Sara Gómez // Ruy Guerra &middot; 73m // 80m &middot; DCP</h3>
           <p><i>5 minutes in between films</i></p><p>This double feature shows two radically different approaches to docufiction as a tool to examine post-colonial independence. <i>De cierta manera</i>, completed by a team of filmmakers after director Sara Gómez’s untimely death, foregrounds the love story of Yolanda and Mario, two people formerly separated by race and class following the Cuban Revolution. <i>Mueda</i>, the story of the real-life 1960 massacre of Mozambicans protesting for independence, is both a fiction and a documentary which tracks the development of the massacre’s recreation by locals who lived through the War of Independence.</p>
-          <h2>Sunday, October 15th 6:30PM</h2>
+          <h3>Sunday, October 15th 6:30PM</h3>
         </div>
 
       </div>
