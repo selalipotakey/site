b972ab7ef964d431bbcc415b1d8401543720014c
selalipotakey <spotakey@uchicago.edu>
2023-12-19 18:21:30 -0600

    test

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@
     <main>
 
       <?php $version='1.2'; include "./includes/dropdown.html";?>
-      <div style="background-color: black; padding: 3vw; margin: 0;">
+      <div style="background-color: black; padding: 3vw; width: 100%;">
         <?php $version='1.2'; include "./includes/carousel-1.html";?>
       </div>
       
