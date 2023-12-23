3aada74bbf4a5b43991275f3c19ca8ca54cfe215
selalipotakey <spotakey@uchicago.edu>
2023-12-22 21:29:53 -0600

    test

--- public/index.php
+++ public/index.php
@@ -15,8 +15,7 @@
 
       <?php $version='1.2'; include "./includes/dropdown.html";?>
       <div style="background: url('/images/2024winter/doc_gif_2.gif') no-repeat top center fixed; width: 98vw; height: 100vw; bottom: 0; object-fit: cover;">
-        <h1 style="display: flex; justify-content: center; font-size: 20vw; color: white; margin: 0 auto 0 auto;">WINTER 2024</h2>
-        <span><img src="/images/2024winter/down.png" alt="" style="display: block; margin: auto; width: 4vw;"></span>
+        <h1 style="display: flex; justify-content: center; font-size: 20vw; color: white; margin: 0 auto 0 auto;">WINTER 2024</h1><span><img src="/images/2024winter/down.png" alt="" style="display: block; margin: auto; width: 4vw;"></span>
       </div>
       <?php $version='1.2'; include "./includes/carousel-1.html";?>
 <!--      
