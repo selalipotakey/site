8d50cb7ef9fe1dd6aaf0bacb050d041b88afc6d8
selalipotakey <spotakey@uchicago.edu>
2023-12-22 19:42:57 -0600

    test

--- public/index.php
+++ public/index.php
@@ -68,7 +68,7 @@
       </div>
 
       <div style="background-image: url('/images/2024winter/Doc-gif.gif'); display: block; margin: auto; width: 95vw; height: 20vw; object-fit: cover; object-position: top;">
-        <h1 style="display: flex; justify-content: center; font-size: 10vw;">WINTER 2024</h2>
+        <h1 style="display: flex; justify-content: center; font-size: 10vw; color: white; margin: 0 auto 0 auto;">WINTER 2024</h2>
       </div>
 <!--
         <div id="piccadilly" class="anchor screening">
