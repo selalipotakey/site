031d8165dcd57595a69861440526e03a5fb034ef
selalipotakey <spotakey@uchicago.edu>
2023-12-22 20:13:18 -0600

    test

--- public/index.php
+++ public/index.php
@@ -27,14 +27,9 @@
         </div>
       </div>
 -->
-      <br><br><br>
-      <div style="background: url('/images/2024winter/Doc-gif.gif') no-repeat top center fixed; background-size: cover; display: block; margin: auto; width: 100vw; height: auto; object-fit: cover; object-position: bottom;">
-        <h3 style="display: flex; justify-content: center; font-size: 10vw; color: white; margin: 0 auto 0 auto;">&middot; this week @ doc &middot;</h2>
-      </div>
-
       <div class="home-list">
-        <!--<h1 style="display: flex; justify-content: center;">&middot; This Week @ Doc &middot;</h1>
-        <hr class="solid">-->
+        <h1 style="display: flex; justify-content: center;">&middot; This Week @ Doc &middot;</h1>
+        <hr class="solid">
 
         <div class="cards">
           <div id="card">
@@ -72,6 +67,9 @@
         </div>
       </div>
 
+      <div style="background: url('/images/2024winter/Doc-gif.gif') no-repeat top center fixed; background-size: cover; display: block; margin: auto; width: 100vw; height: auto; object-fit: cover; object-position: bottom;">
+        <h1 style="display: flex; justify-content: center; font-size: 10vw; color: white; margin: 0 auto 0 auto;">WINTER 2024</h2>
+      </div>
 <!--
         <div id="piccadilly" class="anchor screening">
           <a href="calendar/Fall_2023/monday#piccadilly">
